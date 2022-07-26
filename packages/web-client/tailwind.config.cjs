20f3c1b49c807ae5a46f446218c40a9c40fd9477
Harsh Mandan <harsh@gobazzinga.io>
2022-07-26 16:28:34 +0530

    Add primary button shadow

--- packages/web-client/tailwind.config.cjs
+++ packages/web-client/tailwind.config.cjs
@@ -5,6 +5,9 @@ const config = {
 		extend: {
 			fontFamily: {
 				sans: ['Kumbh Sans', 'sans-serif']
+			},
+			boxShadow: {
+				'button-primary': '0px 4px 10px rgba(255, 113, 33, 0.2);'
 			}
 		}
 	},
