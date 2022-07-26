47cf86a9f2d0836063c95d616223854ceeb6c98d
Harsh Mandan <harsh@gobazzinga.io>
2022-07-26 16:41:19 +0530

    Add siabled state and use clsx utility

--- packages/web-client/src/components/button/Button.svelte
+++ packages/web-client/src/components/button/Button.svelte
@@ -1,29 +1,30 @@
 <script lang="ts">
-export let type: 'primary' | 'secondary' = 'primary';
-export let href = '';
-let exportClass: any = '';
-export { exportClass as class };
+import c from 'clsx';
 
-const commonClasses =
-	'flex items-center rounded-full justify-center px-4 py-3 font-bold text-white';
+export let type: 'primary' | 'secondary' = 'primary';
+export let disabled = false;
+export let href = '';
+export { exportClass as class };
+let exportClass: any = '';
+
+const classes = c(
+	'flex items-center rounded-full justify-center px-4 py-3 font-bold text-white',
+	{
+		'bg-orange-500': type === 'primary' && !disabled,
+		'bg-gray-500': type === 'primary' && disabled,
+		'border-2 border-white bg-transparent': type === 'secondary' && !disabled,
+		'border-2 border-white/20 bg-transparent text-gray-500': type === 'secondary' && disabled
+	},
+	exportClass
+);
 </script>
 
 {#if href}
-	<a
-		href="{href}"
-		class="{commonClasses} {type === 'primary'
-			? 'bg-orange-500'
-			: 'border-2 border-white/20 bg-transparent'} {exportClass}"
-	>
+	<a href="{href}" class="{classes}">
 		<slot />
 	</a>
 {:else}
-	<button
-		on:click
-		class="{commonClasses} {type === 'primary'
-			? 'bg-orange-500'
-			: 'border-2 border-white/20 bg-transparent'} {exportClass}"
-	>
+	<button on:click disabled="{disabled}" class="{classes}">
 		<slot />
 	</button>
 {/if}
