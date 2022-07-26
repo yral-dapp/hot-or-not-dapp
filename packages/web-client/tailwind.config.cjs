0e561e8a5f5b26d6c9aeb44b781a7ff5506f310a
Harsh Mandan <harsh@gobazzinga.io>
2022-07-26 16:24:44 +0530

    Use Kumbh Sans as a default font

--- packages/web-client/tailwind.config.cjs
+++ packages/web-client/tailwind.config.cjs
@@ -2,9 +2,12 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				sans: ['Kumbh Sans', 'sans-serif']
+			}
+		}
 	},
-
 	plugins: [require('@tailwindcss/forms')]
 };
 
