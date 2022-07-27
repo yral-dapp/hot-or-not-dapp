30bee16c3d8f5dfa5ece6d9b54fc05e7138c9ba0
Harsh Mandan <harsh@gobazzinga.io>
2022-07-27 15:38:59 +0530

    Keep all the experimental stuff in the subroute

--- packages/web-client/src/routes/experiment/__layout-home.svelte
+++ packages/web-client/src/routes/experiment/__layout-home.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
 import '../css/app.css';
 import { Buffer } from 'buffer'; // @dfinity/agent requires this. Can be removed once it's fixed
-import Transition from '$components/routing/Transition.svelte';
 import { onMount } from 'svelte';
 import { browser } from '$app/env';
-import GoogleAnalytics from '$components/seo/GoogleAnalytics.svelte';
 
 onMount(async () => {
 	if (browser) {
@@ -13,7 +11,4 @@ onMount(async () => {
 });
 </script>
 
-<Transition>
-	<slot />
-</Transition>
-<GoogleAnalytics />
+<slot />
