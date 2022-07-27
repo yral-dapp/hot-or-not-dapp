80f59e129633415e82c53c419071b276fd39523b
Harsh Mandan <harsh@gobazzinga.io>
2022-07-27 10:15:46 +0530

    Create Avatar component

---
+++ packages/web-client/src/components/avatar/Avatar.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+export let src = '';
+export { exportClass as class };
+let exportClass: any = '';
+</script>
+
+<img
+	alt="Avatar"
+	src="{src}"
+	class="rounded-full border-2 border-white object-cover object-center {exportClass}"
+/>
