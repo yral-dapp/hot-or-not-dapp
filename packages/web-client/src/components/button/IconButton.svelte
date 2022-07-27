ec6502f19a7a1aaa51f120eeb985bf10c200a0ef
Harsh Mandan <harsh@gobazzinga.io>
2022-07-27 10:10:33 +0530

    Add IconButton component

---
+++ packages/web-client/src/components/button/IconButton.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+import c from 'clsx';
+
+export let disabled = false;
+export let href = '';
+export { exportClass as class };
+let exportClass: any = '';
+
+const classes = c(
+	'flex items-center rounded-full select-none justify-center',
+	{
+		'text-zinc-500 pointer-events-none': disabled
+	},
+	exportClass
+);
+</script>
+
+{#if href}
+	<a href="{href}" class="{classes}">
+		<slot />
+	</a>
+{:else}
+	<button on:click disabled="{disabled}" class="{classes}">
+		<slot />
+	</button>
+{/if}
