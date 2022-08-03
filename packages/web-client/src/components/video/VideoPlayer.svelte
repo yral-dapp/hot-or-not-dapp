27f4b49a16b0505ce714dcad400fdeda52fab22b
Harsh Mandan <harsh@gobazzinga.io>
2022-08-03 14:54:57 +0530

    Add controls to video player

--- packages/web-client/src/components/video/VideoPlayer.svelte
+++ packages/web-client/src/components/video/VideoPlayer.svelte
@@ -1,4 +1,11 @@
 <script lang="ts">
+import Avatar from '$components/avatar/Avatar.svelte';
+import IconButton from '$components/button/IconButton.svelte';
+import EyeIcon from '$components/icons/EyeIcon.svelte';
+import FireIcon from '$components/icons/FireIcon.svelte';
+import HeartIcon from '$components/icons/HeartIcon.svelte';
+import ShareIcon from '$components/icons/ShareIcon.svelte';
+
 import { tick } from 'svelte';
 import { fade } from 'svelte/transition';
 
@@ -6,6 +13,10 @@ export let src = '';
 export let thumbnail = '';
 export let load = false;
 export let paused: boolean = true;
+export let avatarPhotoUrl =
+	'https://images.pexels.com/photos/3276046/pexels-photo-3276046.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2';
+export let userName = 'Natasha';
+export let videoViews = 254000;
 
 let isLoaded = false;
 let generatedThumbnail = '';
@@ -64,4 +75,33 @@ async function generateThumbnail(target: EventTarget | null) {
 			src="{thumbnail || generatedThumbnail}"
 		/>
 	{/if}
+
+	<div class="max-w-16 absolute right-4 bottom-20 z-[5]">
+		<div class="flex flex-col space-y-6">
+			<IconButton>
+				<HeartIcon class="h-8 w-8" />
+			</IconButton>
+			<IconButton>
+				<ShareIcon class="h-6 w-6" />
+			</IconButton>
+			<IconButton
+				class="rounded-full border-[0.15rem] border-[#FA9301] bg-gradient-to-b from-[#F63700] to-[#FFC848] p-2"
+			>
+				<FireIcon class="h-5 w-5" />
+			</IconButton>
+		</div>
+	</div>
+
+	<div class="absolute inset-x-0 bottom-20 left-4 right-16 z-[5] w-min">
+		<div class="pointer-events-auto flex space-x-3">
+			<Avatar class="h-12 w-12" src="{avatarPhotoUrl}" />
+			<div class="flex flex-col space-y-1">
+				<span>{userName}</span>
+				<div class="flex items-center space-x-1">
+					<EyeIcon class="h-4 w-4 text-white" />
+					<span class="text-sm">{videoViews}</span>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
