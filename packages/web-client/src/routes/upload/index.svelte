e0d38897556b894dd0c9c9b3e4198a31f07787c9
Harsh Mandan <harsh@gobazzinga.io>
2022-08-10 17:11:30 +0530

    Cleanup

--- packages/web-client/src/routes/upload/index.svelte
+++ packages/web-client/src/routes/upload/index.svelte
@@ -26,10 +26,9 @@ import {
 	getMediaStream,
 	type FacingMode
 } from '$lib/cameraPermissions';
-import { onMount, tick } from 'svelte';
-import { fade, scale } from 'svelte/transition';
+import { onMount, tick, onDestroy } from 'svelte';
+import { fade } from 'svelte/transition';
 import c from 'clsx';
-import { onDestroy } from 'svelte/types/runtime/internal/lifecycle';
 
 let videoEl: HTMLVideoElement;
 let mediaStream: MediaStream;
