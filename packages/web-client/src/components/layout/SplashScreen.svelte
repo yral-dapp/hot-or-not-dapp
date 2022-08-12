8b523037e5e9f94be4c263e87bc3748930f6f3e4
Harsh Mandan <harsh@gobazzinga.io>
2022-08-12 16:00:58 +0530

    Create splash screen component

---
+++ packages/web-client/src/components/layout/SplashScreen.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { fade } from 'svelte/transition';
+import { onMount } from 'svelte/types/runtime/internal/lifecycle';
+
+export let showSplash = true;
+
+onMount(() => setTimeout(() => (showSplash = false), 3000));
+</script>
+
+{#if showSplash}
+	<splash-screen
+		out:fade|local="{{ duration: 500 }}"
+		class="absolute z-[15] flex h-full w-full items-center justify-center bg-primary text-9xl"
+	>
+		H.o.N
+	</splash-screen>
+{/if}
