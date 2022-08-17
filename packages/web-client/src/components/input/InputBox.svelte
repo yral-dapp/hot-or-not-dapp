ec1fe3aba7ccb7e14c5487d9d6abbc7662ef76b3
harshbaz <106158714+harshbaz@users.noreply.github.com>
2022-08-17 16:04:50 +0530

    Add input components (#98)
    * Add autocomplete types
    
    * Add input element
    
    * Add inputBox element

---
+++ packages/web-client/src/components/input/InputBox.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+export let disabled = false;
+export let placeholder = '';
+export let value: string | number = '';
+export let cols: number = 20;
+export let rows: number = 20;
+export { exportClass as class };
+
+let exportClass: any = '';
+</script>
+
+<textarea
+	disabled="{disabled}"
+	placeholder="{placeholder}"
+	cols="{cols}"
+	rows="{rows}"
+	bind:value
+	on:input
+	on:change
+	on:focus
+	on:blur
+	class="grow border-none bg-transparent py-3 px-4 focus:ring-transparent {exportClass}"></textarea>
