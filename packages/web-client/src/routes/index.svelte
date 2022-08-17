34e83168f38b8da2c4b3d048b994e511a20a3000
Harsh Mandan <harsh@gobazzinga.io>
2022-08-17 23:26:06 +0530

    Do not redirect

--- packages/web-client/src/routes/index.svelte
+++ packages/web-client/src/routes/index.svelte
@@ -1,10 +1,2 @@
 <script lang="ts" context="module">
-import type { Load } from '@sveltejs/kit';
-
-export const load: Load = () => {
-	return {
-		status: 307,
-		redirect: '/all/0'
-	};
-};
 </script>
