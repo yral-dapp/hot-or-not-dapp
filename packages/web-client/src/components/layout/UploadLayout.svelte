a1c4da6815e230d8b82b78a795afc8fe9daa1aa0
Harsh Mandan <harsh@gobazzinga.io>
2022-08-17 19:11:52 +0530

    Create upload layout

---
+++ packages/web-client/src/components/layout/UploadLayout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+export let testMode = false;
+$: testClasses = testMode ? 'border-2 border-white/30' : '';
+$: innerHeight = window?.innerHeight;
+</script>
+
+<svelte:window on:resize="{() => (innerHeight = window?.innerHeight)}" />
+
+<upload-layout
+	style="height: 100vh; {innerHeight ? `height: ${innerHeight}px` : ''}"
+	class="relative flex h-full w-full flex-col overflow-hidden text-white"
+>
+	<div class="relative flex h-12 w-full items-center justify-center {testClasses}">
+		<div class="absolute left-0">
+			<slot name="top-left" />
+		</div>
+		<slot name="top-center" />
+	</div>
+	<slot name="content" />
+	<div class="absolute inset-x-0 bottom-8 z-[2] {testClasses}">
+		<slot name="bottom" />
+	</div>
+</upload-layout>
