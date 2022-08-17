0167e50b4b7b3b6d4ff54cf63f9e3b2791a3efaa
Harsh Mandan <harsh@gobazzinga.io>
2022-08-17 23:31:39 +0530

    Revert "Do not redirect"
    This reverts commit 34e83168f38b8da2c4b3d048b994e511a20a3000.

--- packages/web-client/src/routes/index.svelte
+++ packages/web-client/src/routes/index.svelte
@@ -1,2 +1,10 @@
 <script lang="ts" context="module">
+import type { Load } from '@sveltejs/kit';
+
+export const load: Load = () => {
+	return {
+		status: 307,
+		redirect: '/all/0'
+	};
+};
 </script>
