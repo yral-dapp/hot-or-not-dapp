478a29c3805ecd3a65102e03436d6fbfd08d758f
Harsh Mandan <harsh@gobazzinga.io>
2022-08-18 17:06:03 +0530

    Create component for Upload step number indicator

---
+++ packages/web-client/src/components/upload/UploadStep.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+import c from 'clsx';
+export let step: 1 | 2 | 3 = 1;
+export let status: 'queued' | 'active' | 'finished' = 'queued';
+</script>
+
+<div
+	class="{c('-mt-0.5 flex h-8 w-8 shrink-0 items-center justify-center rounded-full', {
+		'border-white/50': status === 'queued',
+		'border-primary': status === 'active',
+		'border-2': status !== 'finished',
+		'bg-green-500': status === 'finished'
+	})}"
+>
+	{#if status !== 'finished'}
+		{step}
+	{:else}
+		✓
+	{/if}
+</div>
