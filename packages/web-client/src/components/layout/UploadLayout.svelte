21ccbf68f60b39faea8d637ba7a0e9b6d600c17f
Harsh Mandan <harsh@gobazzinga.io>
2022-08-18 15:51:38 +0530

    Update header layout

--- packages/web-client/src/components/layout/UploadLayout.svelte
+++ packages/web-client/src/components/layout/UploadLayout.svelte
@@ -10,7 +10,9 @@ $: innerHeight = window?.innerHeight;
 	style="height: 100vh; {innerHeight ? `height: ${innerHeight}px` : ''}"
 	class="relative flex h-full w-full flex-col overflow-hidden text-white"
 >
-	<div class="relative flex h-12 w-full items-center justify-center {testClasses}">
+	<div
+		class="relative z-[2] flex h-12 w-full items-center justify-center bg-black py-8 shadow-xl shadow-black/50 {testClasses}"
+	>
 		<div class="absolute left-1 top-1">
 			<slot name="top-left" />
 		</div>
