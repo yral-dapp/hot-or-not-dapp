14b9e5593bf8a6a210957722462593439937561d
Harsh Mandan <harsh@gobazzinga.io>
2022-08-20 13:55:17 +0530

    Show home icon selected if on home

--- packages/web-client/src/components/bottom-navigation/BottomNavigation.svelte
+++ packages/web-client/src/components/bottom-navigation/BottomNavigation.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
+import { page } from '$app/stores';
 import IconButton from '$components/button/IconButton.svelte';
 import HomeIcon from '$components/icons/HomeIcon.svelte';
 import MenuIcon from '$components/icons/MenuIcon.svelte';
 import PlusIcon from '$components/icons/PlusIcon.svelte';
 import TrophyIcon from '$components/icons/TrophyIcon.svelte';
 import WalletIcon from '$components/icons/WalletIcon.svelte';
+
+$: path = $page.url.pathname;
 </script>
 
 <div class="mb-2 flex w-full justify-between px-4">
-	<IconButton class="flex items-center p-2">
-		<HomeIcon class="h-6 w-6 text-white" />
+	<IconButton href="/all" class="flex items-center p-2">
+		<HomeIcon filled="{path.includes('all')}" class="h-6 w-6 text-white" />
 	</IconButton>
 	<IconButton class="flex items-center p-2">
 		<TrophyIcon class="h-6 w-6 text-white" />
