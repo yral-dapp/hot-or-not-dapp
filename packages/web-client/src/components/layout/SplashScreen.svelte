bf10e73b252a13a4b861b19c399cf6a89e633c79
Harsh Mandan <harsh@gobazzinga.io>
2022-08-20 14:08:09 +0530

    Use store to show splash screen only once

--- packages/web-client/src/components/layout/SplashScreen.svelte
+++ packages/web-client/src/components/layout/SplashScreen.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
 import { fade } from 'svelte/transition';
 import { onMount } from 'svelte';
+import { showSplashScreen } from '$stores/splashScreen';
 
-export let showSplash = true;
-
-onMount(() => setTimeout(() => (showSplash = false), 3000));
+onMount(() =>
+	setTimeout(() => {
+		$showSplashScreen = false;
+	}, 3000)
+);
 </script>
 
-{#if showSplash}
+{#if $showSplashScreen}
 	<splash-screen
 		out:fade|local="{{ duration: 500 }}"
 		class="absolute z-[15] flex h-full w-full items-center justify-center bg-primary text-9xl"
