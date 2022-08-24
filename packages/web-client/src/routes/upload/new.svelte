6ddeaf02c716164d4800400e6dee422dd0110135
Harsh Mandan <harsh@gobazzinga.io>
2022-08-24 13:10:29 +0530

    Update href links to use query based

--- packages/web-client/src/routes/upload/new.svelte
+++ packages/web-client/src/routes/upload/new.svelte
@@ -99,7 +99,7 @@ async function showShareDialog() {
 		await navigator.share({
 			title: 'Hot or Not',
 			text: 'Video title',
-			url: 'https://v2.gobazzinga.io/all/2'
+			url: 'https://v2.gobazzinga.io/all?id=2'
 		});
 	} catch (err) {
 		console.error('Cannot open share dialog', err);
