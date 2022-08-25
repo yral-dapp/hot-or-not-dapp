046557fee61fe275d87787276a0f61015fd18ef9
Harsh Mandan <harsh@gobazzinga.io>
2022-08-25 16:02:30 +0530

    Add cloudflare worker to allow uploads to cfstream

---
+++ packages/workers/video/getVideoProcessingStatus.js
@@ -0,0 +1,41 @@
+const handler = async (request) => {
+	try {
+		const cloudflareVideoUid = request.params.cloudflareVideoUid;
+
+		const cloudflareQueryResponse = await (
+			await fetch(
+				// eslint-disable-next-line no-undef
+				`https://api.cloudflare.com/client/v4/accounts/${CLOUDFLARE_ACCOUNT_ID}/stream/${cloudflareVideoUid}`,
+				{
+					method: 'GET',
+					headers: {
+						// eslint-disable-next-line no-undef
+						Authorization: `Bearer ${CLOUDFLARE_API_TOKEN}`,
+						'Content-Type': 'application/json'
+					}
+				}
+			)
+		).json();
+
+		return new Response(
+			JSON.stringify({
+				readyToStream: cloudflareQueryResponse.result.status.state === 'ready'
+			}),
+			{
+				status: 200,
+				headers: {
+					'Content-Type': 'application/json',
+					...request.corsHeaders
+				}
+			}
+		);
+	} catch (error) {
+		console.error(error);
+		return new Response('Forbidden', {
+			status: 403,
+			headers: { ...request.corsHeaders }
+		});
+	}
+};
+
+export default handler;
