48e35a9afe648f74ad543d2aeaaa25ac284eb183
Harsh Mandan <harsh@gobazzinga.io>
2022-08-25 16:10:58 +0530

    Add user's feed route

---
+++ packages/web-client/src/routes/users/[id].svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+import HomeLayout from '$components/layout/HomeLayout.svelte';
+import Videos from '$components/video/Videos.svelte';
+import BottomNavigation from '$components/bottom-navigation/BottomNavigation.svelte';
+import { page } from '$app/stores';
+import IconButton from '$components/button/IconButton.svelte';
+import CaretLeftIcon from '$components/icons/CaretLeftIcon.svelte';
+
+let startingVideoIndex = Number($page.params.id) ?? 0;
+</script>
+
+<HomeLayout>
+	<svelte:fragment slot="top">
+		<div class="flex items-center rounded-full bg-black/10 py-2 px-4">User's Videos</div>
+		<div class="absolute top-4 left-4">
+			<IconButton>
+				<CaretLeftIcon class="h-5 w-5" />
+			</IconButton>
+		</div>
+	</svelte:fragment>
+	<svelte:fragment slot="content">
+		<Videos fetchFromId="{startingVideoIndex}" />
+	</svelte:fragment>
+	<div class="w-full" slot="bottom-navigation">
+		<BottomNavigation />
+	</div>
+</HomeLayout>
