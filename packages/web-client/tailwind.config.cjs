8f95a7abafd4a5e7dc183c02c23b37de57dbf8b7
Harsh Mandan <harsh@gobazzinga.io>
2022-08-26 12:49:00 +0530

    Add an up direction shadow

--- packages/web-client/tailwind.config.cjs
+++ packages/web-client/tailwind.config.cjs
@@ -7,7 +7,8 @@ const config = {
 				sans: ['Kumbh Sans', 'sans-serif']
 			},
 			boxShadow: {
-				'button-primary': '0px 4px 10px rgba(255, 113, 33, 0.2);'
+				'button-primary': '0px 4px 10px rgba(255, 113, 33, 0.2)',
+				up: ['0px -20px 25px -5px rgba(0,0,0,0.5)', '0px -8px 10px -6px rgba(0,0,0,0.5)']
 			},
 			colors: {
 				primary: '#E96B25'
