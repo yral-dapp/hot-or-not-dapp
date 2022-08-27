cc595c3c2914e5e056a1a6327cf912866a59be09
Harsh Mandan <harsh@gobazzinga.io>
2022-08-27 15:49:51 +0530

    Add Popup components

---
+++ packages/web-client/src/components/popup/LogoutPopup.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+import Button from '$components/button/Button.svelte';
+import Popup from './Popup.svelte';
+import { auth } from '$stores/auth';
+import { initializeAuthClient } from '$lib/authHelper';
+
+export let show = false;
+let loading = false;
+
+async function handleLogout() {
+	loading = true;
+	await $auth.client?.logout();
+	await initializeAuthClient();
+	loading = false;
+	show = false;
+}
+</script>
+
+<Popup bind:show>
+	<div class="flex flex-col space-y-4">
+		<div>Are you sure you want to logout?</div>
+		<Button disabled="{loading}" on:click="{handleLogout}">Yes, I'm sure</Button>
+		<Button
+			on:click="{() => (show = false)}"
+			disabled="{loading}"
+			type="secondary"
+			class="border-black/50 text-black/70"
+		>
+			Go back
+		</Button>
+	</div>
+</Popup>
---
+++ packages/web-client/src/components/popup/Popup.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+import IconButton from '$components/button/IconButton.svelte';
+import CloseIcon from '$components/icons/CloseIcon.svelte';
+import { fade } from 'svelte/transition';
+
+export let show = false;
+</script>
+
+{#if show}
+	<div
+		transition:fade|local="{{ duration: 100 }}"
+		on:click="{() => (show = false)}"
+		class="absolute inset-0 z-[98] flex h-full w-full items-center justify-center bg-black/50"
+	>
+		<div
+			on:click="{(e) => e.stopImmediatePropagation()}"
+			class="relative z-[99] rounded-lg bg-white p-10"
+		>
+			<IconButton on:click="{() => (show = false)}" class="absolute top-2 right-2">
+				<CloseIcon class="h-6 w-6 text-black/50" />
+			</IconButton>
+			<slot />
+		</div>
+	</div>
+{/if}
