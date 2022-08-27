4ac47fbb30aa9dc14e3d360b03973a0cae95a8ef
Harsh Mandan <harsh@gobazzinga.io>
2022-08-27 15:07:46 +0530

    Update backend util to cache actor & use identity

--- packages/web-client/src/lib/backend.ts
+++ packages/web-client/src/lib/backend.ts
@@ -1,10 +1,52 @@
-import { createActor, canisterId } from '../../declarations/backend';
+import {
+	createActor as createUserIndexActor,
+	canisterId as userIndexCanisterId
+} from '$canisters/user_index';
+import { createActor as createIndividualUserActor } from '$canisters/individual_user_template';
+import type { _SERVICE as _USER_INDEX_SERVICE } from '$canisters/user_index/user_index.did';
+import type { _SERVICE as _INDIVIDUAL_USER_SERVICE } from '$canisters/individual_user_template/individual_user_template.did';
+import { auth } from '$stores/auth';
+import type { ActorSubclass } from '@dfinity/agent';
+import { get } from 'svelte/store';
 
 export const host =
 	process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : 'https://ic0.app';
 
-export const backend = createActor(canisterId ?? '', {
-	agentOptions: {
-		host
-	}
-});
+export type UserIndexActor = ActorSubclass<_USER_INDEX_SERVICE>;
+export type IndividualUserCanister = ActorSubclass<_INDIVIDUAL_USER_SERVICE>;
+
+const userIndexCanister: {
+	actor?: UserIndexActor;
+	loginState: boolean;
+} = {
+	loginState: false
+};
+
+const individualUserCanister: {
+	actor?: IndividualUserCanister;
+	loginState: boolean;
+} = {
+	loginState: false
+};
+
+export function userIndex(): UserIndexActor {
+	const authStore = get(auth);
+	if (!userIndexCanister.actor || userIndexCanister.loginState != authStore.isLoggedIn) {
+		userIndexCanister.actor = createUserIndexActor(userIndexCanisterId as string, {
+			agentOptions: { identity: authStore?.identity, host }
+		}) as UserIndexActor;
+		userIndexCanister.loginState = authStore.isLoggedIn;
+		return userIndexCanister.actor;
+	} else return userIndexCanister.actor;
+}
+
+export function individualUser(individualUserCanisterId: string): IndividualUserCanister {
+	const authStore = get(auth);
+	if (!individualUserCanister.actor || individualUserCanister.loginState != authStore.isLoggedIn) {
+		individualUserCanister.actor = createIndividualUserActor(individualUserCanisterId as string, {
+			agentOptions: { identity: authStore?.identity, host }
+		}) as IndividualUserCanister;
+		individualUserCanister.loginState = authStore.isLoggedIn;
+		return individualUserCanister.actor;
+	} else return individualUserCanister.actor;
+}
