4e6789553a303a66458834980811ffa772b37987
Harsh Mandan <harsh@gobazzinga.io>
2022-08-29 16:45:18 +0530

    Update all functions: 1. Update url generate function 2. Update uploadVideoStream function 3. Add status check function

--- packages/web-client/src/lib/stream.ts
+++ packages/web-client/src/lib/stream.ts
@@ -1,37 +1,28 @@
+import { auth } from '$stores/auth';
+import { get } from 'svelte/store';
+
+const cfWorkerHost = import.meta.env.VITE_CLOUDFLARE_WORKERS_API_URL;
+
 async function generateUrl() {
-	const res = await fetch(
-		`https://api.cloudflare.com/client/v4/accounts/${
-			import.meta.env.VITE_CF_ACCOUNT_ID
-		}/stream/direct_upload`,
-		{
-			method: 'POST',
-			headers: {
-				Authorization: `Bearer ${import.meta.env.VITE_CF_STREAM_API}`
-			},
-			body: JSON.stringify({
-				maxDurationSeconds: 3600,
-				expiry: '2020-04-06T02:20:00Z',
-				requireSignedURLs: true,
-				allowedOrigins: ['hotornot.wtf', 'localhost:5173'],
-				thumbnailTimestampPct: 0.568427
-				// TODO: Add watermark
-				// watermark: {
-				// 	uid: '<WATERMARK_UID>'
-				// }
-			})
-		}
-	);
+	const authStore = get(auth);
+	const res = await fetch(`${cfWorkerHost}/video/getVideoUploadURL`, {
+		method: 'POST',
+		body: JSON.stringify({
+			principalId: authStore.principal?.toText() || '',
+			fileName: Date.now().toString()
+		})
+	});
 	const body = await res.json();
 	if (body.success) {
-		return body.result.uploadUrl as string;
+		return body.result as { uploadUrl: string; uid: string };
 	} else {
 		return undefined;
 	}
 }
 
 export async function uploadVideoToStream(file: Blob) {
-	const uploadUrl = await generateUrl();
-	if (!uploadUrl) {
+	const uploadRes = await generateUrl();
+	if (!uploadRes || !uploadRes.uploadUrl) {
 		return {
 			success: false,
 			error: "Couldn't generate upload Url"
@@ -40,15 +31,12 @@ export async function uploadVideoToStream(file: Blob) {
 	try {
 		const formData = new FormData();
 		formData.append('file', file);
-		const res = await fetch(uploadUrl, {
+		const res = await fetch(uploadRes.uploadUrl, {
 			method: 'POST',
-			headers: {
-				Authorization: `Bearer ${import.meta.env.VITE_CF_STREAM_API}`
-			},
 			body: formData
 		});
-		const body = await res.json();
-		if (!body || (body && !body.success)) {
+
+		if (res.status != 200) {
 			return {
 				success: false,
 				error: 'Something went wrong while uploading file'
@@ -56,7 +44,7 @@ export async function uploadVideoToStream(file: Blob) {
 		} else {
 			return {
 				success: true,
-				uploadedUrl: body.result.preview
+				uid: uploadRes.uid
 			};
 		}
 	} catch (e) {
@@ -64,6 +52,25 @@ export async function uploadVideoToStream(file: Blob) {
 		return {
 			success: false,
 			error: 'Something went wrong while uploading file'
+		};
+	}
+}
+
+export async function checkVideoStatus(uid: string) {
+	try {
+		const req = await fetch(`${cfWorkerHost}/video/${uid}/getVideoProcessingStatus`, {
+			method: 'GET'
+		});
+		const res = await req.json();
+		console.log('status res', res);
+		return {
+			success: true,
+			status: (res.readyToStream ? 'ready' : 'processing') as 'ready' | 'processing'
+		};
+	} catch (e) {
+		return {
+			success: false,
+			error: 'Something went wrong while checking status'
 		};
 	}
 }
