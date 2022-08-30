6bf7a713234fd5456fa65f8356a902445df5616c
Harsh Mandan <harsh@gobazzinga.io>
2022-08-30 12:51:37 +0530

    Add env variable to enable ssr

--- packages/web-client/src/hooks/index.js
+++ packages/web-client/src/hooks/index.js
@@ -1,7 +1,8 @@
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
+	console.log('ssr', import.meta.env.IS_SSR);
 	const response = await resolve(event, {
-		ssr: false
+		ssr: import.meta.env.IS_SSR == 'yes'
 	});
 
 	return response;
