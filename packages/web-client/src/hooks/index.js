dcd95a656cb028cc545c01269dd881f58c007f12
Harsh Mandan <harsh@gobazzinga.io>
2022-08-30 13:01:13 +0530

    Update hook

--- packages/web-client/src/hooks/index.js
+++ packages/web-client/src/hooks/index.js
@@ -1,8 +1,9 @@
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
 	console.log('ssr', import.meta.env.IS_SSR);
+	const ssr = import.meta.env.IS_SSR === 'yes';
 	const response = await resolve(event, {
-		ssr: import.meta.env.IS_SSR == 'yes'
+		ssr
 	});
 
 	return response;
