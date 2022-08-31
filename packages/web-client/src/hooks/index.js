65d697120670a42688d88c412b908e2be38ef074
Harsh Mandan <harsh@gobazzinga.io>
2022-08-31 08:37:44 +0530

    Make ssr primary and static secondary

--- packages/web-client/src/hooks/index.js
+++ packages/web-client/src/hooks/index.js
@@ -1,7 +1,7 @@
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
 	const response = await resolve(event, {
-		ssr: import.meta.env.IS_SSR
+		ssr: import.meta.env.ENABLE_SSR
 	});
 
 	return response;
