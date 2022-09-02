faab9f0b2d35b41093865449001c2338816eebe7
harshbaz <106158714+harshbaz@users.noreply.github.com>
2022-09-02 15:28:22 +0530

    Create named layouts and simplify routes (#317)

---
+++ packages/web-client/src/routes/all/[id]@feed.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+import { page } from '$app/stores';
+import SplashScreen from '$components/layout/SplashScreen.svelte';
+import VideosSwiper from '$components/video/VideosSwiper.svelte';
+
+let startingVideoIndex = Number($page.params.id) ?? 0;
+</script>
+
+<SplashScreen showOnPath="{$page.url.pathname.includes('all')}" />
+
+<VideosSwiper fetchFromId="{startingVideoIndex}" />
---
+++ packages/web-client/src/routes/menu/index@feed.svelte
@@ -0,0 +1,132 @@
+<script lang="ts">
+import CaretLeftIcon from '$components/icons/CaretLeftIcon.svelte';
+import TelegramIcon from '$components/icons/TelegramIcon.svelte';
+import DiscordIcon from '$components/icons/DiscordIcon.svelte';
+import TwitterIcon from '$components/icons/TwitterIcon.svelte';
+import CoinBagIcon from '$components/icons/CoinBagIcon.svelte';
+import CenterTextIcon from '$components/icons/CenterTextIcon.svelte';
+import MessageBoxIcon from '$components/icons/MessageBoxIcon.svelte';
+import NotebookIcon from '$components/icons/NotebookIcon.svelte';
+import LockIcon from '$components/icons/LockIcon.svelte';
+import LogoutIcon from '$components/icons/LogoutIcon.svelte';
+import placeholderImg from '$assets/placeholder.png';
+import Button from '$components/button/Button.svelte';
+import { onMount } from 'svelte';
+import { prefetch } from '$app/navigation';
+import { auth } from '$stores/auth';
+import LogoutPopup from '$components/popup/LogoutPopup.svelte';
+import Ic0Icon from '$components/icons/Ic0Icon.svelte';
+
+$: links = [
+	{
+		icon: CoinBagIcon,
+		title: 'Refer and Earn',
+		class: 'w-5 h-5',
+		href: '/refer'
+	},
+	{
+		icon: CenterTextIcon,
+		title: 'About Us',
+		class: 'w-5 h-5',
+		href: '/about-us'
+	},
+	{
+		icon: MessageBoxIcon,
+		title: 'FAQs',
+		href: '/faq'
+	},
+	{
+		icon: NotebookIcon,
+		title: 'Terms of Service',
+		href: '/terms-of-service'
+	},
+	{
+		icon: LockIcon,
+		title: 'Privacy Policy',
+		href: '/privacy-policy'
+	},
+	{
+		icon: LogoutIcon,
+		title: 'Logout',
+		class: 'w-5 h-5 pl-1',
+		onClick: () => (showLogoutPopup = true),
+		hide: !$auth.isLoggedIn
+	}
+];
+
+let showLogoutPopup = false;
+
+function prefetchLinks() {
+	links.forEach((link) => {
+		link.href && prefetch(link.href);
+	});
+	prefetch('/profile/0');
+}
+
+onMount(() => prefetchLinks());
+</script>
+
+<LogoutPopup bind:show="{showLogoutPopup}" />
+
+<div
+	class="flex h-full w-full flex-col justify-between space-y-16 overflow-hidden overflow-y-auto py-20 px-8"
+>
+	<div class="flex w-full shrink-0 flex-col space-y-10">
+		{#if $auth.isLoggedIn}
+			<div class="sticky flex w-full items-center space-x-4 pb-2">
+				<img alt="profile" class="h-24 w-24 rounded-full object-cover" src="{placeholderImg}" />
+				<div class="flex flex-col space-y-1">
+					<div class="text-xl">Harsh</div>
+					<a href="/profile/1" sveltekit:prefetch class=" text-primary">View Profile</a>
+				</div>
+			</div>
+		{:else}
+			<div class="flex h-24 w-full items-center justify-center">
+				<Button on:click="{() => ($auth.showLogin = true)}" class="w-full">Login</Button>
+			</div>
+		{/if}
+		<div class="my-8 h-[1px] w-full bg-white/10"></div>
+		{#each links as link}
+			{#if !link.hide}
+				<svelte:element
+					this="{link.href ? 'a' : 'button'}"
+					on:click="{link.onClick}"
+					href="{link.href}"
+					sveltekit:prefetch="{link.href ? true : null}"
+					class="flex items-center justify-between"
+				>
+					<div class="flex items-center space-x-4 text-white">
+						<svelte:component this="{link.icon}" class="{link.class ?? 'h-6 w-6'}" />
+						<div>{link.title}</div>
+					</div>
+					<CaretLeftIcon class="h-6 w-6 rotate-180" />
+				</svelte:element>
+			{/if}
+		{/each}
+	</div>
+	<div class="flex flex-col items-center justify-center space-y-4">
+		<div class="text-sm text-white/50">Follow us on</div>
+		<div class="flex items-center space-x-4">
+			<div
+				class="flex h-12 w-12 items-center justify-center rounded-full border-[1px] border-primary transition-colors duration-200 active:bg-primary"
+			>
+				<TelegramIcon class="h-5 w-5 -translate-x-[1px]" />
+			</div>
+			<div
+				class="flex h-12 w-12 items-center justify-center rounded-full border-[1px] border-primary transition-colors duration-200 active:bg-primary"
+			>
+				<DiscordIcon class="h-5 w-5" />
+			</div>
+			<div
+				class="flex h-12 w-12 items-center justify-center rounded-full border-[1px] border-primary transition-colors duration-200 active:bg-primary"
+			>
+				<TwitterIcon class="h-4 w-4" />
+			</div>
+			<div
+				class="flex h-12 w-12 items-center justify-center rounded-full border-[1px] border-primary transition-colors duration-200 active:bg-primary"
+			>
+				<Ic0Icon class="h-5 w-5" />
+			</div>
+		</div>
+	</div>
+</div>
