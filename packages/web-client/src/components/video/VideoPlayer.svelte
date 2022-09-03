f3fa211fad68dfc307517447d6c4e1d638957511
Harsh Mandan <harsh@gobazzinga.io>
2022-09-03 17:56:55 +0530

    Update bottom padding

--- packages/web-client/src/components/video/VideoPlayer.svelte
+++ packages/web-client/src/components/video/VideoPlayer.svelte
@@ -77,7 +77,7 @@ async function handleClick() {
 	i="{i}"
 	on:click="{handleClick}"
 	class="{c(
-		'relative flex h-full items-center justify-center transition-all duration-500',
+		'inline-flex h-full items-center justify-center transition-all duration-500',
 		loaded ? 'opacity-100' : 'opacity-0',
 		swiperJs ? 'w-full' : 'min-h-full w-auto snap-center snap-always'
 	)}"
@@ -126,7 +126,7 @@ async function handleClick() {
 		</div>
 	{/if}
 
-	<div class="max-w-16 absolute right-4 bottom-20 z-[5]">
+	<div class="max-w-16 absolute right-4 bottom-20 z-[10]">
 		<div class="flex flex-col space-y-6">
 			<IconButton>
 				<HeartIcon class="h-8 w-8" />
@@ -160,6 +160,7 @@ async function handleClick() {
 		</div>
 	</div>
 </player>
+
 {#if !loaded}
 	<loader
 		transition:fade|local="{{ duration: 300 }}"
@@ -168,3 +169,11 @@ async function handleClick() {
 		<LoadingIcon class="h-36 w-36 animate-spin-slow text-primary" />
 	</loader>
 {/if}
+
+<style>
+@supports (-webkit-overflow-scrolling: touch) {
+	* {
+		transform: translate3d(0, 0, 0);
+	}
+}
+</style>
