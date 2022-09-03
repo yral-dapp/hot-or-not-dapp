288c3e3c3e701597303db350fed2bbdc3205001e
Saikat Das <saikatdas0790@gmail.com>
2022-09-03 17:37:28 +0530

    Provide actions and reporting APIs (#332)

--- packages/web-client/declarations/individual_user_template/individual_user_template.did.js
+++ packages/web-client/declarations/individual_user_template/individual_user_template.did.js
@@ -4,9 +4,31 @@ export const idlFactory = ({ IDL }) => {
     'description' : IDL.Text,
     'video_url' : IDL.Text,
   });
+  const PostViewDetailsFromFrontend = IDL.Variant({
+    'WatchedMultipleTimes' : IDL.Record({
+      'percentage_watched' : IDL.Nat8,
+      'watch_count' : IDL.Nat8,
+    }),
+    'WatchedPartially' : IDL.Record({ 'percentage_watched' : IDL.Nat8 }),
+  });
   return IDL.Service({
     'create_post' : IDL.Func([PostDetailsFromFrontend], [IDL.Nat64], []),
-    'mark_post_as_ready_to_view' : IDL.Func([IDL.Nat64], [], []),
+    'update_post_add_view_details' : IDL.Func(
+        [IDL.Nat64, PostViewDetailsFromFrontend],
+        [],
+        [],
+      ),
+    'update_post_as_ready_to_view' : IDL.Func([IDL.Nat64], [], []),
+    'update_post_increment_share_count' : IDL.Func(
+        [IDL.Nat64],
+        [IDL.Nat64],
+        [],
+      ),
+    'update_post_toggle_like_status_by_caller' : IDL.Func(
+        [IDL.Nat64],
+        [IDL.Bool],
+        [],
+      ),
   });
 };
 export const init = ({ IDL }) => { return []; };
