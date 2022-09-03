39833c06bd9b6c878509abbdc336d1975ff0b2c5
Harsh Mandan <harsh@gobazzinga.io>
2022-09-03 13:40:44 +0530

    Add a separate popup for cropping image

--- packages/web-client/src/components/profile/ProfileImageSelector.svelte
+++ packages/web-client/src/components/profile/ProfileImageSelector.svelte
@@ -1,15 +1,28 @@
 <script lang="ts">
 import IconButton from '$components/button/IconButton.svelte';
+import CheckIcon from '$components/icons/CheckIcon.svelte';
 import PencilIcon from '$components/icons/PencilIcon.svelte';
 import PlusIcon from '$components/icons/PlusIcon.svelte';
 import UserAvatarIcon from '$components/icons/UserAvatarIcon.svelte';
 import Popup from '$components/popup/Popup.svelte';
+import { getCroppedImg } from '$lib/canvasUtils';
+import Cropper from 'svelte-easy-crop/src/index.svelte';
 
 export let src = '';
+let latestCropDetails: {
+	x: number;
+	y: number;
+	height: number;
+	width: number;
+} | null = null;
+
+$: console.log({ latestCropDetails });
 
 function checkFileSelected(files: FileList | null) {
 	loading = true;
 	if (files && files[0]) {
+		menuPopup = false;
+		cropPopup = true;
 		// if (files[0].size / 1024 / 1024 > 5) {
 		// 	//file is larger than 200 MiB
 		// 	// invalidFileSelected = {
@@ -27,9 +40,8 @@ function checkFileSelected(files: FileList | null) {
 	}
 }
 
-let popup = {
-	show: false
-};
+let menuPopup = false;
+let cropPopup = false;
 let inputEl: HTMLInputElement;
 let loading = false;
 </script>
@@ -43,7 +55,7 @@ let loading = false;
 		<img alt="User avatar" src="{src}" class="h-48 w-48 rounded-full object-cover" />
 	{/if}
 	<IconButton
-		on:click="{() => (popup.show = true)}"
+		on:click="{() => (menuPopup = true)}"
 		disabled="{loading}"
 		class="absolute bottom-0 right-0 flex h-12 w-12 items-center justify-center rounded-full bg-orange-500 focus:bg-orange-600"
 	>
@@ -64,13 +76,13 @@ let loading = false;
 	on:change="{(e) => checkFileSelected(e.currentTarget.files)}"
 />
 
-<Popup on:close="{() => (inputEl.value = '')}" bind:show="{popup.show}" class="mx-20 w-full">
+<Popup on:close="{() => (inputEl.value = '')}" bind:show="{menuPopup}" class="mx-20 w-full">
 	<div class="flex w-full flex-col gap-4 divide-y-2 divide-zinc-200 text-black">
 		{#if src}
 			<button
 				on:click="{() => {
 					src = '';
-					popup.show = false;
+					menuPopup = false;
 				}}"
 				class="text-red-500">Delete photo</button
 			>
@@ -80,10 +92,34 @@ let loading = false;
 			class="pt-4"
 			on:click="{() => {
 				inputEl?.click();
-				popup.show = false;
 			}}"
 		>
 			Choose photo
 		</button>
 	</div>
+</Popup>
+
+<Popup class="h-1/2" bind:show="{cropPopup}">
+	<div
+		class="absolute left-2 top-2 z-[100] rounded-md bg-white px-4 py-2 text-black backdrop-blur-md"
+	>
+		Crop image to continue
+	</div>
+
+	<IconButton
+		on:click="{async () => {
+			src = await getCroppedImg(src, latestCropDetails);
+			cropPopup = false;
+		}}"
+		class="absolute right-2 top-2 z-[100] flex h-10 w-10 items-center rounded-full bg-primary text-white backdrop-blur-md"
+	>
+		<CheckIcon class="h-4 w-4" />
+	</IconButton>
+	<Cropper
+		on:cropcomplete="{({ detail }) => (latestCropDetails = detail.pixels)}"
+		image="{src}"
+		aspect="{1}"
+		crop="{{ x: 0, y: 0 }}"
+		zoom="{1}"
+	/>
 </Popup>
