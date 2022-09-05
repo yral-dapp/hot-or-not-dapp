decba46000f83b5cfc7d21f2f2bef43b6a3927e2
Harsh Mandan <harsh@gobazzinga.io>
2022-09-05 14:11:15 +0530

    Add image helper

---
+++ packages/web-client/src/lib/imageHelper.ts
@@ -0,0 +1,44 @@
+import { auth } from '$stores/auth';
+import { get } from 'svelte/store';
+
+const cfWorkerHost = import.meta.env.VITE_CLOUDFLARE_WORKERS_API_HOST;
+
+async function generateUrl() {
+	const authStore = get(auth);
+	const res = await fetch(`${cfWorkerHost}/video/getVideoUploadURL`, {
+		method: 'POST',
+		body: JSON.stringify({
+			principalId: authStore.principal?.toText() || '',
+			fileName: Date.now().toString()
+		})
+	});
+	const body = await res.json();
+	console.log('image url request', body);
+
+	if (body.success) {
+		return body.result as { uploadURL: string; id: string };
+	} else {
+		return undefined;
+	}
+}
+
+export async function uploadProfilePicture(file: Blob | File) {
+	const uploadRes = await generateUrl();
+	if (!uploadRes || !uploadRes.uploadURL) {
+		return {
+			success: false,
+			error: "Couldn't generate upload Url"
+		};
+	}
+	const formData = new FormData();
+	formData.append('file', file);
+	const res = await fetch(uploadRes.uploadURL, {
+		method: 'POST',
+		headers: {
+			Authorization: `Bearer ${import.meta.env.VITE_CF_STREAM_API}`
+		},
+		body: formData
+	});
+	const body = await res.json();
+	console.log('uploaded image', body);
+}
