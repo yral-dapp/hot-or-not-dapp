33ec20d417d3728eec752adaccd6866f8413e593
Harsh Mandan <harsh@gobazzinga.io>
2022-09-05 14:08:31 +0530

    Add image upload url generator url

---
+++ packages/workers/video/getImageUploadURL.js
@@ -0,0 +1,48 @@
+const handler = async (request) => {
+	try {
+		const requestBody = await request.json();
+
+		if (!requestBody.principalId) {
+			return new Response('Bad Request', {
+				status: 400,
+				headers: { ...request.corsHeaders }
+			});
+		}
+
+		const cloudflareGenerateUploadURLResponse = await (
+			await fetch(
+				// eslint-disable-next-line no-undef
+				`https://api.cloudflare.com/client/v4/accounts/${CLOUDFLARE_ACCOUNT_ID}/images/v2/direct_upload`,
+				{
+					method: 'POST',
+					headers: {
+						// eslint-disable-next-line no-undef
+						Authorization: `Bearer ${CLOUDFLARE_API_TOKEN}`
+					},
+					body: JSON.stringify({
+						meta: {
+							creator: requestBody.principalId,
+							uploadType: 'profile-image'
+						}
+					})
+				}
+			)
+		).json();
+
+		return new Response(JSON.stringify(cloudflareGenerateUploadURLResponse), {
+			status: 200,
+			headers: {
+				'Content-Type': 'application/json',
+				...request.corsHeaders
+			}
+		});
+	} catch (error) {
+		console.error(error);
+		return new Response(JSON.stringify({ error }), {
+			status: 403,
+			headers: { ...request.corsHeaders }
+		});
+	}
+};
+
+export default handler;
