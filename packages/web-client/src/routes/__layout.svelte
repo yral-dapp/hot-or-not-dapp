56f47e97fc29c15e01d79d31ce2598a85fce1e9b
Harsh Mandan <harsh@gobazzinga.io>
2022-09-06 10:49:06 +0530

    Initialize Logrocket

--- packages/web-client/src/routes/__layout.svelte
+++ packages/web-client/src/routes/__layout.svelte
@@ -6,11 +6,13 @@ import { browser } from '$app/env';
 import { initializeAuthClient } from '$lib/authHelper';
 import { auth } from '$stores/auth';
 import Login from '$components/login/Login.svelte';
+import LogRocket from 'logrocket';
 
 onMount(async () => {
 	if (browser) {
 		window.Buffer = Buffer;
 		await initializeAuthClient();
+		LogRocket.init('8n3yhe/hotornot');
 	}
 });
 </script>
