104c4b565cc07304a5f0520bcc54870d38d8a894
Harsh Mandan <harsh@gobazzinga.io>
2022-09-06 12:57:34 +0530

    Add option to reset all fields

--- packages/web-client/src/routes/profile/[id]/edit.svelte
+++ packages/web-client/src/routes/profile/[id]/edit.svelte
@@ -5,6 +5,14 @@ import ProfileLayout from '$components/layout/ProfileLayout.svelte';
 import ProfileImageSelector from '$components/profile/ProfileImageSelector.svelte';
 import Input from '$components/input/Input.svelte';
 import Button from '$components/button/Button.svelte';
+
+let src = '';
+let name = '';
+let username = '';
+
+function resetAllFields() {
+	src = name = username = '';
+}
 </script>
 
 <ProfileLayout>
@@ -20,10 +28,11 @@ import Button from '$components/button/Button.svelte';
 		<div
 			class="flex h-full w-full flex-col items-center justify-start space-y-8 overflow-y-auto p-8"
 		>
-			<ProfileImageSelector />
+			<ProfileImageSelector bind:src />
 			<div class="flex w-full flex-col space-y-2">
 				<span class="text-white/60">Your name</span>
 				<Input
+					bind:value="{name}"
 					type="text"
 					placeholder="Enter your name here"
 					class="w-full rounded-md bg-white/10 py-4"
@@ -32,13 +41,14 @@ import Button from '$components/button/Button.svelte';
 			<div class="flex w-full flex-col space-y-2">
 				<span class="text-white/60">Username</span>
 				<Input
+					bind:value="{username}"
 					type="text"
 					placeholder="Enter your username here"
 					class="w-full rounded-md bg-white/10 py-4"
 				/>
 			</div>
 			<div class="flex w-full items-center justify-between space-x-4 pt-16">
-				<Button type="secondary" class="w-full flex-1">Reset</Button>
+				<Button on:click="{resetAllFields}" type="secondary" class="w-full flex-1">Reset</Button>
 				<Button prefetch class="w-full flex-1">Save changes</Button>
 			</div>
 		</div>
