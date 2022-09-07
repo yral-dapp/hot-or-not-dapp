5f91b7fe2c6518dc4a2cab4022e2975cec9362e6
Harsh Mandan <harsh@gobazzinga.io>
2022-09-07 16:21:39 +0530

    Add Icons

---
+++ packages/web-client/src/components/icons/ChevronUpIcon.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+export { exportClass as class };
+let exportClass: any = '';
+</script>
+
+<svg class="{exportClass}" viewBox="0 0 14 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M1 7.33333L7 2L13 7.33333" stroke="white" stroke-width="1.5" stroke-linecap="round"
+	></path>
+</svg>
