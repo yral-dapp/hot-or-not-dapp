97d6b38f4c59428ca26f7aa653447b5812abb478
Harsh Mandan <harsh@gobazzinga.io>
2022-09-07 15:30:36 +0530

    Create a separate layout for hot or not

---
+++ packages/web-client/src/routes/__layout-hotornot@default.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { page } from '$app/stores';
+import HomeLayout from '$components/layout/HomeLayout.svelte';
+import Selector from '$components/home/Selector.svelte';
+import HotOrNot from '$components/navigation/HotOrNot.svelte';
+</script>
+
+<HomeLayout>
+	<svelte:fragment slot="top">
+		<Selector selected="hot-or-not" />
+	</svelte:fragment>
+	<svelte:fragment slot="content">
+		<slot />
+	</svelte:fragment>
+	<div class="w-full" slot="bottom-navigation">
+		<HotOrNot />
+	</div>
+</HomeLayout>
