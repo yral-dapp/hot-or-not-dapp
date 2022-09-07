289c35bae8b07bff3c6a6fe5cb9040cb7bb7a21d
Harsh Mandan <harsh@gobazzinga.io>
2022-09-07 15:51:37 +0530

    Remove show on path

--- packages/web-client/src/components/layout/SplashScreen.svelte
+++ packages/web-client/src/components/layout/SplashScreen.svelte
@@ -3,16 +3,14 @@ import { fade } from 'svelte/transition';
 import { showSplashScreen } from '$stores/splashScreen';
 import FireIcon from '$components/icons/FireIcon.svelte';
 
-export let showOnPath = false;
-
-$: if ($showSplashScreen && showOnPath) {
+$: if ($showSplashScreen) {
 	setTimeout(() => {
 		$showSplashScreen = false;
 	}, 2000);
 }
 </script>
 
-{#if $showSplashScreen && showOnPath}
+{#if $showSplashScreen}
 	<splash-screen
 		out:fade|local="{{ duration: 500 }}"
 		class="absolute z-[15] flex h-full w-full items-center justify-center bg-primary text-9xl"
