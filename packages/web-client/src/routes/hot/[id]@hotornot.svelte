d2f132b9be8019bc2a0126e4bda26dcf07b9a91b
Harsh Mandan <harsh@gobazzinga.io>
2022-09-07 15:30:51 +0530

    Add route for hot or not feed

---
+++ packages/web-client/src/routes/hot/[id]@hotornot.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+import { page } from '$app/stores';
+import SplashScreen from '$components/layout/SplashScreen.svelte';
+import VideosSwiper from '$components/video/VideosSwiper.svelte';
+
+let startingVideoIndex = Number($page.params.id) ?? 0;
+</script>
+
+<SplashScreen showOnPath="{$page.url.pathname.includes('hot')}" />
