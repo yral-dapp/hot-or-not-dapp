f3f37ec32037dc48b585d781e9ddae85845467dd
harshbaz <106158714+harshbaz@users.noreply.github.com>
2022-09-08 10:37:25 +0530

    Update logrocket (#377)
    * Remove Logrocket npm module
    
    * Use logorocket in app.html

--- packages/web-client/src/routes/__layout.svelte
+++ packages/web-client/src/routes/__layout.svelte
@@ -6,14 +6,11 @@ import { browser } from '$app/env';
 import { initializeAuthClient } from '$lib/authHelper';
 import { auth } from '$stores/auth';
 import Login from '$components/login/Login.svelte';
-import LogRocket from 'logrocket';
 
 onMount(async () => {
 	if (browser) {
 		window.Buffer = Buffer;
 		await initializeAuthClient();
-		LogRocket.init('8n3yhe/hotornot');
-		LogRocket.identify(navigator.platform, { name: 'Tester' });
 	}
 });
 </script>
