5e16ec1f87ded1f764fedfe9a2eef478776ed09d
harshbaz <106158714+harshbaz@users.noreply.github.com>
2022-09-08 17:53:29 +0530

    Add hot or not onboarding screen (#385)
    * Add decor assets
    
    * Remove unused placeholder asset
    
    * Add starts Icon
    
    * Add store for hot or not onboarding
    
    * Create hot or not onboarding page
    
    * Cleanup
    
    * Remove unused placeholder image
    
    * Show hot or not onboarding

---
+++ packages/web-client/src/components/popup/HotorNotOnboarding.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+import { fade } from 'svelte/transition';
+import { showOnboarding } from '$stores/hotOrNotOnboarding';
+import left from '$assets/decore-left.png';
+import right from '$assets/decore-right.png';
+import StarsIcon from '$components/icons/StarsIcon.svelte';
+import Button from '$components/button/Button.svelte';
+</script>
+
+{#if $showOnboarding}
+	<onboarding
+		out:fade|local="{{ duration: 500 }}"
+		class="absolute z-[15] flex h-full w-full flex-col items-center justify-center space-y-32 bg-black/90 text-white"
+	>
+		<div class="pointer-events-none absolute top-0 flex w-full justify-between">
+			<img alt="decore" src="{left}" class="select-none" />
+			<img alt="decore" src="{right}" class="select-none" />
+		</div>
+		<div class="relative select-none text-center text-3xl font-bold ">
+			<StarsIcon class="absolute -left-10 -top-12 h-24" />
+			A new experience<br />awaits you
+		</div>
+		<div class="flex w-full flex-col space-y-6 px-8">
+			<Button>Start Tutorial</Button>
+			<button on:click="{() => ($showOnboarding = false)}" class="font-medium">Maybe Later</button>
+		</div>
+	</onboarding>
+{/if}
--- packages/web-client/src/routes/__layout-hotornot@default.svelte
+++ packages/web-client/src/routes/__layout-hotornot@default.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 import Selector from '$components/home/Selector.svelte';
 import HotOrNotLayout from '$components/layout/HotOrNotLayout.svelte';
+import HotorNotOnboarding from '$components/popup/HotorNotOnboarding.svelte';
 </script>
+
+<HotorNotOnboarding />
 
 <HotOrNotLayout>
 	<svelte:fragment slot="top">
