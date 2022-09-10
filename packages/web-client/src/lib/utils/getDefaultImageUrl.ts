f6204fd8b1bf8fadc3a41b15fd607a351e00efeb
Harsh Mandan <harsh@gobazzinga.io>
2022-09-10 15:26:08 +0530

    Add log

--- packages/web-client/src/lib/utils/getDefaultImageUrl.ts
+++ packages/web-client/src/lib/utils/getDefaultImageUrl.ts
@@ -1,4 +1,5 @@
 import { Principal } from '@dfinity/principal';
+import Log from './Log';
 
 const avatars = [
 	'https://i.imgur.com/QlRZXTs.png',
@@ -30,6 +31,7 @@ export default (principal?: Principal | string) => {
 	} else if (principal) {
 		string = principal;
 	}
+	Log({ defaultUrl: string }, 'info');
 	const sum = string.split('').reduce((acc, val) => val.charCodeAt(0) + acc, 0);
 	return avatars[sum % 20];
 };
