06c1c4af2738145b38ee23699a64b05a8f4d53d0
Harsh Mandan <harsh@gobazzinga.io>
2022-09-14 13:43:21 +0530

    Return name and return username

--- packages/web-client/src/lib/utils/randomUsername.ts
+++ packages/web-client/src/lib/utils/randomUsername.ts
@@ -4,14 +4,14 @@ export function generateRandomNumber(length: number): string {
 		.padEnd(length, '0');
 }
 
-export function generateUsername() {
+export function generateRandomName(type: 'name' | 'username') {
 	const n1 = ['Blue ', 'Green', 'Red', 'Orange', 'Violet', 'Indigo', 'Yellow '];
 	const n2 = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Zero'];
-	return (
-		n1[Math.floor(Math.random() * n1.length)] +
-		'-' +
-		n2[Math.floor(Math.random() * n2.length)] +
-		'-' +
-		generateRandomNumber(4)
-	);
+	const r1 = n1[Math.floor(Math.random() * n1.length)];
+	const r2 = n2[Math.floor(Math.random() * n2.length)];
+	if (type === 'name') {
+		return `${r1} ${r2}`;
+	} else {
+		return `${r1.toLowerCase()}-${r2.toLowerCase()}-${generateRandomNumber(4)}`;
+	}
 }
