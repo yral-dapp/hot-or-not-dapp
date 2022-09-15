bc00fc6e492af0129e490b594001310ca55328d4
Harsh Mandan <harsh@gobazzinga.io>
2022-09-15 14:06:01 +0530

    Update declarations

--- scripts/packages/web-client/declarations/user_index/user_index.did.d.ts
+++ scripts/packages/web-client/declarations/user_index/user_index.did.d.ts
@@ -6,8 +6,13 @@ export type UserAccessRole = { 'CanisterController' : null } |
   { 'CanisterAdmin' : null };
 export interface _SERVICE {
   'delete_user_index_reset_user_canisters' : ActorMethod<[], undefined>,
+  'get_index_details_is_user_name_taken' : ActorMethod<[string], boolean>,
   'get_user_canister_id_from_user_principal_id' : ActorMethod<[], Principal>,
   'get_user_roles' : ActorMethod<[Principal], Array<UserAccessRole>>,
+  'update_index_with_unique_user_name_corresponding_to_user_principal_id' : ActorMethod<
+    [string],
+    undefined,
+  >,
   'update_user_add_role' : ActorMethod<[UserAccessRole, Principal], undefined>,
   'update_user_remove_role' : ActorMethod<
     [UserAccessRole, Principal],
