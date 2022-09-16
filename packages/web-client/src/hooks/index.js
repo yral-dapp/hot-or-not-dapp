39072f5193abf6931901c29f1d32f081fb63a43b
Harsh Mandan <harsh@gobazzinga.io>
2022-09-16 13:31:13 +0530

    Remove hooks

--- packages/web-client/src/hooks/index.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('@sveltejs/kit').Handle} */
-export async function handle({ event, resolve }) {
-	const response = await resolve(event, {
-		ssr: import.meta.env.ENABLE_SSR
-	});
-
-	return response;
-}
