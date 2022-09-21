5c2ccc852233465a267cc285657571dbe8eeebe2
Harsh Mandan <harsh@gobazzinga.io>
2022-09-21 16:52:14 +0530

    Update profile store

--- packages/web-client/src/lib/helpers/profile.ts
+++ packages/web-client/src/lib/helpers/profile.ts
@@ -1,6 +1,10 @@
 import type { UserProfile } from '$canisters/individual_user_template/individual_user_template.did';
+import getDefaultImageUrl from '$lib/utils/getDefaultImageUrl';
 import Log from '$lib/utils/Log';
+import { generateRandomName } from '$lib/utils/randomUsername';
+import { authState } from '$stores/auth';
 import userProfile from '$stores/userProfile';
+import { get } from 'svelte/store';
 
 async function fetchProfile() {
 	const { individualUser } = await import('./backend');
@@ -19,12 +23,22 @@ export async function updateProfile(profile?: UserProfile) {
 		updateProfile = await fetchProfile();
 	}
 	if (updateProfile) {
+		const authStateData = get(authState);
 		userProfile.set({
-			...updateProfile,
+			username_set: !!updateProfile.unique_user_name[0],
+			unique_user_name:
+				updateProfile.unique_user_name[0] || generateRandomName('username', authStateData.idString),
+			profile_picture_url:
+				updateProfile.profile_picture_url[0] || getDefaultImageUrl(authStateData.idString),
+			display_name:
+				updateProfile.display_name[0] || generateRandomName('name', authStateData.idString),
+			principal_id: updateProfile.principal_id[0],
+			followers: updateProfile.followers,
+			following: updateProfile.following,
 			profile_stats: {
-				lifetime_earnings: Number(updateProfile.profile_stats.lifetime_earnings),
-				hots_earned_count: Number(updateProfile.profile_stats.hots_earned_count),
-				nots_earned_count: Number(updateProfile.profile_stats.nots_earned_count)
+				hots_earned_count: Number(updateProfile.profile_stats.hots_earned_count) || 0,
+				lifetime_earnings: Number(updateProfile.profile_stats.hots_earned_count) || 0,
+				nots_earned_count: Number(updateProfile.profile_stats.hots_earned_count) || 0
 			},
 			updated_at: Date.now()
 		});
