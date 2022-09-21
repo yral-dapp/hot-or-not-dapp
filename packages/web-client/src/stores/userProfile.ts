6480d9690ec2e6e0b40cc74f1ed591bac98f6cba
Harsh Mandan <harsh@gobazzinga.io>
2022-09-21 16:53:24 +0530

    Update userProfile helper

--- packages/web-client/src/stores/userProfile.ts
+++ packages/web-client/src/stores/userProfile.ts
@@ -1,10 +1,11 @@
 import type { Principal } from '@dfinity/principal';
 import { writable } from 'svelte-local-storage-store';
 
-export default writable<{
-	unique_user_name: [] | [string];
-	profile_picture_url: [] | [string];
-	display_name: [] | [string];
+export type UserProfile = {
+	username_set: boolean;
+	unique_user_name: string;
+	profile_picture_url: string;
+	display_name: string;
 	principal_id?: Principal;
 	followers: Principal[];
 	following: Principal[];
@@ -14,10 +15,13 @@ export default writable<{
 		nots_earned_count: number;
 	};
 	updated_at: number;
-}>('user-profile', {
-	unique_user_name: [],
-	profile_picture_url: [],
-	display_name: [],
+};
+
+export default writable<UserProfile>('user-profile', {
+	username_set: false,
+	unique_user_name: '',
+	profile_picture_url: '',
+	display_name: '',
 	followers: [],
 	following: [],
 	profile_stats: {
