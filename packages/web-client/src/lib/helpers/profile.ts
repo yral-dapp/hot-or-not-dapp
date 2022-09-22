a0e6381368292ded4235b42e0cdabff62d55bc47
Harsh Mandan <harsh@gobazzinga.io>
2022-09-22 12:22:52 +0530

    Separate function to generate sanitized profile

--- packages/web-client/src/lib/helpers/profile.ts
+++ packages/web-client/src/lib/helpers/profile.ts
@@ -1,9 +1,9 @@
-import type { UserProfile } from '$canisters/individual_user_template/individual_user_template.did';
+import type { UserProfile as ServerUserProfile } from '$canisters/individual_user_template/individual_user_template.did';
 import getDefaultImageUrl from '$lib/utils/getDefaultImageUrl';
 import Log from '$lib/utils/Log';
 import { generateRandomName } from '$lib/utils/randomUsername';
 import { authState } from '$stores/auth';
-import userProfile from '$stores/userProfile';
+import userProfile, { type UserProfile } from '$stores/userProfile';
 import { get } from 'svelte/store';
 
 async function fetchProfile() {
@@ -15,8 +15,26 @@ async function fetchProfile() {
 	}
 }
 
-export async function updateProfile(profile?: UserProfile) {
-	let updateProfile: UserProfile | undefined = undefined;
+export function sanitizeProfile(profile: ServerUserProfile, userId: string): UserProfile {
+	return {
+		username_set: !!profile.unique_user_name[0],
+		unique_user_name: profile.unique_user_name[0] || generateRandomName('username', userId),
+		profile_picture_url: profile.profile_picture_url[0] || getDefaultImageUrl(userId),
+		display_name: profile.display_name[0] || generateRandomName('name', userId),
+		principal_id: profile.principal_id,
+		followers: profile.followers,
+		following: profile.following,
+		profile_stats: {
+			hots_earned_count: Number(profile.profile_stats.hots_earned_count) || 0,
+			lifetime_earnings: Number(profile.profile_stats.hots_earned_count) || 0,
+			nots_earned_count: Number(profile.profile_stats.hots_earned_count) || 0
+		},
+		updated_at: Date.now()
+	};
+}
+
+export async function updateProfile(profile?: ServerUserProfile) {
+	let updateProfile: ServerUserProfile | undefined = undefined;
 	if (profile) {
 		updateProfile = profile;
 	} else {
@@ -25,23 +43,9 @@ export async function updateProfile(profile?: UserProfile) {
 	if (updateProfile) {
 		const authStateData = get(authState);
 		userProfile.set({
-			username_set: !!updateProfile.unique_user_name[0],
-			unique_user_name:
-				updateProfile.unique_user_name[0] || generateRandomName('username', authStateData.idString),
-			profile_picture_url:
-				updateProfile.profile_picture_url[0] || getDefaultImageUrl(authStateData.idString),
-			display_name:
-				updateProfile.display_name[0] || generateRandomName('name', authStateData.idString),
-			principal_id: updateProfile.principal_id[0],
-			followers: updateProfile.followers,
-			following: updateProfile.following,
-			profile_stats: {
-				hots_earned_count: Number(updateProfile.profile_stats.hots_earned_count) || 0,
-				lifetime_earnings: Number(updateProfile.profile_stats.hots_earned_count) || 0,
-				nots_earned_count: Number(updateProfile.profile_stats.hots_earned_count) || 0
-			},
-			updated_at: Date.now()
+			...sanitizeProfile(updateProfile, authStateData.idString || 'random')
 		});
+		Log({ profile: get(userProfile), from: '0 updateProfile' }, 'info');
 	} else {
 		Log({ error: 'No profile fetched', from: '1 updateProfile' }, 'error');
 	}
