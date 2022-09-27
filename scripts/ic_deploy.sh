869d1370ba82a67408504f0478b290b867c15ec6
Saikat Das <saikatdas0790@gmail.com>
2022-09-27 19:47:44 +0530

    chore: current state

--- scripts/ic_deploy.sh
+++ scripts/ic_deploy.sh
@@ -3,9 +3,12 @@ set -euo pipefail
 
 cargo test
 
+export CANISTER_ID_user_index=$(dfx canister --network=ic id user_index)
+export CANISTER_ID_post_cache=$(dfx canister --network=ic id post_cache)
+
 dfx build --network=ic individual_user_template
 dfx build --network=ic user_index
 dfx build --network=ic post_cache
 
-dfx canister --network=ic install user_index
-dfx canister --network=ic install post_cache
+dfx canister --network=ic install --mode=reinstall user_index
+dfx canister --network=ic install --mode=reinstall post_cache
