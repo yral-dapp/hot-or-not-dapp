fbd3a4a2f49d52878a19749a190b78f301b7d8a0
harshbaz <106158714+harshbaz@users.noreply.github.com>
2022-09-27 17:38:11 +0530

    Add coming soon pages (#602)
    * Add coming soon icon
    
    * Add coming soon component
    
    * Add wallet and leader board routes

---
+++ packages/web-client/src/components/coming-soon/ComingSoon.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+import ComingSoonIcon from '$components/icons/ComingSoonIcon.svelte';
+</script>
+
+<div class="flex h-full w-full flex-col items-center justify-center space-y-2">
+	<ComingSoonIcon class="h-36 w-36" />
+	<span>Coming Soon</span>
+</div>
