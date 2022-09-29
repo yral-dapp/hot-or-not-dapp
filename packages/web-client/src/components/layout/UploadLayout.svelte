3bc4af88e67cf7831928548d8c39704eaff92b40
Harsh Mandan <harsh@gobazzinga.io>
2022-09-29 18:38:48 +0530

    Cleanup UploadLayout

--- packages/web-client/src/components/layout/UploadLayout.svelte
+++ packages/web-client/src/components/layout/UploadLayout.svelte
@@ -4,17 +4,13 @@ export let testMode = false;
 $: testClasses = testMode ? 'border-2 border-white/30' : '';
 </script>
 
-<upload-layout class="relative flex h-full w-full flex-col overflow-hidden text-white">
+<upload-layout class="relative h-screen w-full overflow-hidden text-white">
 	<div
-		class="relative z-[2] flex h-12 w-full items-center justify-center bg-black py-8 shadow-xl shadow-black/50 {testClasses}"
-	>
+		class="relative z-[2] flex h-12 w-full items-center justify-center bg-black py-8 shadow-xl shadow-black/50 {testClasses}">
 		<div class="absolute left-4 top-4">
 			<slot name="top-left" />
 		</div>
 		<slot name="top-center" />
 	</div>
 	<slot name="content" />
-	<div class="absolute inset-x-0 bottom-0 z-[2] {testClasses}">
-		<slot name="bottom" />
-	</div>
 </upload-layout>
