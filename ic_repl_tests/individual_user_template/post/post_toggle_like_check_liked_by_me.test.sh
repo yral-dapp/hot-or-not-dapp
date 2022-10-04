5c0454ed97421e2cda74bd84b9819ffa9595819b
Saikat Das <saikatdas0790@gmail.com>
2022-10-04 10:38:03 +0530

    test: add test for liked by me

---
+++ ic_repl_tests/individual_user_template/post/post_toggle_like_check_liked_by_me.test.sh
@@ -0,0 +1,18 @@
+#!/home/saikat/bin/ic-repl -r http://127.0.0.1:8000/
+
+import user_index_canister = "r7inp-6aaaa-aaaaa-aaabq-cai";
+
+identity default;
+
+let my_canister = call user_index_canister.get_user_index_create_if_not_exists_else_return_canister_id_for_embedded_user_principal_id();
+
+let post_id = call my_canister.add_post(record { hashtags = vec { "a"; "b"; "c" }; description = "This is post from integration test"; video_uid = "#1234567890"; creator_consent_for_inclusion_in_hot_or_not = false; });
+
+call my_canister.update_post_toggle_like_status_by_caller(post_id);
+call my_canister.get_individual_post_details_by_id(post_id);
+
+call my_canister.update_post_toggle_like_status_by_caller(post_id);
+call my_canister.get_individual_post_details_by_id(post_id);
+
+call my_canister.update_post_toggle_like_status_by_caller(post_id);
+call my_canister.get_individual_post_details_by_id(post_id);
