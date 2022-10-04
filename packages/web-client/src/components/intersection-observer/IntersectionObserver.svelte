baaea510361fae45dba1a4dd3ebcec58d5b045da
Harsh Mandan <harsh@gobazzinga.io>
2022-10-04 07:50:06 +0530

    Add intersection observer component

---
+++ packages/web-client/src/components/intersection-observer/IntersectionObserver.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+import { createEventDispatcher, onMount } from 'svelte';
+
+export let threshold: number = 1;
+export let intersect: boolean;
+
+const dispatch = createEventDispatcher<{
+	intersected: void;
+}>();
+
+let observer: IntersectionObserver;
+let intersectionWrapperEl: HTMLElement;
+
+function selectListElementAndWatch() {
+	try {
+		const el = intersectionWrapperEl.children[0];
+		console.log('sel el', el);
+		if (el) {
+			observer.observe(el);
+		}
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+function initIntersectionObserver() {
+	observer = new IntersectionObserver(
+		async (entries) => {
+			if (entries[0].isIntersecting) {
+				if (intersect) {
+					dispatch('intersected');
+				} else {
+					observer.disconnect();
+				}
+			}
+		},
+		{ threshold }
+	);
+}
+
+onMount(() => {
+	initIntersectionObserver();
+	selectListElementAndWatch();
+});
+</script>
+
+<intersection-observer bind:this="{intersectionWrapperEl}">
+	<slot />
+</intersection-observer>
