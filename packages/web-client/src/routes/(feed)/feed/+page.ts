b143eb321f42c36f32aacc66fd697baff30e66e8
Harsh Mandan <harsh@gobazzinga.io>
2022-10-05 17:53:40 +0530

    Update message

--- packages/web-client/src/routes/(feed)/feed/+page.ts
+++ packages/web-client/src/routes/(feed)/feed/+page.ts
@@ -5,7 +5,7 @@ export async function load() {
 	const res = await getTopPosts(0, 1);
 	console.log('res', res);
 	if (res.error || !res.posts[0]) {
-		throw redirect(307, '/feed/invalid-video');
+		throw redirect(307, '/feed/no-videos');
 	}
 	throw redirect(307, `/feed/${res.posts[0].publisher_canister_id}:${res.posts[0].post_id}`);
 	// throw redirect(307, '/feed/1');
