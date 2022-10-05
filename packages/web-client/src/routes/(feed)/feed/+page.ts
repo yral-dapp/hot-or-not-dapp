b8cb655a33247c3ff710a39f7553de5caa35c47a
Harsh Mandan <harsh@gobazzinga.io>
2022-10-05 17:27:39 +0530

    Add redirect to the first post

--- packages/web-client/src/routes/(feed)/feed/+page.ts
+++ packages/web-client/src/routes/(feed)/feed/+page.ts
@@ -1,5 +1,12 @@
+import { getTopPosts } from '$lib/helpers/feed';
 import { redirect } from '@sveltejs/kit';
 
-export function load() {
-	throw redirect(307, '/feed/1');
+export async function load() {
+	const res = await getTopPosts(0, 1);
+	console.log('res', res);
+	if (res.error || !res.posts[0]) {
+		throw redirect(307, '/feed/invalid-video');
+	}
+	throw redirect(307, `/feed/${res.posts[0].publisher_canister_id}:${res.posts[0].post_id}`);
+	// throw redirect(307, '/feed/1');
 }
