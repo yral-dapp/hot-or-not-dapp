2fc67d9d37689beca925c9d16a3b3918e852192f
harshbaz <106158714+harshbaz@users.noreply.github.com>
2022-10-06 17:40:53 +0530

    Use @ as a delimiter for video url (#683)
    Use @ as a delimiter

--- packages/web-client/src/params/videoId.ts
+++ packages/web-client/src/params/videoId.ts
@@ -1,10 +1,10 @@
 import { isPrincipal } from '$lib/helpers/idb';
 import type { ParamMatcher } from '@sveltejs/kit';
 export const match: ParamMatcher = (param: string) => {
-	if (!param.includes(':')) {
+	if (!param.includes('@')) {
 		return false;
 	}
-	const idArr = param.split(':');
+	const idArr = param.split('@');
 	if (idArr.length != 2 && !isPrincipal(idArr[0]) && isNaN(Number(idArr[1]))) {
 		return false;
 	}
