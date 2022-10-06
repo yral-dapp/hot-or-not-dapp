16a7758a57713e44c887c087e3dc5202617d560c
Harsh Mandan <harsh@gobazzinga.io>
2022-10-06 16:40:18 +0530

    Disable ssr when no id is present

--- packages/web-client/src/routes/(feed)/feed/+page.ts
+++ packages/web-client/src/routes/(feed)/feed/+page.ts
@@ -1,12 +1,11 @@
-import { getTopPosts } from '$lib/helpers/feed';
+export const ssr = false;
 import { redirect } from '@sveltejs/kit';
 
 export async function load() {
+	const { getTopPosts } = await import('$lib/helpers/feed');
 	const res = await getTopPosts(0, 1);
-	console.log('res', res);
 	if (res.error || !res.posts[0]) {
 		throw redirect(307, '/feed/no-videos');
 	}
 	throw redirect(307, `/feed/${res.posts[0].publisher_canister_id}:${res.posts[0].post_id}`);
-	// throw redirect(307, '/feed/1');
 }
