5771154ac9aa701ef9757fe28ab7485148837e30
Harsh Mandan <harsh@gobazzinga.io>
2022-10-06 17:49:34 +0530

    Cleanup and update fns

--- packages/web-client/src/routes/(feed)/feed/[id=videoId]/+page.svelte
+++ packages/web-client/src/routes/(feed)/feed/[id=videoId]/+page.svelte
@@ -89,7 +89,12 @@ async function handleChange(e: CustomEvent) {
 	updateStats(currentPlayingIndex);
 	playVideo(index);
 	fetchNextVideos();
-	updateURL();
+	updateURL(videos[currentVideoIndex]);
+	updateMetadata(videos[currentVideoIndex]);
+}
+
+function updateMetadata(video?: PostPopulated) {
+	if (!video) return;
 }
 
 const playVideo = debounce(50, async (index: number) => {
@@ -99,13 +104,11 @@ const playVideo = debounce(50, async (index: number) => {
 	currentPlayingIndex = index;
 });
 
-function updateURL() {
-	if (videos[currentVideoIndex]) {
-		const url =
-			videos[currentVideoIndex].publisher_canister_id + '@' + videos[currentVideoIndex].post_id;
-		$playerState.currentVideoUrl = url;
-		window.history.replaceState('', '', url);
-	}
+function updateURL(video?: PostPopulated) {
+	if (!video) return;
+	const url = video.publisher_canister_id + '@' + video.post_id;
+	$playerState.currentVideoUrl = url;
+	window.history.replaceState('', '', url);
 }
 
 function recordStats(progress: number, canisterId: Principal, videoId: bigint) {
