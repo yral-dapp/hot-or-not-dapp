e69b6186bc02a53351649e037c5194374921c1ec
Saikat Das <saikatdas0790@gmail.com>
2022-10-07 20:54:33 +0530

    Put well known canister IDs in shared utils and abstract away env variables and constants Fixes #694

--- packages/canisters/individual_user_template/src/access_control/util.rs
+++ packages/canisters/individual_user_template/src/access_control/util.rs
@@ -1,6 +1,9 @@
 use candid::Principal;
 use ic_stable_memory::{collections::hash_map::SHashMap, utils::ic_types::SPrincipal};
-use shared_utils::access_control::{get_global_sprincipal, UserAccessRole};
+use shared_utils::{
+    access_control::UserAccessRole,
+    constant::{get_global_owner_principal_id, get_post_cache_canister_principal_id},
+};
 
 pub fn setup_initial_access_control(
     user_id_access_control_map: &mut SHashMap<SPrincipal, Vec<UserAccessRole>>,
@@ -9,7 +12,7 @@ pub fn setup_initial_access_control(
 ) {
     // * add global owner
     user_id_access_control_map.insert(
-        get_global_sprincipal(),
+        SPrincipal(get_global_owner_principal_id()),
         &vec![
             UserAccessRole::CanisterController,
             UserAccessRole::CanisterAdmin,
@@ -34,7 +37,7 @@ pub fn setup_initial_access_control(
 
     // * add post_cache canister as a project sibling canister
     user_id_access_control_map.insert(
-        SPrincipal(Principal::from_text(option_env!("CANISTER_ID_post_cache").unwrap()).unwrap()),
+        SPrincipal(get_post_cache_canister_principal_id()),
         &vec![UserAccessRole::ProjectCanister],
     );
 }
