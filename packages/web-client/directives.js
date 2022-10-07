566969423ed04e4d01db2d2da406bcf0525f536e
Harsh Mandan <harsh@gobazzinga.io>
2022-10-07 17:45:35 +0530

    Update directives

--- packages/web-client/directives.js
+++ packages/web-client/directives.js
@@ -1,57 +1,36 @@
-const rootDomain = process.env.VITE_DOMAIN;
-
+/** @type {import("@sveltejs/kit/types/private").CspDirectives} */
 const cspDirectives = {
-	'base-uri': ["'self'"],
-	'child-src': ["'self'"],
-	'connect-src': ["'self'", 'ws://localhost:*'],
-	// 'connect-src': ["'self'", 'ws://localhost:*', 'https://hcaptcha.com', 'https://*.hcaptcha.com'],
-	'img-src': ["'self'", 'data:'],
-	'font-src': ["'self'", 'data:'],
-	'form-action': ["'self'"],
-	'frame-ancestors': ["'self'"],
-	'frame-src': [
-		"'self'"
-		// "https://*.stripe.com",
-		// "https://*.facebook.com",
-		// "https://*.facebook.net",
-		// 'https://hcaptcha.com',
-		// 'https://*.hcaptcha.com',
-	],
-	'manifest-src': ["'self'"],
-	'media-src': ["'self'", 'data:'],
-	'object-src': ["'none'"],
-	'style-src': ["'self'", "'unsafe-inline'"],
-	// 'style-src': ["'self'", "'unsafe-inline'", 'https://hcaptcha.com', 'https://*.hcaptcha.com'],
-	'default-src': [
-		'self',
-		...(rootDomain ? [rootDomain, `ws://${rootDomain}`] : [])
-		// 'https://*.google.com',
-		// 'https://*.googleapis.com',
-		// 'https://*.firebase.com',
-		// 'https://*.gstatic.com',
-		// 'https://*.cloudfunctions.net',
-		// 'https://*.algolia.net',
-		// 'https://*.facebook.com',
-		// 'https://*.facebook.net',
-		// 'https://*.stripe.com',
-		// 'https://*.sentry.io',
-	],
 	'script-src': [
-		'self'
-		// 'https://*.stripe.com',
-		// 'https://*.facebook.com',
-		// 'https://*.facebook.net',
-		// 'https://hcaptcha.com',
-		// 'https://*.hcaptcha.com',
-		// 'https://*.sentry.io',
-		// 'https://polyfill.io',
+		'self',
+		'report-sample',
+		'https://*.lr-in-prod.com',
+		'https://*.googletagmanager.com',
+		'wasm-eval'
 	],
-	'worker-src': ["'self'"],
-	// remove report-to & report-uri if you do not want to use Sentry reporting
-	'report-to': ["'csp-endpoint'"],
-	'report-uri': [
-		`https://sentry.io/api/${process.env.VITE_SENTRY_PROJECT_ID}/security/?sentry_key=${process.env.VITE_SENTRY_KEY}`
-	]
+	'worker-src': ['self', 'data:', 'blob:'],
+	'connect-src': [
+		'self',
+		'ws://localhost:*',
+		'https://ic0.app',
+		'https://*.lr-in-prod.com',
+		'https://*.google-analytics.com'
+	],
+	'img-src': [
+		'self',
+		'https://customer-2p3jflss4r4hmpnz.cloudflarestream.com',
+		'https://imagedelivery.net',
+		'https://images.unsplash.com'
+	],
+	'media-src': ['self', 'data:', 'https://customer-2p3jflss4r4hmpnz.cloudflarestream.com'],
+	'font-src': ['self', 'data:', 'https://fonts.gstatic.com'],
+	'style-src': ['self', 'unsafe-inline', 'https://fonts.googleapis.com'],
+	'default-src': ['self'],
+	'base-uri': ['self'],
+	'child-src': ['self'],
+	'form-action': ['self'],
+	'frame-src': ['self'],
+	'manifest-src': ['self'],
+	'object-src': ['none']
 };
 
 export default cspDirectives;
