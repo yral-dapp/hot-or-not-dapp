69fc2cbee805ed1d9ebf6f30ddd232241981fd97
Harsh Mandan <harsh@gobazzinga.io>
2022-10-08 17:57:20 +0530

    Update test

--- packages/web-client/tests/login.spec.ts
+++ packages/web-client/tests/login.spec.ts
@@ -1,10 +1,15 @@
 import { test, expect } from '@playwright/test';
 
 test('Menu page loads', async ({ page }) => {
-	await page.goto('http://localhost:5173');
+	await page.goto('http://localhost:5173/menu');
 
 	// Expect a title "to contain" a substring.
 	await expect(page).toHaveTitle(/Hot or Not/);
+
+	await page.evaluate(() => {
+		// wait for 5 seconds for AgentJs to initialize
+		return new Promise((resolve) => setTimeout(resolve, 5000));
+	});
 
 	// create a locator
 	const loginBtn = page.locator('text=Login');
@@ -12,6 +17,11 @@ test('Menu page loads', async ({ page }) => {
 	// Click the get started link.
 	await loginBtn.click();
 
+	await page.evaluate(() => {
+		// wait for modal animation to finish
+		return new Promise((resolve) => setTimeout(resolve, 1500));
+	});
+
 	// Expects the URL to contain intro.
-	await expect(page).toContain('Join Hot or Not');
+	await expect(page.getByText('Join Hot or Not')).toBeVisible();
 });
