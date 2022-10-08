d1c6d8be4d81f4f181afbc440e27e0618efde456
Saikat Das <saikatdas0790@gmail.com>
2022-10-08 15:31:39 +0530

    refactor: create post_cache local deploy script

--- scripts/canisters/local_deploy/post_cache.sh
+++ scripts/canisters/local_deploy/post_cache.sh
@@ -3,10 +3,8 @@ set -euo pipefail
 
 cargo test
 
-dfx deploy --no-wallet internet_identity
 export GLOBAL_OWNER_PRINCIPAL_ID=$(dfx identity get-principal)
 
-dfx canister create --no-wallet individual_user_template
 dfx canister create --no-wallet user_index
 dfx canister create --no-wallet post_cache
 
@@ -14,13 +12,8 @@ export CANISTER_ID_user_index=$(dfx canister id user_index)
 export CANISTER_ID_post_cache=$(dfx canister id post_cache)
 export LOCAL_TOP_POSTS_SYNC_INTERVAL="10000000000"
 
-dfx build individual_user_template
-dfx build user_index
 dfx build post_cache
 
-dfx canister install user_index
-dfx canister install post_cache
+dfx canister install post_cache --mode upgrade
 
-dfx generate individual_user_template
-dfx generate user_index
 dfx generate post_cache
