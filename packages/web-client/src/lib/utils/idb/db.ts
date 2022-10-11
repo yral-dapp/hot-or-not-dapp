9a7cfad168aa79e29310cfd8e8d5d9adf39f8789
Harsh Mandan <harsh@gobazzinga.io>
2022-10-11 19:24:17 +0530

    Bump version

--- packages/web-client/src/lib/utils/idb/db.ts
+++ packages/web-client/src/lib/utils/idb/db.ts
@@ -1,6 +1,6 @@
 import { openDB } from 'idb';
 
-export const dbPromise = openDB('keyval-store', 1, {
+export const dbPromise = openDB('keyval-store', 2, {
 	upgrade(db) {
 		db.createObjectStore('keyval');
 		db.createObjectStore('watch');
