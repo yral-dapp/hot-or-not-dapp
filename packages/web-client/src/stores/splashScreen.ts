34be53aabbd2e3ce5ad301d75bc211938393cd71
Harsh Mandan <harsh@gobazzinga.io>
2022-10-12 17:13:19 +0530

    Export helper function

--- packages/web-client/src/stores/splashScreen.ts
+++ packages/web-client/src/stores/splashScreen.ts
@@ -1,3 +1,9 @@
 import { writable } from 'svelte/store';
 
 export const showSplashScreen = writable<boolean>(true);
+
+export function hideSplashScreen() {
+	setTimeout(() => {
+		showSplashScreen.set(false);
+	}, 2000);
+}
