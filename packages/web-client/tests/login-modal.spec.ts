fc4ab9e896bff9d014df64ed000725663169462b
Harsh Mandan <harsh@gobazzinga.io>
2022-10-15 17:00:53 +0530

    Add istanbul coverage report

--- packages/web-client/tests/login-modal.spec.ts
+++ packages/web-client/tests/login-modal.spec.ts
@@ -1,7 +1,10 @@
 import { test, expect } from '@playwright/test';
+import v8toIstanbul from 'v8-to-istanbul';
 
 test('Menu page loads', async ({ page }) => {
 	await page.goto('http://localhost:4173/menu');
+
+	await page.coverage.startJSCoverage();
 
 	// Expect a title "to contain" a substring.
 	await expect(page).toHaveTitle(/Hot or Not/);
@@ -14,4 +17,13 @@ test('Menu page loads', async ({ page }) => {
 	await loginBtn.click();
 
 	await expect(page.getByText('Join Hot or Not')).toBeVisible();
+
+	const coverage = await page.coverage.stopJSCoverage();
+
+	for (const entry of coverage) {
+		const converter = v8toIstanbul('', 0, { source: entry.source ?? '' });
+		await converter.load();
+		converter.applyCoverage(entry.functions);
+		console.log(JSON.stringify(converter.toIstanbul()));
+	}
 });
