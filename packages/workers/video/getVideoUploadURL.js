d20282af1b43a3dc19e4b02e0fa413a317619725
Harsh Mandan <harsh@gobazzinga.io>
2022-10-18 17:31:55 +0530

    Add watermark uids for testing

--- packages/workers/video/getVideoUploadURL.js
+++ packages/workers/video/getVideoUploadURL.js
@@ -1,3 +1,10 @@
+const watermarks = {
+	upperLeft_P15_O20: '81d801d5ae267107463c3675dd2c774f',
+	upperLeft_P10_O10: 'ef2464f8046f87db494bfc34da7bc72f',
+	lowerLeft_P10_O10: '4f7af67fcb07e9a3627ec4f91ce3f85e',
+	lowerRight_P10_O10: 'dcb685a38ef945d2ef48e6307b5e0f0f'
+};
+
 const handler = async (request) => {
 	try {
 		const requestBody = await request.json();
@@ -22,7 +29,7 @@ const handler = async (request) => {
 					body: JSON.stringify({
 						maxDurationSeconds: 60,
 						watermark: {
-							uid: 'abbe92301a12e8964dba43cae85fe24c'
+							uid: watermarks.upperLeft_P15_O20
 						},
 						meta: {
 							creator: requestBody.principalId,
