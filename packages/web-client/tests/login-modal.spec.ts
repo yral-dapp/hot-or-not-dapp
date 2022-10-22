4a8718349fd18d4a005e98055bc3596d90967d89
Harsh Mandan <harsh@gobazzinga.io>
2022-10-22 14:16:54 +0530

    Update modal script

--- packages/web-client/tests/login-modal.spec.ts
+++ packages/web-client/tests/login-modal.spec.ts
@@ -6,7 +6,7 @@ test('Menu page loads', async ({ page }) => {
 	// Expect a title "to contain" a substring.
 	await expect(page).toHaveTitle(/Hot or Not/);
 
-	await page.waitForResponse((res) => res.url().includes('https://ic0.app/api/v2/canister'));
+	await page.waitForResponse((res) => res.url().includes('http://localhost:8000/api/v2/canister'));
 
 	await expect(page.getByText('Join Hot or Not')).toBeHidden();
 
