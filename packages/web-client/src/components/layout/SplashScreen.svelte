c8c9a96a90cac4700da35a6e0fa0a8ff4ba2df93
Harsh Mandan <harsh@gobazzinga.io>
2022-10-27 16:37:46 +0530

    Add loading icon to splash screen

--- packages/web-client/src/components/layout/SplashScreen.svelte
+++ packages/web-client/src/components/layout/SplashScreen.svelte
@@ -2,12 +2,14 @@
 import { fade } from 'svelte/transition';
 import { showSplashScreen } from '$stores/splashScreen';
 import HotorNotIcon from '$components/icons/HotorNotIcon.svelte';
+import LoadingIcon from '$components/icons/LoadingIcon.svelte';
 </script>
 
 {#if $showSplashScreen}
 	<splash-screen
 		out:fade|local="{{ duration: 500 }}"
-		class="absolute z-[15] flex h-full w-full items-center justify-center bg-black">
+		class="spacy-y-8 absolute z-[15] flex h-full w-full flex-col items-center justify-center space-y-10 bg-black">
 		<HotorNotIcon class="h-56" />
+		<LoadingIcon class="h-5 w-5 animate-spin-slow opacity-50" />
 	</splash-screen>
 {/if}
