f863206e0b765ee6a8e0dcf9aa79615b2d8266f5
Harsh Mandan <harsh@gobazzinga.io>
2022-10-27 16:48:02 +0530

    Rename prop

--- packages/web-client/src/routes/profile/[id]/post/[postId]/+page.ts
+++ packages/web-client/src/routes/profile/[id]/post/[postId]/+page.ts
@@ -11,41 +11,34 @@ export const load: PageLoad = async ({ params }) => {
 	if (!browser) return;
 
 	try {
-		const vid = params.vid;
+		const pid = params.postId;
 		const id = params.id;
 		let me = false;
 		console.log('here');
-		if (!vid || isNaN(Number(vid))) {
-			Log({ from: '1 noVid' }, 'warn');
+		if (!pid || isNaN(Number(pid))) {
+			Log({ from: '1 posts/[vid] load', pid, id }, 'warn');
 			throw redirect(307, '/profile');
 		}
 
 		const canId = await getCanisterId(id);
 		if (!canId) {
-			Log({ from: '1 noCanId' }, 'warn');
 			throw redirect(307, '/profile');
 		}
 
-		Log({ canId, from: '0 canId' }, 'info');
-
 		const userProfileData = get(userProfile);
 
-		if (id === userProfileData.unique_user_name || id === userProfileData.principal_id) {
-			me = true;
-		}
+		if (id === userProfileData.unique_user_name || id === userProfileData.principal_id) me = true;
 
 		const individualUser = (await import('$lib/helpers/backend')).individualUser;
 		const video = await individualUser(Principal.from(canId)).get_individual_post_details_by_id(
-			BigInt(vid)
+			BigInt(pid)
 		);
-		console.log('returning', video);
 		if (!video) {
-			console.log('hi');
 			throw redirect(307, '/profile');
 		}
 		return { me, video, publisherId: canId };
 	} catch (e) {
-		console.log('e', e);
+		Log({ from: '1 posts/[vid] load', error: e }, 'warn');
 		throw redirect(307, '/profile');
 	}
 };
