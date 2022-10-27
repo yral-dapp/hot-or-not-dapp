7356937d187d5ca5ec0ef37cb1c1dacf664c69c0
Harsh Mandan <harsh@gobazzinga.io>
2022-10-27 17:01:22 +0530

    Add swiper functionality to posts route

---
+++ packages/web-client/src/routes/profile/[id]/posts/[postId]/+page.ts
@@ -0,0 +1,53 @@
+import { browser } from '$app/environment';
+import { getCanisterId } from '$lib/helpers/canisterId';
+import type { PostPopulated } from '$lib/helpers/feed';
+import Log from '$lib/utils/Log';
+import userProfile from '$stores/userProfile';
+import { Principal } from '@dfinity/principal';
+import { redirect } from '@sveltejs/kit';
+import { get } from 'svelte/store';
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async ({ params }) => {
+	if (!browser) return;
+
+	try {
+		const pid = params.postId;
+		const id = params.id;
+		let me = false;
+		console.log('here');
+		if (!pid || isNaN(Number(pid))) {
+			Log({ from: '1 posts/[vid] load', pid, id }, 'warn');
+			throw redirect(307, '/profile');
+		}
+
+		const canId = await getCanisterId(id);
+		if (!canId) {
+			throw redirect(307, '/profile');
+		}
+
+		const userProfileData = get(userProfile);
+
+		if (id === userProfileData.unique_user_name || id === userProfileData.principal_id) me = true;
+
+		const individualUser = (await import('$lib/helpers/backend')).individualUser;
+		const post = await individualUser(Principal.from(canId)).get_individual_post_details_by_id(
+			BigInt(pid)
+		);
+
+		const video: PostPopulated = {
+			...post,
+			post_id: BigInt(pid),
+			publisher_canister_id: canId,
+			score: 0n,
+			created_by_user_principal_id: post.created_by_user_principal_id.toText()
+		};
+		if (!video) {
+			throw redirect(307, '/profile');
+		}
+		return { me, video };
+	} catch (e) {
+		Log({ from: '1 posts/[vid] load', error: e }, 'warn');
+		throw redirect(307, '/profile');
+	}
+};
