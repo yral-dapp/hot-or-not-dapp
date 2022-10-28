4ade5a063a3c796000e4d9e2a877c7e257a2ceaa
Harsh Mandan <harsh@gobazzinga.io>
2022-10-28 17:10:35 +0530

    Add ic0 test

--- packages/web-client/tests/login-modal-ic0.spec.ts
+++ packages/web-client/tests/login-modal-ic0.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Menu page loads', async ({ page }) => {
-	await page.goto('http://localhost:4173/menu');
+	await page.goto('https://vyatz-hqaaa-aaaam-qauea-cai.raw.ic0.app/menu');
 
 	await page.waitForResponse((res) => res.url().includes('https://ic0.app/api/v2/canister'));
 
