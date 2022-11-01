9031892aa87934a7c202928a7f939499ec875bbd
Harsh Mandan <harsh@gobazzinga.io>
2022-11-01 19:12:37 +0700

    Allow bet UI interaction temporarily

--- packages/web-client/src/routes/(feed)/hotornot/+page.svelte
+++ packages/web-client/src/routes/(feed)/hotornot/+page.svelte
@@ -3,10 +3,8 @@ import ComingSoon from '$components/coming-soon/ComingSoon.svelte';
 import HotOrNot from '$components/navigation/HotOrNot.svelte';
 </script>
 
-<ComingSoon />
-
 <div
 	style="-webkit-transform: translate3d(0, 0, 0);"
-	class="pointer-events-none absolute inset-x-0 bottom-12 z-[5] max-h-48 opacity-20">
+	class=" absolute inset-x-0 bottom-12 z-[5] max-h-48">
 	<HotOrNot />
 </div>
