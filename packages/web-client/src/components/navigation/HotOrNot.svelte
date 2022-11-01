f288655138a6de77bb6a3a7bcd6a416b86667726
Harsh Mandan <harsh@gobazzinga.io>
2022-11-01 19:09:31 +0700

    Animate hot or not bet ui

--- packages/web-client/src/components/navigation/HotOrNot.svelte
+++ packages/web-client/src/components/navigation/HotOrNot.svelte
@@ -4,8 +4,12 @@ import BetCoinIcon from '$components/icons/BetCoinIcon.svelte';
 import ChevronUpIcon from '$components/icons/ChevronUpIcon.svelte';
 import HotIcon from '$components/icons/HotIcon.svelte';
 import NotIcon from '$components/icons/NotIcon.svelte';
+import c from 'clsx';
 
 export let tutorialMode = false;
+export let betPlaced: false | 'hot' | 'not' = false;
+
+let coinsBet = 10;
 
 function increaseBet() {
 	if (coinsBet == 10) coinsBet = 50;
@@ -22,11 +26,17 @@ function toggleBet() {
 	else increaseBet();
 }
 
-let coinsBet = 10;
+$: console.log({ betPlaced });
 </script>
 
-<hot-or-not class="flex w-full items-center justify-center space-x-8 px-4">
-	<div class="pointer-events-auto relative flex flex-col items-center space-y-1">
+<hot-or-not class="flex w-full items-center justify-center space-x-8 px-4 transition-all">
+	<div
+		class="{c(
+			'pointer-events-auto relative flex w-14 flex-col items-center space-y-1 transition-all',
+			{
+				'w-0 opacity-0': betPlaced === 'hot'
+			}
+		)}">
 		{#if tutorialMode}
 			<div class="absolute -top-2 z-[-1] h-36 w-36 rounded-full bg-white/10"></div>
 		{/if}
@@ -34,8 +44,8 @@ let coinsBet = 10;
 			disabled="{tutorialMode}"
 			on:click="{(e) => {
 				e.stopImmediatePropagation();
-			}}"
-		>
+				betPlaced = 'not';
+			}}">
 			<NotIcon class="h-24" />
 		</IconButton>
 		<span class="text-sm">Not</span>
@@ -43,16 +53,14 @@ let coinsBet = 10;
 	<div
 		class="relative flex flex-col items-center {tutorialMode
 			? '!pointer-events-none opacity-0'
-			: 'pointer-events-auto'}"
-	>
+			: 'pointer-events-auto'}">
 		<IconButton
 			disabled="{coinsBet == 100}"
 			on:click="{(e) => {
 				e.stopImmediatePropagation();
 				increaseBet();
 			}}"
-			class="z-[10] flex items-center p-4 disabled:opacity-50"
-		>
+			class="{c('z-[10] flex items-center p-4 disabled:opacity-50', { invisible: betPlaced })}">
 			<ChevronUpIcon class="h-2" />
 		</IconButton>
 		<button on:click|stopPropagation="{toggleBet}" class="relative h-20 w-20 select-none">
@@ -60,8 +68,7 @@ let coinsBet = 10;
 			<div class="absolute inset-0 flex select-none items-center justify-center">
 				<span
 					style="text-shadow: 3px 3px 0 #EA9C00;"
-					class="select-none text-3xl font-extrabold text-[#FFCC00]"
-				>
+					class="select-none text-3xl font-extrabold text-[#FFCC00]">
 					{coinsBet}
 				</span>
 			</div>
@@ -72,8 +79,7 @@ let coinsBet = 10;
 				decreaseBet();
 			}}"
 			disabled="{coinsBet == 10}"
-			class="z-[10] flex items-center p-4 disabled:opacity-50"
-		>
+			class="{c('z-[10] flex items-center p-4 disabled:opacity-50', { invisible: betPlaced })}">
 			<ChevronUpIcon class="h-2 rotate-180" />
 		</IconButton>
 	</div>
@@ -85,8 +91,8 @@ let coinsBet = 10;
 			disabled="{tutorialMode}"
 			on:click="{(e) => {
 				e.stopImmediatePropagation();
-			}}"
-		>
+				betPlaced = 'hot';
+			}}">
 			<HotIcon class="h-24" />
 		</IconButton>
 		<span class="text-sm">Hot</span>
