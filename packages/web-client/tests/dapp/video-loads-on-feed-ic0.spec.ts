782693456b18e38bb76153d4ed70f00df5fec210
Harsh Mandan <harsh@gobazzinga.io>
2022-11-01 19:22:11 +0700

    Add test for checking if video loads on feed

--- packages/web-client/tests/dapp/video-loads-on-feed-ic0.spec.ts
+++ packages/web-client/tests/dapp/video-loads-on-feed-ic0.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test('[Web2] Video source is properly loaded on feed', async ({ page }) => {
-	await page.goto('https://hotornot.wtf');
+test('[DApp] Video source is properly loaded on feed', async ({ page }) => {
+	await page.goto('https://vyatz-hqaaa-aaaam-qauea-cai.raw.ic0.app');
 
 	await page.waitForLoadState('networkidle');
 
