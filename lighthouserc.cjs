b110db0ae1d46b61717dee9deed90d6d87ee9f4c
Harsh Mandan <harsh@gobazzinga.io>
2022-11-04 19:15:33 +0700

    Update commands

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -6,7 +6,7 @@ module.exports = {
 				'http://localhost:4173/menu',
 				'http://localhost:4173/profile/iancu'
 			],
-			startServerCommand: 'npm run web-client:preview:static'
+			startServerCommand: 'npm run web-client:preview'
 		},
 		upload: {
 			target: 'filesystem',
