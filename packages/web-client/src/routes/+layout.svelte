dc5e06b37b385dec4d1fa91c2298351184142bfa
Harsh Mandan <harsh@gobazzinga.io>
2022-11-05 15:26:08 +0700

    Deferred imports

--- packages/web-client/src/routes/+layout.svelte
+++ packages/web-client/src/routes/+layout.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 import '../css/app.css';
 import { onMount } from 'svelte';
-import { Buffer } from 'buffer'; // @dfinity/agent requires this
-import { initializeAuthClient } from '$lib/helpers/auth';
 import { authState } from '$stores/auth';
 import Login from '$components/login/Login.svelte';
 import Log from '$lib/utils/Log';
@@ -11,7 +9,6 @@ import navigateBack from '$stores/navigateBack';
 import CornerRibbon from '$components/corner-ribbon/CornerRibbon.svelte';
 import GoogleAnalytics, { registerEvent } from '$components/seo/GoogleAnalytics.svelte';
 import { hideSplashScreen } from '$stores/splashScreen';
-import * as Sentry from '@sentry/svelte';
 import { BrowserTracing } from '@sentry/tracing';
 import userProfile from '$stores/userProfile';
 
@@ -26,20 +23,34 @@ beforeNavigate(({ from, to }) => {
 	$navigateBack = from?.url.pathname ?? null;
 });
 
+async function initClient() {
+	const { Buffer } = await import('buffer'); // @dfinity/agent requires this
+	window.Buffer = Buffer;
+	const { initializeAuthClient } = await import('$lib/helpers/auth');
+	initializeAuthClient();
+}
+
+async function initSentry() {
+	const Sentry = await import('@sentry/svelte');
+
+	if (process.env.NODE_ENV != 'development') {
+		Sentry.init({
+			dsn: 'https://7586a69b01314524b31c8f4f64b41988@o4504076385124352.ingest.sentry.io/4504076386238464',
+			integrations: [new BrowserTracing()],
+			tracesSampleRate: 1.0
+		});
+		Log('Sentry Initialized', 'info');
+	}
+}
+
 onMount(() => {
 	try {
 		hideSplashScreen(10000);
 		$navigateBack = null;
-		window.Buffer = Buffer;
-		if (process.env.NODE_ENV != 'development') {
-			Sentry.init({
-				dsn: 'https://7586a69b01314524b31c8f4f64b41988@o4504076385124352.ingest.sentry.io/4504076386238464',
-				integrations: [new BrowserTracing()],
-				tracesSampleRate: 1.0
-			});
-			Log('Sentry Initialized', 'info');
-		}
-		initializeAuthClient();
+
+		initSentry();
+
+		initClient();
 	} catch (e) {
 		Log({ error: e, source: '0 layout' }, 'error');
 	}
