7b9ea37d2906dbc048c5a58f69a5db0d820c67e4
Harsh Mandan <harsh@gobazzinga.io>
2022-11-08 20:01:15 +0700

    Cleanup

--- packages/web-client/src/routes/+layout.svelte
+++ packages/web-client/src/routes/+layout.svelte
@@ -75,7 +75,7 @@ onMount(() => {
 <GoogleAnalytics />
 
 <alpha-ribbon
-	class="pointer-events-none absolute -right-9 top-2 z-[51] flex w-28 rotate-45 items-center justify-center overflow-hidden bg-primary py-1 px-2 text-xs font-bold uppercase text-white opacity-60">
+	class="pointer-events-none absolute -right-9 top-2 z-[50] flex w-28 rotate-45 items-center justify-center overflow-hidden bg-primary py-1 px-2 text-xs font-bold uppercase text-white opacity-60">
 	Alpha
 </alpha-ribbon>
 
