f86aa3eec735172084ba3ef26fdf029f410515c4
Harsh Mandan <harsh@gobazzinga.io>
2022-11-09 19:07:53 +0700

    Add partytown component

---
+++ packages/web-client/src/components/partytown/PartyTown.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+import { partytownSnippet } from '@builder.io/partytown/integration';
+import GoogleAnalytics from '$components/seo/GoogleAnalytics.svelte';
+import { onMount } from 'svelte';
+
+let scriptEl;
+onMount(() => {
+	if (scriptEl) {
+		scriptEl.textContent = partytownSnippet();
+	}
+});
+</script>
+
+<svelte:head>
+	<script>
+	partytown = {
+		forward: ['dataLayer.push'],
+		resolveUrl: (url) => {
+			const siteUrl = 'https://hotornot.wtf/proxytown';
+
+			if (url.hostname === 'www.googletagmanager.com') {
+				const proxyUrl = new URL(`${siteUrl}/gtm`);
+
+				const gtmId = new URL(url).searchParams.get('id');
+				gtmId && proxyUrl.searchParams.append('id', gtmId);
+				return proxyUrl;
+			}
+
+			return url;
+		}
+	};
+	</script>
+	<script bind:this="{scriptEl}"></script>
+	<script type="text/partytown" src="https://www.googletagmanager.com/gtag/js"></script>
+	<script type="text/partytown">
+	window.dataLayer = window.dataLayer || [];
+
+	function gtag() {
+		dataLayer.push(arguments);
+	}
+	gtag('js', new Date());
+
+	gtag('config', import.meta.env.VITE_GA_TRACKING_ID, {
+		page_path: window.location.pathname
+	});
+	</script>
+</svelte:head>
+
+<GoogleAnalytics />
