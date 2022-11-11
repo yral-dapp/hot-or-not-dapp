10db9e7ab90a92ff99f1ec472f8d10259b376863
Harsh Mandan <harsh@gobazzinga.io>
2022-11-11 12:20:33 +0700

    Cleanup script

--- packages/web-client/src/components/partytown/PartyTown.svelte
+++ packages/web-client/src/components/partytown/PartyTown.svelte
@@ -6,30 +6,27 @@ import { onMount } from 'svelte';
 let scriptEl;
 onMount(() => {
 	if (scriptEl) {
-		scriptEl.textContent = partytownSnippet();
+		scriptEl.textContent = partytownSnippet({
+			forward: ['dataLayer.push'],
+			resolveUrl: (url) => {
+				const siteUrl = 'https://hotornot.wtf';
+
+				if (url.hostname === 'www.googletagmanager.com') {
+					const proxyUrl = new URL(`${siteUrl}/gtm`);
+
+					const gtmId = new URL(url).searchParams.get('id');
+					gtmId && proxyUrl.searchParams.append('id', gtmId);
+					return proxyUrl;
+				}
+
+				return url;
+			}
+		});
 	}
 });
 </script>
 
 <svelte:head>
-	<script>
-	partytown = {
-		forward: ['dataLayer.push'],
-		resolveUrl: (url) => {
-			const siteUrl = 'https://hotornot.wtf/proxytown';
-
-			if (url.hostname === 'www.googletagmanager.com') {
-				const proxyUrl = new URL(`${siteUrl}/gtm`);
-
-				const gtmId = new URL(url).searchParams.get('id');
-				gtmId && proxyUrl.searchParams.append('id', gtmId);
-				return proxyUrl;
-			}
-
-			return url;
-		}
-	};
-	</script>
 	<script bind:this="{scriptEl}"></script>
 	<script type="text/partytown" src="https://www.googletagmanager.com/gtag/js"></script>
 	<script type="text/partytown">
