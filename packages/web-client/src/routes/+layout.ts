14f96b5b0e6e16fcc1da891bf4f5dba0cfcb3db4
Harsh Mandan <harsh@gobazzinga.io>
2022-11-12 17:11:09 +0700

    Revert

--- packages/web-client/src/routes/+layout.ts
+++ packages/web-client/src/routes/+layout.ts
@@ -1,5 +1,4 @@
 export const ssr = import.meta.env.ENABLE_SSR;
-export const prerender = true;
 
 // Polyfill Buffer for development purpose.
 // ⚠️ For production build the polyfill needs to be injected with esbuild (see vite.config.ts) because the page might be loaded before the _layout.js which will contains this polyfill
