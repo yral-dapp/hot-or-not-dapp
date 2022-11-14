992be806bc66ca16a0a9ada9c713cc0632220bc4
Saikat Das <saikatdas0790@gmail.com>
2022-11-14 18:41:32 +0530

    ci: gzip canisters before deploy (#959)

--- scripts/canisters/local_deploy/upgrade_all_canisters_without_test.sh
+++ scripts/canisters/local_deploy/upgrade_all_canisters_without_test.sh
@@ -3,27 +3,31 @@ set -euo pipefail
 
 export USER_ID_global_super_admin=$(dfx identity get-principal)
 export CANISTER_ID_user_index=$(dfx canister id user_index)
-export CANISTER_ID_project_member_index=$(dfx canister id project_member_index)
 export CANISTER_ID_post_cache=$(dfx canister id post_cache)
+export CANISTER_ID_project_member_index=$(dfx canister id project_member_index)
 export LOCAL_TOP_POSTS_SYNC_INTERVAL="10000000000"
 
 dfx build individual_user_template
+gzip -f -1 ./target/wasm32-unknown-unknown/release/individual_user_template.wasm
 dfx build user_index
-dfx build project_member_index
+gzip -f -1 ./target/wasm32-unknown-unknown/release/user_index.wasm
 dfx build post_cache
+gzip -f -1 ./target/wasm32-unknown-unknown/release/post_cache.wasm
+dfx build project_member_index
+gzip -f -1 ./target/wasm32-unknown-unknown/release/project_member_index.wasm
 
 dfx canister install user_index --mode upgrade --argument "(record {
   known_principal_ids = vec {}
 })"
 dfx canister call user_index update_user_index_upgrade_user_canisters_with_latest_wasm
-dfx canister install project_member_index --mode upgrade --argument "(record {
+dfx canister install post_cache --mode upgrade --argument "(record {
   known_principal_ids = vec {}
 })"
-dfx canister install post_cache --mode upgrade --argument "(record {
+dfx canister install project_member_index --mode upgrade --argument "(record {
   known_principal_ids = vec {}
 })"
 
 dfx generate individual_user_template
 dfx generate user_index
-dfx generate project_member_index
 dfx generate post_cache
+dfx generate project_member_index
