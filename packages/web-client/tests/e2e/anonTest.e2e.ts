6dd2e50d37a27e639b1021f44de086086f6db8ed
Harsh Mandan <harsh@gobazzinga.io>
2022-12-01 17:58:22 +0700

    Update test to check anon canister id

--- packages/web-client/tests/e2e/anonTest.e2e.ts
+++ packages/web-client/tests/e2e/anonTest.e2e.ts
@@ -1,7 +1,6 @@
 import { HttpAgent } from '@dfinity/agent';
 import canisterIds from '../../../../.dfx/local/canister_ids.json' assert { type: 'json' };
 import { createActor as createUserIndexActor } from '../../declarations/user_index/index.js';
-import { createActor as createIndividualUserActor } from '../../declarations/individual_user_template/index.js';
 import fetch from 'isomorphic-fetch';
 import { expect, test } from 'vitest';
 
@@ -11,30 +10,12 @@ import { expect, test } from 'vitest';
 const agent = new HttpAgent({ host: 'http://0.0.0.0:4943', fetch });
 await agent.fetchRootKey();
 
-test('Setup initial data', async () => {
+test('Test anon identity canister', async () => {
 	const userIndexActor = createUserIndexActor(canisterIds.user_index.local, { agent });
 	const anonPrincipalId =
 		await userIndexActor.get_requester_principals_canister_id_create_if_not_exists_and_optionally_allow_referrer(
 			[]
 		);
 
-	expect(anonPrincipalId.toText()).toBeTruthy();
-
-	// const anonCanisterId = anonPrincipalId.toText();
-
-	const individualUserActor = createIndividualUserActor(
-		canisterIds.individual_user_template.local,
-		{ agent }
-	);
-
-	const post1 = await individualUserActor.add_post({
-		description: 'Test post #2',
-		hashtags: ['test', 'video', '2'],
-		video_uid: 'unique_video_id',
-		creator_consent_for_inclusion_in_hot_or_not: true
-	});
-
-	console.log({ post1 });
-
-	expect(Number(post1)).toBeGreaterThan(-1);
+	expect(anonPrincipalId.toText()).toBe('renrk-eyaaa-aaaaa-aaada-cai');
 });
