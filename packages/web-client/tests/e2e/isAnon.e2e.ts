32dcbab4ca297451344456f4886fdb90ec5b53bc
Harsh Mandan <harsh@gobazzinga.io>
2022-12-01 17:38:52 +0700

    Add post

--- packages/web-client/tests/e2e/isAnon.e2e.ts
+++ packages/web-client/tests/e2e/isAnon.e2e.ts
@@ -1,26 +1,40 @@
 import { HttpAgent } from '@dfinity/agent';
 import canisterIds from '../../../../.dfx/local/canister_ids.json' assert { type: 'json' };
-const canisterId = canisterIds.user_index.local;
-import { createActor } from '../../declarations/user_index/index.js';
+import { createActor as createUserIndexActor } from '../../declarations/user_index/index.js';
+import { createActor as createIndividualUserActor } from '../../declarations/individual_user_template/index.js';
 import fetch from 'isomorphic-fetch';
 import { expect, test } from 'vitest';
 
-// can't use 127.0.0.1 & localhost
+// can't use 127.0.0.1 or localhost
 // isomorphic-fetch not required with node>=18
+
 const agent = new HttpAgent({ host: 'http://0.0.0.0:4943', fetch });
 await agent.fetchRootKey();
 
-test('Anon user canisterId', async () => {
-	try {
-		const userIndexActor = createActor(canisterId, { agent });
-		const anonPrincipalId =
-			await userIndexActor.get_requester_principals_canister_id_create_if_not_exists_and_optionally_allow_referrer(
-				[]
-			);
+test('Setup initial data', async () => {
+	const userIndexActor = createUserIndexActor(canisterIds.user_index.local, { agent });
+	const anonPrincipalId =
+		await userIndexActor.get_requester_principals_canister_id_create_if_not_exists_and_optionally_allow_referrer(
+			[]
+		);
 
-		console.log('anon canisterId', anonPrincipalId.toText());
-		expect(anonPrincipalId.toText()).toBeTruthy();
-	} catch (e) {
-		console.error('ERROR::::::', e);
-	}
+	expect(anonPrincipalId.toText()).toBeTruthy();
+
+	// const anonCanisterId = anonPrincipalId.toText();
+
+	const individualUserActor = createIndividualUserActor(
+		canisterIds.individual_user_template.local,
+		{ agent }
+	);
+
+	const post1 = await individualUserActor.add_post({
+		description: 'Test post #2',
+		hashtags: ['test', 'video', '2'],
+		video_uid: 'unique_video_id',
+		creator_consent_for_inclusion_in_hot_or_not: true
+	});
+
+	console.log({ post1 });
+
+	expect(Number(post1)).toBeGreaterThan(-1);
 });
