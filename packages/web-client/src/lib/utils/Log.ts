9534e4e754eb7d39b9a9607af8a020b8d0549e15
Harsh Mandan <harsh@gobazzinga.io>
2022-12-08 18:41:14 +0700

    Always log

--- packages/web-client/src/lib/utils/Log.ts
+++ packages/web-client/src/lib/utils/Log.ts
@@ -1,5 +1,3 @@
 export default (data: any, type: 'log' | 'info' | 'warn' | 'error') => {
-	if (import.meta.env.NODE_ENV == 'development' || type == 'error') {
-		console[type](type, data);
-	}
+	console[type](type, data);
 };
