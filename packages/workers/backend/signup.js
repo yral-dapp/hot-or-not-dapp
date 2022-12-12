c5b0e997266b4ace3d5d8ca4209c9f0c439e2f98
Harsh Mandan <harsh@gobazzinga.io>
2022-12-12 15:54:05 +0700

    Revert changes

--- packages/workers/backend/signup.js
+++ packages/workers/backend/signup.js
@@ -1,15 +1,18 @@
 const handler = async (request) => {
 	try {
 		// eslint-disable-next-line no-undef
-		const val = await this.env.CONFIG.get('signupsEnabled');
+		const val = await CONFIG.get('signupsEnabled');
 
 		return new Response({ allowed: val === true });
 	} catch (error) {
 		console.error(error);
-		return new Response(JSON.stringify({ error }), {
-			status: 403,
-			headers: { ...request.corsHeaders }
-		});
+		return new Response(
+			{ error },
+			{
+				status: 403,
+				headers: { ...request.corsHeaders }
+			}
+		);
 	}
 };
 
