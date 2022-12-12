88b8bfedc57531d1fd8197649035f65e3a9d0f4f
Harsh Mandan <harsh@gobazzinga.io>
2022-12-12 15:13:52 +0700

    Make it a get request

--- packages/workers/index.js
+++ packages/workers/index.js
@@ -32,7 +32,7 @@ router.post('/video/getVideoUploadURL', getVideoUploadURL);
 router.get('/video/:cloudflareVideoUid/getVideoProcessingStatus', getVideoProcessingStatus);
 router.get('/video/:cloudflareVideoUid/enableMp4Download', enableMp4Download);
 router.post('/image/getImageUploadURL', getImageUploadURL);
-router.post('/backend/signupStatus', signupStatus);
+router.get('/backend/signupStatus', signupStatus);
 
 // 404 for everything else
 router.all('*', () => new Response('Not Found.', { status: 404 }));
