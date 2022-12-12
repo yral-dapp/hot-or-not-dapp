f0f13e1617dff45f341c69bd9a238ba001dc3b85
Harsh Mandan <harsh@gobazzinga.io>
2022-12-12 17:30:17 +0700

    Return value

--- packages/web-client/src/lib/helpers/signup.ts
+++ packages/web-client/src/lib/helpers/signup.ts
@@ -2,13 +2,16 @@ import Log from '$lib/utils/Log';
 
 const cfWorkerHost = import.meta.env.VITE_CLOUDFLARE_WORKERS_API_HOST;
 
-export async function checkSignupStatus() {
+export async function checkSignupStatus(): Promise<boolean> {
 	try {
 		const res = await fetch(`${cfWorkerHost}/backend/signupStatus`);
 		const body = await res.json();
 		Log({ body, from: '0 checkSignupStatus' }, 'info');
+		if (body.allowed) {
+			return true;
+		} else return false;
 	} catch (e) {
 		Log({ error: e, from: '1 checkSignupStatus' }, 'error');
-		return;
+		return false;
 	}
 }
