5304bed5b362525c1bf8c04a3cd8936680b2c7e1
Harsh Mandan <harsh@gobazzinga.io>
2022-12-13 18:02:29 +0700

    Add test feed

---
+++ packages/web-client/src/routes/(feed)/(splash)/tfeed/[id=videoId]/+page.svelte
@@ -0,0 +1,282 @@
+<script lang="ts">
+import NoVideosIcon from '$components/icons/NoVideosIcon.svelte';
+import { registerEvent } from '$components/seo/GoogleAnalytics.svelte';
+import TVideoPlayer from '$components/video/VideoPlayer.svelte';
+import { individualUser } from '$lib/helpers/backend';
+import {
+	getTopPosts,
+	getWatchedVideosFromCache,
+	type PostPopulated,
+	type PostPopulatedHistory
+} from '$lib/helpers/feed';
+import { getMp4Url, getThumbnailUrl } from '$lib/utils/cloudflare';
+import Log from '$lib/utils/Log';
+import { handleParams } from '$lib/utils/params';
+import { playerState } from '$stores/playerState';
+import { hideSplashScreen } from '$stores/splashScreen';
+import userProfile from '$stores/userProfile';
+import { Principal } from '@dfinity/principal';
+import { onMount, tick } from 'svelte';
+import 'swiper/css';
+import { Swiper, SwiperSlide } from 'swiper/svelte';
+import { debounce } from 'throttle-debounce';
+import type { PageData } from './$types';
+
+export let data: PageData;
+
+const fetchCount = 50;
+const fetchWhenVideosLeft = 10;
+const keepVideosLoadedCount: number = 4;
+
+let videos: PostPopulated[] = [];
+let currentVideoIndex = 0;
+let noMoreVideos = false;
+let loading = false;
+let currentPlayingIndex = 0;
+let videoPlayers: TVideoPlayer[] = [];
+let fetchedVideosCount = 0;
+
+type VideoViewReport = {
+	progress: number;
+	videoId: bigint;
+	canisterId: string;
+	profileId: string;
+	count: number;
+	score: bigint;
+};
+
+let videoStats: Record<number, VideoViewReport> = {};
+
+function joinArrayUniquely<T>(a: T[], b: T[]): T[] {
+	const arrayWithoutNoDuplicates = [...a, ...b].filter(
+		(value: any, index, self) =>
+			index ===
+			self.findIndex(
+				(t: any) =>
+					t.post_id === value.post_id && t.publisher_canister_id === value.publisher_canister_id
+			)
+	);
+	return arrayWithoutNoDuplicates;
+}
+
+async function fetchNextVideos() {
+	// console.log(`to fetch: ${!noMoreVideos} && ${videos.length}-${currentVideoIndex}<${fetchCount}`);
+	if (!noMoreVideos && videos.length - currentVideoIndex < fetchWhenVideosLeft) {
+		try {
+			Log({ res: 'fetching from ' + fetchedVideosCount, source: '0 fetchNextVideos' }, 'info');
+			loading = true;
+			const res = await getTopPosts(fetchedVideosCount, fetchCount, true);
+			if (res.error) {
+				// TODO: Handle error
+				loading = false;
+				return;
+			}
+
+			fetchedVideosCount = res.from;
+
+			videos = [...videos, ...res.posts];
+
+			if (!res.noMorePosts && res.posts.length == 0) {
+				fetchNextVideos();
+			} else if (res.noMorePosts) {
+				const watchedVideos = await getWatchedVideosFromCache();
+				videos = [...videos, ...watchedVideos];
+			}
+
+			noMoreVideos = res.noMorePosts;
+			await tick();
+			loading = false;
+
+			Log({ res: 'fetched', noMoreVideos, source: '0 fetchNextVideos' }, 'info');
+		} catch (e) {
+			Log({ error: e, noMoreVideos, source: '1 fetchNextVideos' }, 'error');
+			loading = false;
+		}
+	}
+}
+
+async function updateStats(oldIndex) {
+	const stats = videoStats[oldIndex] as VideoViewReport | undefined;
+	if (!stats || (stats?.count === 0 && stats?.progress === 0)) {
+		return;
+	}
+
+	delete videoStats[oldIndex];
+	const payload =
+		stats.count == 0
+			? {
+					WatchedPartially: { percentage_watched: Math.ceil(stats.progress) || 1 }
+			  }
+			: {
+					WatchedMultipleTimes: {
+						percentage_watched: Math.ceil(stats.progress) || 1,
+						watch_count: stats.count
+					}
+			  };
+	Log({ from: '0 updateStats', id: stats.videoId, payload }, 'info');
+	registerEvent('view_video', {
+		userId: $userProfile.principal_id,
+		video_publisher_id: stats.profileId,
+		video_publisher_canister_id: stats.canisterId,
+		video_id: stats.videoId,
+		watch_count: Math.ceil(stats.count + stats.progress),
+		home_feed_score: stats.score
+	});
+	await individualUser(Principal.from(stats.canisterId)).update_post_add_view_details(
+		stats.videoId,
+		payload
+	);
+}
+
+async function recordView(post?: PostPopulated) {
+	if (!post) return;
+	const postHistory: PostPopulatedHistory = {
+		...post,
+		watched_at: Date.now()
+	};
+	const { watchHistoryIdb } = await import('$lib/utils/idb');
+	await watchHistoryIdb.set(post.publisher_canister_id + '@' + post.post_id, postHistory);
+}
+
+async function handleChange(e: CustomEvent) {
+	const index = e.detail[0].realIndex;
+	currentVideoIndex = index;
+	Log({ currentVideoIndex, source: '0 handleChange' }, 'info');
+	updateStats(currentPlayingIndex);
+	playVideo(index);
+	recordView(videos[currentVideoIndex]);
+	fetchNextVideos();
+	updateURL(videos[currentVideoIndex]);
+	updateMetadata(videos[currentVideoIndex]);
+}
+
+function updateMetadata(video?: PostPopulated) {
+	if (!video) return;
+	if (!('mediaSession' in navigator)) return;
+	navigator.mediaSession.metadata = new MediaMetadata({
+		title: video.description,
+		artist: video.created_by_display_name[0] || video.created_by_unique_user_name[0] || '',
+		album: 'Hot or Not',
+		artwork: [{ src: getThumbnailUrl(video.video_uid), type: 'image/png' }]
+	});
+}
+
+const playVideo = debounce(50, async (index: number) => {
+	try {
+		videoPlayers[currentPlayingIndex]?.stop();
+		videoPlayers[index]?.play();
+		videoPlayers[index + 1]?.stop();
+		currentPlayingIndex = index;
+	} catch (e) {
+		Log({ error: e, index, source: '1 playVideo' }, 'error');
+	}
+});
+
+function updateURL(post?: PostPopulated) {
+	if (!post) return;
+	const url = post.publisher_canister_id + '@' + post.post_id;
+	$playerState.currentFeedUrl = url;
+	window.history.replaceState('', '', url);
+}
+
+function recordStats(
+	progress: number,
+	canisterId: string,
+	videoId: bigint,
+	profileId: string,
+	score: bigint
+) {
+	if (videoStats[currentPlayingIndex]) {
+		videoStats[currentPlayingIndex].progress = progress;
+		if (progress == 0) videoStats[currentPlayingIndex].count++;
+	} else {
+		videoStats[currentPlayingIndex] = {
+			progress,
+			videoId,
+			canisterId,
+			profileId,
+			count: 0,
+			score
+		};
+	}
+}
+
+onMount(async () => {
+	updateURL();
+	$playerState.initialized = false;
+	$playerState.muted = true;
+	if (data.post) {
+		videos = [data.post, ...videos];
+		await recordView(data.post);
+	}
+	await tick();
+	await fetchNextVideos();
+	handleParams();
+});
+</script>
+
+<svelte:head>
+	<title>Home Feed | Hot or Not</title>
+</svelte:head>
+
+<Swiper
+	direction="{'vertical'}"
+	observer
+	slidesPerView="{1}"
+	on:slideChange="{handleChange}"
+	cssMode
+	spaceBetween="{100}"
+	class="h-full w-full">
+	{#each videos as video, i (i)}
+		{@const src = getMp4Url(video.video_uid)}
+		<SwiperSlide class="flex h-full w-full snap-always items-center justify-center">
+			{#if currentVideoIndex - keepVideosLoadedCount < i && currentVideoIndex + keepVideosLoadedCount > i}
+				<TVideoPlayer
+					on:loaded="{() => hideSplashScreen(500)}"
+					on:watchedPercentage="{({ detail }) =>
+						recordStats(
+							detail,
+							video.publisher_canister_id,
+							video.id,
+							video.created_by_unique_user_name[0] ?? video.created_by_user_principal_id,
+							video.home_feed_ranking_score
+						)}"
+					bind:this="{videoPlayers[i]}"
+					i="{i}"
+					id="{video.id}"
+					likeCount="{Number(video.like_count)}"
+					displayName="{video.created_by_display_name[0]}"
+					profileLink="{video.created_by_unique_user_name[0] ?? video.created_by_user_principal_id}"
+					liked="{video.liked_by_me}"
+					description="{video.description}"
+					createdById="{video.created_by_user_principal_id}"
+					videoViews="{Number(video.total_view_count)}"
+					publisherCanisterId="{video.publisher_canister_id}"
+					userProfileSrc="{video.created_by_profile_photo_url[0]}"
+					individualUser="{individualUser}"
+					nextVideo="{currentVideoIndex + 1 == i || currentVideoIndex + 2 == i}"
+					inView="{i == currentVideoIndex}"
+					swiperJs
+					enrolledInHotOrNot="{video.hot_or_not_feed_ranking_score &&
+						video.hot_or_not_feed_ranking_score[0] !== undefined}"
+					thumbnail="{getThumbnailUrl(video.video_uid)}"
+					src="{src}" />
+			{/if}
+		</SwiperSlide>
+	{/each}
+	{#if loading}
+		<SwiperSlide class="flex h-full w-full items-center justify-center">
+			<div class="relative flex h-full w-full flex-col items-center justify-center space-y-8 px-8">
+				<div class="text-center text-lg font-bold">Loading</div>
+			</div>
+		</SwiperSlide>
+	{/if}
+	{#if noMoreVideos}
+		<SwiperSlide class="flex h-full w-full items-center justify-center">
+			<div class="relative flex h-full w-full flex-col items-center justify-center space-y-8 px-8">
+				<NoVideosIcon class="w-56" />
+				<div class="text-center text-lg font-bold">No more videos to display today</div>
+			</div>
+		</SwiperSlide>
+	{/if}
+</Swiper>
