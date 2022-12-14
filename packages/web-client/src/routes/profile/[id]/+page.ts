cb26279aa42db3978b2ead7eb44d520c4fcb41ba
Harsh Mandan <harsh@gobazzinga.io>
2022-12-14 18:00:08 +0700

    Handle username

--- packages/web-client/src/routes/profile/[id]/+page.ts
+++ packages/web-client/src/routes/profile/[id]/+page.ts
@@ -22,8 +22,15 @@ export const load: PageLoad = async ({ params }) => {
 	const userProfileData = get(userProfile);
 	const authStateData = get(authState);
 
-	if (id === '2vxsx-fae' && !authStateData.t) {
-		throw redirect(307, `/menu${id === userProfileData.principal_id ? '?logout=true' : ''}`);
+	if ((id === '2vxsx-fae' || id === 'rajeshmoundekar') && !authStateData.t) {
+		throw redirect(
+			307,
+			`/menu${
+				id === userProfileData.principal_id || id === userProfileData.unique_user_name
+					? '?logout=true'
+					: ''
+			}`
+		);
 	}
 
 	if (id === userProfileData.unique_user_name || id === userProfileData.principal_id) {
