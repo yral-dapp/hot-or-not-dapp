90ac864e316ddadec68399270f5d27018eeafe07
Saikat Das <saikatdas0790@gmail.com>
2022-12-17 17:24:19 +0530

    build: update build scripts for local and ic deploy

--- scripts/canisters/local_deploy/build_all_canisters.sh
+++ scripts/canisters/local_deploy/build_all_canisters.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+export CANISTER_ID_configuration=$(dfx canister id configuration)
 export USER_ID_global_super_admin=$(dfx identity get-principal)
 export CANISTER_ID_user_index=$(dfx canister id user_index)
 export CANISTER_ID_post_cache=$(dfx canister id post_cache)
-export CANISTER_ID_project_member_index=$(dfx canister id project_member_index)
 export LOCAL_TOP_POSTS_SYNC_INTERVAL="10000000000"
 
+dfx build configuration
+gzip -f -1 ./target/wasm32-unknown-unknown/release/configuration.wasm
 dfx build individual_user_template
 gzip -f -1 ./target/wasm32-unknown-unknown/release/individual_user_template.wasm
 dfx build user_index
 gzip -f -1 ./target/wasm32-unknown-unknown/release/user_index.wasm
 dfx build post_cache
 gzip -f -1 ./target/wasm32-unknown-unknown/release/post_cache.wasm
-dfx build project_member_index
-gzip -f -1 ./target/wasm32-unknown-unknown/release/project_member_index.wasm
