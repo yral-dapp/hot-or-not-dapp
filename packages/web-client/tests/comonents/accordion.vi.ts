85f8ae53d0a12366373ea1714389d122178390c9
Harsh Mandan <harsh@gobazzinga.io>
2022-12-21 19:14:40 +0700

    Add component test

---
+++ packages/web-client/tests/comonents/accordion.vi.ts
@@ -0,0 +1,23 @@
+import { tick } from 'svelte';
+import Accordion from '../../src/components/accordion/Accordion.svelte';
+
+let host: HTMLElement;
+
+afterEach(() => {
+	host.remove();
+});
+
+test('mount component', async () => {
+	host = document.createElement('div');
+	host.setAttribute('id', 'host');
+	document.body.appendChild(host);
+	const instance = new Accordion({ target: host, props: { expanded: false } });
+	expect(instance).toBeTruthy();
+	const btn = host.getElementsByTagName('button')[0];
+	btn.click();
+	await tick();
+	expect(host.innerHTML).toContain('-');
+	btn.click();
+	await tick();
+	expect(host.innerHTML).toContain('+');
+});
