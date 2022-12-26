cb1f0d33563d4bd004c5b9bf6ba3604f1c564a47
harshbaz <106158714+harshbaz@users.noreply.github.com>
2022-12-26 09:16:17 +0530

    Fix dependency conflicts (#1235)
    * Update deps
    
    * Update lockfile
    
    * Update lockfile
    
    * Cleanup
    
    * Update test
    
    * Use npm ci

--- packages/web-client/tests/web2/video-loads-on-feed-web2.spec.ts
+++ packages/web-client/tests/web2/video-loads-on-feed-web2.spec.ts
@@ -6,5 +6,5 @@ test('[Web2] Video source is properly loaded on feed', async ({ page }) => {
 	const videoEls = await page.locator('div .swiper-slide > player > video');
 	const videoSrc = await videoEls.nth(0).getAttribute('src');
 
-	expect(videoSrc?.includes('default.mp4'));
+	expect(videoSrc?.includes('video.m3u8'));
 });
