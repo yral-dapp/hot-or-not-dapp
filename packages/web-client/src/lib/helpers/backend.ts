078b17659719f8cd7b08de26a8bab97074f8e87a
harshbaz <106158714+harshbaz@users.noreply.github.com>
2022-12-29 10:01:05 +0530

    Fetch signup configuration from configuration canister (#1264)
    * Add actor helper for configuration canister
    
    * Create utility to check signup config on canister
    
    * Use canister to check signup status

--- packages/web-client/src/lib/helpers/backend.ts
+++ packages/web-client/src/lib/helpers/backend.ts
@@ -7,9 +7,16 @@ import {
 	createActor as createPostCacheActor,
 	canisterId as postCacheCanisterId
 } from '$canisters/post_cache';
+
+import {
+	createActor as createConfigurationActor,
+	canisterId as configurationCanisterId
+} from '$canisters/configuration';
+
 import type { _SERVICE as _USER_INDEX_SERVICE } from '$canisters/user_index/user_index.did';
 import type { _SERVICE as _INDIVIDUAL_USER_SERVICE } from '$canisters/individual_user_template/individual_user_template.did';
 import type { _SERVICE as _POST_CACHE_SERVICE } from '$canisters/post_cache/post_cache.did';
+import type { _SERVICE as _CONFIGURATION_SERVICE } from '$canisters/configuration/configuration.did';
 import { authHelper, authState } from '$stores/auth';
 import type { ActorSubclass } from '@dfinity/agent';
 import { get } from 'svelte/store';
@@ -21,6 +28,7 @@ export const host =
 export type UserIndexActor = ActorSubclass<_USER_INDEX_SERVICE>;
 export type IndividualUserActor = ActorSubclass<_INDIVIDUAL_USER_SERVICE>;
 export type PostCacheActor = ActorSubclass<_POST_CACHE_SERVICE>;
+export type ConfigurationActor = ActorSubclass<_CONFIGURATION_SERVICE>;
 
 export function userIndex(): UserIndexActor {
 	const authHelperData = get(authHelper);
@@ -47,4 +55,10 @@ export function postCache(): PostCacheActor {
 	return createPostCacheActor(postCacheCanisterId as string, {
 		agentOptions: { identity: authHelperData?.identity, host }
 	}) as PostCacheActor;
+}
+
+export function configuration(): ConfigurationActor {
+	return createConfigurationActor(configurationCanisterId, {
+		agentOptions: { host }
+	}) as ConfigurationActor;
 }
--- packages/web-client/src/lib/helpers/signup.ts
+++ packages/web-client/src/lib/helpers/signup.ts
@@ -1,17 +1,28 @@
 import Log from '$lib/utils/Log';
+import { configuration } from './backend';
 
 const cfWorkerHost = import.meta.env.VITE_CLOUDFLARE_WORKERS_API_HOST;
 
-export async function checkSignupStatus(): Promise<boolean> {
+export async function checkSignupStatusCf(): Promise<boolean> {
 	try {
 		const res = await fetch(`${cfWorkerHost}/backend/signupStatus`);
 		const body = await res.json();
-		Log({ body, from: '0 checkSignupStatus' }, 'info');
+		Log({ body, from: '0 checkSignupStatusCf' }, 'info');
 		if (body.allowed) {
 			return true;
 		} else return false;
 	} catch (e) {
-		Log({ error: e, from: '1 checkSignupStatus' }, 'error');
+		Log({ error: e, from: '1 checkSignupStatusCf' }, 'error');
+		return false;
+	}
+}
+
+export async function checkSignupStatusCanister(): Promise<boolean> {
+	try {
+		const res = await configuration().are_signups_enabled();
+		return res;
+	} catch (e) {
+		Log({ error: e, from: '1 checkSignupStatusCanister' }, 'error');
 		return false;
 	}
 }
