7c751d201785eec5bf9c9c7add96b43244767f36
Harsh Mandan <harsh@gobazzinga.io>
2022-12-30 17:59:43 +0530

    Add test for video

---
+++ packages/web-client/cypress/video.cy.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+
+describe('visit home feed', () => {
+	beforeEach(() => {
+		cy.visit('https://hotornot.wtf');
+	});
+
+	it('video has source', () => {
+		cy.get('video').should('have.prop', 'src', true);
+	});
+
+	it('video is autoplaying', () => {
+		cy.get('video').should('have.prop', 'paused', false).and('have.prop', 'ended', false);
+	});
+
+	it('video is not playing', () => {
+		cy.get('video')
+			.should('have.prop', 'paused', true)
+			.then(($video) => {
+				$video[0].play();
+			});
+
+		cy.get('video').should('have.prop', 'paused', false).and('have.prop', 'ended', false);
+	});
+
+	it('it has duration', () => {
+		cy.get('video').should(($video) => {
+			expect($video[0].duration).to.be.gt(0);
+		});
+	});
+});
