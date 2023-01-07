b5ef452a96b51360d4c38dcd294a9cfce7a3f4c6
Harsh Mandan <harsh@gobazzinga.io>
2023-01-07 11:13:45 +0530

    Add GoSquared component

---
+++ packages/web-client/src/components/seo/GoSquared.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+	<script>
+	!(function (g, s, q, r, d) {
+		r = g[r] =
+			g[r] ||
+			function () {
+				(r.q = r.q || []).push(arguments);
+			};
+		d = s.createElement(q);
+		d.src = '//d1l6p2sc9645hc.cloudfront.net/gosquared.js';
+		q = s.getElementsByTagName(q)[0];
+		q.parentNode.insertBefore(d, q);
+	})(window, document, 'script', '_gs');
+
+	_gs('GSN-686484-W');
+	_gs('set', 'anonymizeIP', true);
+	</script>
+</svelte:head>
