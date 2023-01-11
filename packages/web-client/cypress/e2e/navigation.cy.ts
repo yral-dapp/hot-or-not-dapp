b5f6cc669a9567aa8e69f4ded2d8359e4df668fa
Harsh Mandan <harsh@gobazzinga.io>
2023-01-11 17:31:49 +0530

    Update navigation tests

--- packages/web-client/cypress/e2e/navigation.cy.ts
+++ packages/web-client/cypress/e2e/navigation.cy.ts
@@ -1,5 +1,5 @@
 describe('Navigation test', () => {
-	const TEST_HOST = Cypress.env('TEST_HOST') || 'https://hotornot.wtf';
+	const TEST_HOST = 'http://localhost:5173';
 	const IC0_HOST = 'https://ic0.app';
 
 	before(() => {
@@ -15,25 +15,28 @@ describe('Navigation test', () => {
 	});
 
 	it('On a video, navigate to user profile', () => {
-		cy.get('splash-screen').and('not.exist');
-		cy.get('a[aria-roledescription=video-info]').click();
+		cy.get('div[aria-roledescription=video-info] > a').and('be.visible').click({ force: true });
 		expect(cy.url().should('contain', 'profile'));
 	});
 
 	it("Navigate to user profile and then navigate to user's lovers list", () => {
-		cy.get('splash-screen').and('not.exist');
-		cy.get('a[aria-roledescription=video-info]').click();
-		cy.find('a').contains('Lovers').click();
-		expect(cy.url().should('contain', 'lovers'));
+		cy.get('div[aria-roledescription=video-info] > a').and('be.visible').click({ force: true });
+		cy.wait(5000).then(() => {
+			cy.contains('Lovers').click();
+			expect(cy.url().should('contain', 'lovers'));
+		});
 	});
 
 	it('Navigate to user profile and then view a post', () => {
-		cy.get('splash-screen').and('not.exist');
-		cy.get('a[aria-roledescription=video-info]').click();
+		cy.get('div[aria-roledescription=video-info] > a').and('be.visible').click({ force: true });
 		cy.scrollTo('bottom');
-		cy.wait(2000).then(() => {
-			cy.find('a[aria-roledescription=user-post]').click();
-			expect(cy.url().should('contain', 'post'));
+		cy.wait(5000).then(() => {
+			cy.get('a[aria-roledescription=user-post]').then(($posts) => {
+				$posts[0].click();
+				cy.wait(500).then(() => {
+					expect(cy.url().should('contain', 'post'));
+				});
+			});
 		});
 	});
 });
