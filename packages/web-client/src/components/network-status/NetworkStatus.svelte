8918420278c33561f54c67dd06c85ce2cf0c3189
Harsh Mandan <harsh@gobazzinga.io>
2023-01-12 15:53:00 +0530

    Add network status component

---
+++ packages/web-client/src/components/network-status/NetworkStatus.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+export let offline = false;
+</script>
+
+{#if offline}
+	<network-status class="h-3 text-xs">
+		Offline. Please check your internet connection.
+	</network-status>
+{/if}
