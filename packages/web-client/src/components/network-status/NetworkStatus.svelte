92119aafe569cf5886e7be7e025e678b4268c7a3
Harsh Mandan <harsh@gobazzinga.io>
2023-01-12 17:15:22 +0530

    Add transition

--- packages/web-client/src/components/network-status/NetworkStatus.svelte
+++ packages/web-client/src/components/network-status/NetworkStatus.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
+import { slide } from 'svelte/transition';
+
 export let offline = false;
 </script>
 
 <svelte:window on:offline="{() => (offline = true)}" on:online="{() => (offline = false)}" />
 
 {#if offline}
-	<network-status class="h-3 text-xs">
+	<network-status
+		transition:slide|local="{{ delay: 500 }}"
+		class="h-3 text-xs {offline ? 'bg-red-500' : 'bg-green-500'}">
 		Offline. Please check your internet connection.
 	</network-status>
 {/if}
