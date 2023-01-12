44a4ed2f2711140eb70dc585e04a56e5a34c6918
Harsh Mandan <harsh@gobazzinga.io>
2023-01-12 17:15:13 +0530

    Add event listeners

--- packages/web-client/src/components/network-status/NetworkStatus.svelte
+++ packages/web-client/src/components/network-status/NetworkStatus.svelte
@@ -2,6 +2,8 @@
 export let offline = false;
 </script>
 
+<svelte:window on:offline="{() => (offline = true)}" on:online="{() => (offline = false)}" />
+
 {#if offline}
 	<network-status class="h-3 text-xs">
 		Offline. Please check your internet connection.
