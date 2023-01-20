275cdc879a7f575bc794920bac029eb80e529c67
harshbaz <106158714+harshbaz@users.noreply.github.com>
2023-01-20 10:53:11 +0530

    Update sentry logging (#1417)
    * Setup env
    
    * Update log
    
    * Update deps

--- packages/web-client/src/lib/utils/Log.ts
+++ packages/web-client/src/lib/utils/Log.ts
@@ -1,5 +1,34 @@
-export default (data: any, type: 'log' | 'info' | 'warn' | 'error') => {
+import * as Sentry from '@sentry/svelte';
+
+function replaceErrors(_, value) {
+	if (value instanceof Error) {
+		const error = {};
+
+		Object.getOwnPropertyNames(value).forEach((propName) => {
+			error[propName] = value[propName];
+		});
+
+		return error;
+	}
+
+	return value;
+}
+
+type Logs = 'log' | 'info' | 'warn' | 'error';
+
+const logTypeMap: Record<Logs, string> = {
+	log: '📺',
+	info: 'ℹ️',
+	warn: '⚠️',
+	error: '🚨'
+};
+
+export default (data: any, type: Logs) => {
+	const dataStr = JSON.stringify(data, replaceErrors) || data;
 	if (import.meta.env.NODE_ENV == 'development' || type == 'error') {
-		console[type](type, data);
+		console[type](logTypeMap[type], dataStr);
+	}
+	if (type === 'error') {
+		Sentry.captureException(dataStr);
 	}
 };
