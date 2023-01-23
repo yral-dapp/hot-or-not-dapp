38ab70b1e74ee0d8b7e13985317a43503168b1f7
Harsh Mandan <harsh@gobazzinga.io>
2023-01-23 10:55:52 +0700

    Add page to fetch watch history

---
+++ packages/web-client/src/routes/dev/feed/watchhistory/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+import Button from '$components/button/Button.svelte';
+import type { PostPopulated } from '$lib/helpers/feed';
+
+let history: PostPopulated[] = [];
+let error = false;
+
+async function getWatchedHistory() {
+	try {
+		const { watchHistoryIdb } = await import('$lib/utils/idb');
+		const values = (await watchHistoryIdb.values()) as PostPopulated[];
+		if (values.length) {
+			history = values;
+			console.log({ history });
+		}
+	} catch (_) {}
+	error = true;
+	history = [];
+}
+</script>
+
+<Button on:click="{getWatchedHistory}">Fetch history</Button>
+
+<div class="py-2 text-sm text-white">
+	{#if error}
+		Error fetching history
+	{:else if history.length}
+		<pre>{JSON.stringify(history, null, 2)}</pre>
+	{/if}
+</div>
