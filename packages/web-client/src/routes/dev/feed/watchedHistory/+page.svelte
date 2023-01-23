90e7cef63fa64e024e080f5696620d3ac1683a32
Harsh Mandan <harsh@gobazzinga.io>
2023-01-23 10:18:47 +0700

    Add watchedHistory dev page

---
+++ packages/web-client/src/routes/dev/feed/watchedHistory/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+import Button from '$components/button/Button.svelte';
+import type { PostPopulated } from '$lib/helpers/feed';
+
+let history: PostPopulated[] = [];
+
+function getWatchedHistory() {}
+</script>
+
+<Button on:click="{getWatchedHistory}">Fetch history</Button>
+
+{#if history.length}
+	<pre>{history}</pre>
+{/if}
