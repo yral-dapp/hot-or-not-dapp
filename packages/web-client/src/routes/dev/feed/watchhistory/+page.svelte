9873c80b7b3db2a580ba245458dc9a1d5ac6475a
Harsh Mandan <harsh@gobazzinga.io>
2023-01-24 12:51:52 +0700

    Use a table

--- packages/web-client/src/routes/dev/feed/watchhistory/+page.svelte
+++ packages/web-client/src/routes/dev/feed/watchhistory/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-import Button from '$components/button/Button.svelte';
-import type { PostPopulated } from '$lib/helpers/feed';
+import type { PostPopulatedHistory } from '$lib/helpers/feed';
+import { onMount } from 'svelte';
 
-let history: PostPopulated[] = [];
+let history: PostPopulatedHistory[] = [];
 let error = false;
 
-async function getWatchedHistory() {
+async function getHistory() {
 	try {
 		const { watchHistoryIdb } = await import('$lib/utils/idb');
-		const values = (await watchHistoryIdb.values()) as PostPopulated[];
+		const values = (await watchHistoryIdb.values()) as PostPopulatedHistory[];
 		if (values.length) {
 			history = values;
 			console.log({ history });
@@ -18,22 +18,33 @@ async function getWatchedHistory() {
 		history = [];
 	}
 }
-</script>
 
-<Button on:click="{getWatchedHistory}">Fetch history</Button>
+onMount(() => getHistory());
+</script>
 
 <div class="py-2 text-sm text-white">
 	{#if error}
 		Error fetching history
 	{:else if history.length}
-		<pre>{JSON.stringify(
-				history,
-				(_, val) => {
-					if (typeof val === 'bigint') {
-						return Number(val);
-					} else return val;
-				},
-				2
-			)}</pre>
+		<table class="table-auto border divide-y divide-white/30">
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Uploaded by</th>
+					<th>Watched at</th>
+					<th>Description</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each history as item}
+					<tr>
+						<td>{item.id}</td>
+						<td>{item.created_by_user_principal_id} ({item.created_by_display_name})</td>
+						<td>{new Date(item.watched_at).toLocaleString()}</td>
+						<td>{item.description}</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
 	{/if}
 </div>
