8f96082605a4b71bf0203f83612de58d1f997144
Harsh Mandan <harsh@gobazzinga.io>
2023-01-25 15:15:40 +0700

    Update identity generation

--- packages/web-client/tests/vitest/e2e/identity.ts
+++ packages/web-client/tests/vitest/e2e/identity.ts
@@ -1,16 +1,10 @@
 import { Secp256k1KeyIdentity } from '@dfinity/identity-secp256k1';
-import hdkey from 'hdkey';
-import bip39 from 'bip39';
 
 // Completely insecure seed phrase. Do not use for any purpose other than testing.
 const seed = 'harsh harsh harsh harsh harsh harsh harsh harsh harsh harsh harsh harsh';
 
 export const identityFromSeed = async (phrase) => {
-	const seed = await bip39.mnemonicToSeed(phrase);
-	const root = hdkey.fromMasterSeed(seed);
-	const addrnode = root.derive("m/44'/223'/0'/0/0");
-
-	return Secp256k1KeyIdentity.fromSecretKey(addrnode.privateKey);
+	return Secp256k1KeyIdentity.fromSeedPhrase(phrase);
 };
 
 export const identity = identityFromSeed(seed);
