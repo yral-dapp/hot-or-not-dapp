9b359463c9da8919341dcfa0d242b31494178b4c
Harsh Mandan <harsh@gobazzinga.io>
2023-01-27 19:49:10 +0700

    Handle first failed calls

--- packages/web-client/src/routes/(feed)/(splash)/feed/[id=videoId]/+page.ts
+++ packages/web-client/src/routes/(feed)/(splash)/feed/[id=videoId]/+page.ts
@@ -8,37 +8,42 @@ import { individualUser } from '$lib/helpers/backend'
 import Log from '$lib/utils/Log'
 
 export const load: PageLoad = async ({ params, fetch }) => {
-  const id = params.id.split('@')
-  const postId = BigInt(Number(id[1]))
-  const principal = Principal.from(id[0])
-  let cachedPost: PostPopulated | undefined = undefined
-
   try {
-    const { watchHistoryIdb } = await import('$lib/utils/idb')
-    cachedPost = await watchHistoryIdb.get(params.id)
-  } catch (e) {
-    Log({ error: e, source: '1 videoFeedLoad', type: 'idb' }, 'error')
-  }
+    const id = params.id.split('@')
+    const postId = BigInt(Number(id[1]))
+    const principal = Principal.from(id[0])
+    let cachedPost: PostPopulated | undefined = undefined
 
-  if (cachedPost) {
-    return { post: cachedPost }
-  } else {
-    const r = await individualUser(
-      principal,
-      fetch,
-    ).get_individual_post_details_by_id(postId)
-    if (r.video_uid) {
-      return {
-        post: {
-          ...r,
-          publisher_canister_id: id[0],
-          created_by_user_principal_id: r.created_by_user_principal_id.toText(),
-          post_id: postId,
-          score: BigInt(0),
-        } as PostPopulated,
-      }
-    } else {
-      return
+    try {
+      const { watchHistoryIdb } = await import('$lib/utils/idb')
+      cachedPost = await watchHistoryIdb.get(params.id)
+    } catch (e) {
+      Log({ error: e, source: '1 videoFeedLoad', type: 'idb' }, 'error')
     }
+
+    if (cachedPost) {
+      return { post: cachedPost }
+    } else {
+      const r = await individualUser(
+        principal,
+        fetch,
+      ).get_individual_post_details_by_id(postId)
+      if (r.video_uid) {
+        return {
+          post: {
+            ...r,
+            publisher_canister_id: id[0],
+            created_by_user_principal_id:
+              r.created_by_user_principal_id.toText(),
+            post_id: postId,
+            score: BigInt(0),
+          } as PostPopulated,
+        }
+      } else {
+        return
+      }
+    }
+  } catch (e) {
+    return
   }
 }
