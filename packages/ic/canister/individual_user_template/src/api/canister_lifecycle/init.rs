cb02f3a9d6d66c96e0e2f9c89549e969e4353dd5
Saikat Das <saikatdas0790@gmail.com>
2023-02-02 11:25:29 +0530

    refactor: move individual_user_template canister logic to a single crate

---
+++ packages/ic/canister/individual_user_template/src/api/canister_lifecycle/init.rs
@@ -0,0 +1,46 @@
+use std::collections::HashMap;
+
+use crate::{
+    data_model::{
+        version_details::VersionDetails, AccessControlMap, AllCreatedPosts, AllCreatedPostsV1,
+        MyKnownPrincipalIdsMap, MyTokenBalance, PostsIndexSortedByHomeFeedScore,
+        PostsIndexSortedByHotOrNotFeedScore, PostsIndexSortedByScore, PrincipalsIFollow,
+        PrincipalsThatFollowMe, Profile, SVersionDetails,
+    },
+    util::{access_control, known_principal_ids, periodic_update},
+};
+use ic_stable_memory::{s, stable_memory_init};
+use shared_utils::canister_specific::individual_user_template::types::args::IndividualUserTemplateInitArgs;
+
+#[ic_cdk_macros::init]
+#[candid::candid_method(init)]
+fn init(init_args: IndividualUserTemplateInitArgs) {
+    // * initialize stable memory
+    stable_memory_init(true, 0);
+
+    // * initialize stable variables
+    s! { Profile = Profile::new(init_args.profile_owner) };
+    s! { SVersionDetails = VersionDetails::new() };
+    s! { MyKnownPrincipalIdsMap = HashMap::new() }
+    known_principal_ids::save_known_principal_ids_from_user_index_init_args_to_my_known_principal_ids_map(&init_args);
+    s! { MyTokenBalance = MyTokenBalance::default() };
+
+    // * initialize stable collections
+    s! { AllCreatedPosts = AllCreatedPosts::new() };
+    s! { AccessControlMap = AccessControlMap::new_with_capacity(100) };
+    s! { PostsIndexSortedByScore = PostsIndexSortedByScore::new() };
+    s! { PostsIndexSortedByHomeFeedScore = PostsIndexSortedByHomeFeedScore::default() };
+    s! { PostsIndexSortedByHotOrNotFeedScore = PostsIndexSortedByHotOrNotFeedScore::default() };
+    s! { PrincipalsIFollow = PrincipalsIFollow::new() };
+    s! { PrincipalsThatFollowMe = PrincipalsThatFollowMe::new() };
+    s! { AllCreatedPostsV1 = AllCreatedPostsV1::new() };
+
+    // * initialize access control
+    let mut user_id_access_control_map = s!(AccessControlMap);
+    access_control::setup_initial_access_control(&mut user_id_access_control_map, init_args);
+    s! { AccessControlMap = user_id_access_control_map };
+
+    // * initialize periodic update
+    periodic_update::share_top_post_scores_with_post_cache_canister_v1();
+    periodic_update::update_post_scores_every_hour_v1();
+}
---
+++ packages/ic/canister/individual_user_template/src/api/post/add_post.rs
@@ -0,0 +1,40 @@
+use ic_stable_memory::{s, utils::ic_types::SPrincipal};
+use shared_utils::{
+    access_control::{self, UserAccessRole},
+    date_time::system_time,
+    types::post::PostDetailsFromFrontend,
+};
+
+use crate::data_model::{post::v1::Post, AccessControlMap, AllCreatedPostsV1};
+
+/// # Access Control
+/// Only the user whose profile details are stored in this canister can create a post.
+#[ic_cdk_macros::update]
+#[candid::candid_method(update)]
+fn add_post(post_details: PostDetailsFromFrontend) -> u64 {
+    // * access control
+    let user_id_access_control_map = s!(AccessControlMap);
+    assert!(access_control::does_principal_have_role(
+        &user_id_access_control_map,
+        UserAccessRole::ProfileOwner,
+        SPrincipal(ic_cdk::caller())
+    ));
+
+    let mut all_posts_mut = s!(AllCreatedPostsV1);
+    let id = all_posts_mut.len();
+
+    let mut post = Post::new(
+        id,
+        post_details,
+        &system_time::get_current_system_time_from_ic,
+    );
+
+    post.recalculate_home_feed_score(&system_time::get_current_system_time_from_ic);
+    post.recalculate_hot_or_not_feed_score(&system_time::get_current_system_time_from_ic);
+
+    all_posts_mut.push(&post);
+
+    s! { AllCreatedPostsV1 = all_posts_mut };
+
+    id
+}
