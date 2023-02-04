7d60610c7a298b2f9c9641aba92a8084c1daaef3
Harsh Mandan <harsh@gobazzinga.io>
2023-02-04 15:07:04 +0700

    Update source

--- packages/web-client/src/components/partytown/PartyTown.svelte
+++ packages/web-client/src/components/partytown/PartyTown.svelte
@@ -18,11 +18,12 @@ onMount(() => scriptEl && (scriptEl.textContent = partytownSnippet()))
   }
   </script>
   <script bind:this={scriptEl}></script>
+
   <script
     type="text/partytown"
     async
     defer
-    src="https://www.googletagmanager.com/gtag/js"></script>
+    src="https://hot-or-not-upload-api-main.go-bazzinga.workers.dev/proxy/ga"></script>
   <script type="text/partytown">
   window.dataLayer = window.dataLayer || []
   function gtag() {
@@ -30,19 +31,16 @@ onMount(() => scriptEl && (scriptEl.textContent = partytownSnippet()))
   }
   gtag('js', new Date())
   </script>
+  <script
+    type="text/partytown"
+    async
+    defer
+    src="https://hot-or-not-upload-api-main.go-bazzinga.workers.dev/proxy/gs"></script>
   <script type="text/partytown">
   try {
-    !(function (g, s, q, r, d) {
-      r = g[r] =
-        g[r] ||
-        function () {
-          ;(r.q = r.q || []).push(arguments)
-        }
-      d = s.createElement(q)
-      d.src = '//d1l6p2sc9645hc.cloudfront.net/gosquared.js'
-      q = s.getElementsByTagName(q)[0]
-      q.parentNode.insertBefore(d, q)
-    })(window, document, 'script', '_gs')
+    _gs = window['_gs'] = window['_gs'] || function () {
+      (_gs['script'] = _gs.['script'] || []).push(arguments)
+    }
 
     _gs('GSN-745719-F')
     _gs('set', 'anonymizeIP', true)
