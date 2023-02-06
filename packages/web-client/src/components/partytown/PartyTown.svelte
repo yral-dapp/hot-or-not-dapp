65c58fcc6793cbe951528f1c5986601cbceeee97
Harsh Mandan <harsh@gobazzinga.io>
2023-02-06 17:48:32 +0700

    Add sentry to Partytown

--- packages/web-client/src/components/partytown/PartyTown.svelte
+++ packages/web-client/src/components/partytown/PartyTown.svelte
@@ -31,4 +31,30 @@ onMount(() => scriptEl && (scriptEl.textContent = partytownSnippet()))
   gtag('js', new Date())
   window.gtag = gtag
   </script>
+
+  <script
+    src="https://browser.sentry-cdn.com/7.36.0/bundle.tracing.replay.min.js"
+    integrity="sha384-7tfJTi5j94hjodX3ESndyn59/88CYVw9kwwqXDzVHJH1FOT9KB/FAie508nQlJXJ"
+    crossorigin="anonymous"
+    type="text/partytown"></script>
+
+  <script type="text/partytown">
+  Sentry.init({
+    dsn: 'https://7586a69b01314524b31c8f4f64b41988@o4504076385124352.ingest.sentry.io/4504076386238464',
+    integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+    environment: 'local',
+    replaysSessionSampleRate: 0.3,
+    replaysOnErrorSampleRate: 1.0,
+    ignoreErrors: [
+      /(reading 'apply')/i, // Party-town
+      /Adding invalid event/i, // Replay Error
+      /Error in compression worker/i, // Replay Error
+      /e.getLastBreadcrumb/i, // Sentry error
+    ],
+    beforeSend: (event) => {
+      console.log('[SENTRY LOG]', event)
+      return event
+    },
+  })
+  </script>
 </svelte:head>
