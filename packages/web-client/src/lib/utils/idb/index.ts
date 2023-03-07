59ee34a95005397818fd9257da5f121383027df4
harshbaz <harsh@gobazzinga.io>
2023-03-07 15:50:32 +0700

    Remove idb util to create a singular idb interface

--- packages/web-client/src/lib/utils/idb/index.ts
+++
@@ -1,7 +0,0 @@
-import * as canisterIdb from './canisterIdb'
-import * as watchHistoryHomeIdb from './watchHistoryHomeIdb'
-import * as watchHistoryHoNIdb from './watchHistoryHoNIdb'
-
-export type CanisterIdb = typeof canisterIdb
-
-export { canisterIdb, watchHistoryHomeIdb, watchHistoryHoNIdb }
