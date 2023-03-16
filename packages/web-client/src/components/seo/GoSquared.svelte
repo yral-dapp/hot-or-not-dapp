164135e2cc649c4110d1e91d5ddb94d818c8b489
harshbaz <106158714+harshbaz@users.noreply.github.com>
2023-03-16 14:12:33 +0530

    Configure CSP and security headers (#1652)
    * Add directives list
    
    * Configure csp
    
    * Update deps
    
    * Update lockfile
    
    * Add headers file
    
    * Add permissions policy header
    
    * Update headers file
    
    * Use server hooks
    
    * Updated directives
    
    * Removed unsafe inline script
    
    * Enable csp
    
    * Add hash
    
    * Use ts
    
    * Revert "Use ts"
    
    This reverts commit a42a3d0b5981f9332e2ef4c4085f40320e9a3935.
    
    * Update directives
    
    * Use nonce mode
    
    * Remove script-src
    
    * Cleanup
    
    * Update csp mode
    
    * Use https
    
    * Update directives
    
    * Cleanup
    
    * Update directives
    
    * Convert to ts
    
    * Update import
    
    * Revert changes
    
    * Do not use csp in dev builds
    
    * Add report uri
    
    * Fix report uri
    
    * Introduce csp error
    
    * Allow sentry domain to connect
    
    * Add sentry url to report uri
    
    * Revert adding the error temporarily
    
    * Set environment as production for uri reports
    
    * Update img-src

--- packages/web-client/src/components/seo/GoSquared.svelte
+++ packages/web-client/src/components/seo/GoSquared.svelte
@@ -43,7 +43,7 @@ export const unidentifyUserGS = () => {
           ;(r.q = r.q || []).push(arguments)
         }
       d = s.createElement(q)
-      d.src = '//d1l6p2sc9645hc.cloudfront.net/gosquared.js'
+      d.src = 'https://d1l6p2sc9645hc.cloudfront.net/gosquared.js'
       q = s.getElementsByTagName(q)[0]
       q.parentNode.insertBefore(d, q)
     })(window, document, 'script', '_gs')
