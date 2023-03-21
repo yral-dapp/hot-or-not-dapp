524b5c93bc0bf557ce0a1f5f747eb4e617c68ee0
harshbaz <harsh@gobazzinga.io>
2023-03-21 15:39:03 +0530

    Use force mode to click

--- packages/web-client/cypress/e2e/navigation.cy.ts
+++ packages/web-client/cypress/e2e/navigation.cy.ts
@@ -1,6 +1,7 @@
 describe('Navigation test', () => {
   const TEST_HOST = Cypress.env('TEST_HOST') || 'https://hotornot.wtf'
   const IC0_HOST = 'https://ic0.app'
+  const timeout = 20_000
 
   before(() => {
     cy.task(
@@ -19,36 +20,36 @@ describe('Navigation test', () => {
 
   it('Navigate to user profile from the feed', () => {
     cy.get('div[aria-roledescription=video-info] > a', {
-      timeout: 10_000,
+      timeout,
     })
       .first()
-      .click()
+      .click({ force: true })
 
     expect(cy.url().should('contain', 'profile'))
   })
 
   it("Navigate to user profile and then navigate to user's lovers list", () => {
     cy.get('div[aria-roledescription=video-info] > a', {
-      timeout: 10_000,
+      timeout,
     })
       .first()
-      .click()
+      .click({ force: true })
 
-    cy.contains('Lovers', { timeout: 20_000 }).click()
+    cy.contains('Lovers', { timeout: 20_000 }).click({ force: true })
     expect(cy.url().should('contain', 'lovers'))
   })
 
   it('Navigate to user profile and then view a post', () => {
     cy.get('div[aria-roledescription=video-info] > a', {
-      timeout: 10_000,
+      timeout,
     })
       .first()
-      .click()
+      .click({ force: true })
     cy.scrollTo('bottom')
 
-    cy.get('a[aria-roledescription=user-post]', { timeout: 20_000 })
+    cy.get('a[aria-roledescription=user-post]', { timeout })
       .first()
-      .click()
+      .click({ force: true })
       .then(() => {
         expect(cy.url().should('contain', 'post'))
       })
