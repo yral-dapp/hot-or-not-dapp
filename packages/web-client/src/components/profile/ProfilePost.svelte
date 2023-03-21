e635e5e61d2b0f68b24e6729b78cb378461b4b34
harshbaz <106158714+harshbaz@users.noreply.github.com>
2023-03-21 17:35:42 +0530

    Update profile post thumbnail style (#1721)
    Update background. Add gradient

--- packages/web-client/src/components/profile/ProfilePost.svelte
+++ packages/web-client/src/components/profile/ProfilePost.svelte
@@ -14,10 +14,13 @@ export let likes: number
   aria-roledescription="user-post"
   href={`/profile/${$page.params.id}/post/${id}`}
   data-sveltekit-preload-data="tap"
-  class="relative aspect-[9/16] w-full cursor-pointer overflow-hidden rounded-md border-[1px] border-white/5 bg-cover bg-center"
-  style="background-image: url('{imageBg}')">
+  class="relative aspect-[9/16] w-full cursor-pointer overflow-hidden rounded-md border-[1px] border-white/5">
   <div
-    class="pointer-events-none absolute inset-x-0 bottom-2 flex items-center justify-between space-x-2 px-2">
+    class="absolute inset-0 scale-110 bg-cover bg-center"
+    style="background-image: url('{imageBg}')" />
+  <div
+    class="pointer-events-none absolute inset-x-0 bottom-0 flex h-1/3 items-end justify-between space-x-2 px-2 pb-2"
+    style="background: linear-gradient(0deg, rgba(0,0,0,0.5) 0%, transparent 100%);">
     <div class="flex items-center space-x-1">
       <div
         class="flex h-5 w-5 items-center justify-center rounded-full bg-black/50">
