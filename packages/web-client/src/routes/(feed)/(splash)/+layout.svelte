a0b31fb9b9cb37e2ca99ac7065b44487cc328709
harshbaz <harsh@gobazzinga.io>
2023-03-24 16:45:31 +0530

    Remove handling logic and move it to feed layout

--- packages/web-client/src/routes/(feed)/(splash)/+layout.svelte
+++ packages/web-client/src/routes/(feed)/(splash)/+layout.svelte
@@ -1,23 +1,5 @@
 <script lang="ts">
 import SplashScreen from '$components/layout/SplashScreen.svelte'
-import { playerState } from '$stores/playerState'
-import { onDestroy, onMount } from 'svelte'
-
-function handleVisibilityChange() {
-  if (document.visibilityState === 'hidden') {
-    $playerState.visible = true
-  } else {
-    $playerState.visible = false
-  }
-}
-
-onMount(async () => {
-  document.addEventListener('visibilitychange', handleVisibilityChange)
-})
-
-onDestroy(() => {
-  document.removeEventListener('visibilitychange', handleVisibilityChange)
-})
 </script>
 
 <SplashScreen />
