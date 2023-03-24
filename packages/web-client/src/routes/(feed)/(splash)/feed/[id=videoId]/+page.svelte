7d0bf0a229e14608df0fce3c091e63b00ccda252
harshbaz <harsh@gobazzinga.io>
2023-03-24 16:29:03 +0530

    Update main feed with new layout component

--- packages/web-client/src/routes/(feed)/(splash)/feed/[id=videoId]/+page.svelte
+++ packages/web-client/src/routes/(feed)/(splash)/feed/[id=videoId]/+page.svelte
@@ -3,31 +3,20 @@ import { beforeNavigate } from '$app/navigation'
 import { page } from '$app/stores'
 import Button from '$components/button/Button.svelte'
 import NoVideosIcon from '$components/icons/NoVideosIcon.svelte'
-import HomeFeedPlayer from '$components/player/HomeFeedPlayer.svelte'
-import { registerEvent } from '$components/seo/GA.svelte'
+import PlayerLayout from '$components/layout/PlayerLayout.svelte'
 import VideoPlayer from '$components/video/VideoPlayer.svelte'
-import { individualUser } from '$lib/helpers/backend'
 import {
   getTopPosts,
   getWatchedVideosFromCache,
   updatePostInWatchHistory,
   type PostPopulated,
 } from '$lib/helpers/feed'
-import { getThumbnailUrl } from '$lib/utils/cloudflare'
 import { updateURL } from '$lib/utils/feedUrl'
-import { isiPhone } from '$lib/utils/isSafari'
 import Log from '$lib/utils/Log'
 import { handleParams } from '$lib/utils/params'
-import {
-  joinArrayUniquely,
-  updateMetadata,
-  type VideoViewReport,
-} from '$lib/utils/video'
-import { authState } from '$stores/auth'
+import { joinArrayUniquely, updateMetadata } from '$lib/utils/video'
 import { homeFeedVideos, playerState } from '$stores/playerState'
 import { hideSplashScreen } from '$stores/splashScreen'
-import userProfile from '$stores/userProfile'
-import { Principal } from '@dfinity/principal'
 import Hls from 'hls.js/dist/hls.min'
 import { onDestroy, onMount, tick } from 'svelte'
 import 'swiper/css'
@@ -42,13 +31,10 @@ const keepVideosLoadedCount: number = 3
 
 let videos: PostPopulated[] = []
 let currentVideoIndex = 0
+let lastWatchedVideoIndex = -1
 let noMoreVideos = false
 let loading = false
-let currentPlayingIndex = 0
 let fetchedVideosCount = 0
-let isIPhone = isiPhone()
-let isDocumentHidden = false
-let videoStats: Record<string, VideoViewReport> = {}
 
 let loadTimeout: ReturnType<typeof setTimeout> | undefined = undefined
 let errorCount = 0
@@ -116,97 +102,12 @@ async function fetchNextVideos(force = false) {
 }
 
 async function handleChange(e: CustomEvent) {
+  lastWatchedVideoIndex = currentVideoIndex
   const newIndex = e.detail[0].realIndex
   currentVideoIndex = newIndex
-  Log({ currentVideoIndex, source: '0 handleChange' }, 'info')
-  const lastVideo = videos[currentPlayingIndex]
-  lastVideo?.publisher_canister_id &&
-    updateStats(lastVideo.publisher_canister_id, Number(lastVideo.post_id))
   fetchNextVideos()
   updateURL(videos[currentVideoIndex])
   updateMetadata(videos[currentVideoIndex])
-  currentPlayingIndex = newIndex
-}
-
-function handleVisibilityChange() {
-  if (document.visibilityState === 'hidden') {
-    isDocumentHidden = true
-  } else {
-    isDocumentHidden = false
-  }
-}
-
-async function updateStats(canisterId: string, videoId: number) {
-  const stats = videoStats[`${canisterId}@${videoId}`] as
-    | VideoViewReport
-    | undefined
-
-  if (!stats || (stats?.count === 0 && stats?.progress === 0)) {
-    return
-  }
-
-  delete videoStats[`${canisterId}@${videoId}`]
-
-  if ($page.url.host.includes('t:')) return
-
-  const payload =
-    stats.count == 0
-      ? {
-          WatchedPartially: {
-            percentage_watched: Math.ceil(stats.progress) || 1,
-          },
-        }
-      : {
-          WatchedMultipleTimes: {
-            percentage_watched: Math.ceil(stats.progress) || 1,
-            watch_count: stats.count,
-          },
-        }
-
-  Log({ from: '0 updateStats', id: stats.videoId, payload }, 'info')
-
-  registerEvent('view_video', {
-    userId: $userProfile.principal_id,
-    video_publisher_id: stats.profileId,
-    video_publisher_canister_id: stats.canisterId,
-    video_id: stats.videoId,
-    watch_count: Math.ceil(stats.count + stats.progress),
-    home_feed_score: stats.score,
-  })
-  await individualUser(
-    Principal.from(stats.canisterId),
-  ).update_post_add_view_details(stats.videoId, payload)
-}
-
-function recordStats(
-  progress: number,
-  canisterId: string,
-  videoId: bigint,
-  profileId: string,
-  score: bigint,
-) {
-  const id = `${canisterId}@${videoId}`
-  if (videoStats[id]) {
-    videoStats[id].progress = progress
-    if (progress == 0) {
-      videoStats[id].count++
-
-      if (videos[currentPlayingIndex]) {
-        videos[currentPlayingIndex].total_view_count =
-          videos[currentPlayingIndex].total_view_count + BigInt(1)
-        updatePostInWatchHistory('watch', videos[currentPlayingIndex])
-      }
-    }
-  } else {
-    videoStats[id] = {
-      progress,
-      videoId,
-      canisterId,
-      profileId,
-      count: 0,
-      score,
-    }
-  }
 }
 
 onMount(async () => {
@@ -223,88 +124,13 @@ onMount(async () => {
   await tick()
   await fetchNextVideos()
   handleParams()
-  document.addEventListener('visibilitychange', handleVisibilityChange)
 })
 
 onDestroy(() => {
-  document.removeEventListener('visibilitychange', handleVisibilityChange)
   if (loadTimeout) {
     clearTimeout(loadTimeout)
   }
 })
-
-beforeNavigate(() => {
-  videos.length > 2 && homeFeedVideos.set(videos.slice(currentPlayingIndex))
-  isDocumentHidden = true
-})
-
-async function handleLike(videoIndex: number) {
-  if (!$authState.isLoggedIn) {
-    $authState.showLogin = true
-    return
-  }
-  const post = videos[videoIndex]
-  if (!post) return
-
-  updatePostInWatchHistory('watch', post, {
-    liked_by_me: !post.liked_by_me,
-    like_count: post.like_count + BigInt(post.liked_by_me ? -1 : 1),
-  })
-
-  videos[videoIndex] = {
-    ...post,
-    liked_by_me: !post.liked_by_me,
-    like_count: post.like_count + BigInt(post.liked_by_me ? -1 : 1),
-  }
-
-  registerEvent('like_video', {
-    userId: $userProfile.principal_id,
-    video_publisher_id:
-      post.created_by_unique_user_name[0] ?? post.created_by_user_principal_id,
-    video_publisher_canister_id: post.publisher_canister_id,
-    video_id: post.id,
-    likes: post.like_count,
-  })
-
-  try {
-    await individualUser(
-      post.publisher_canister_id,
-    ).update_post_toggle_like_status_by_caller(post.id)
-  } catch (e) {
-    updatePostInWatchHistory('watch', post, {
-      liked_by_me: post.liked_by_me,
-      like_count: post.like_count,
-    })
-
-    videos[videoIndex] = {
-      ...post,
-      liked_by_me: post.liked_by_me,
-      like_count: post.like_count,
-    }
-  }
-}
-
-async function handleShare(videoIndex: number) {
-  const post = videos[videoIndex]
-  if (!post) return
-  try {
-    await navigator.share({
-      title: 'Hot or Not',
-      text: `Check out this hot video by ${post.created_by_display_name[0]}. \n${post.description}`,
-      url: `https://hotornot.wtf/feed/${post.publisher_canister_id}@${post.id}`,
-    })
-  } catch (_) {}
-  registerEvent('share_video', {
-    userId: $userProfile.principal_id,
-    video_publisher_id:
-      post.created_by_unique_user_name[0] ?? post.created_by_user_principal_id,
-    video_publisher_canister_id: post.publisher_canister_id,
-    video_id: post.id,
-  })
-  await individualUser(
-    post.publisher_canister_id,
-  ).update_post_increment_share_count(post.id)
-}
 </script>
 
 <svelte:head>
@@ -319,47 +145,29 @@ async function handleShare(videoIndex: number) {
   on:slideChange={handleChange}
   spaceBetween={300}
   class="h-full w-full">
-  {#each videos as video, i (i)}
+  {#each videos as post, i (i)}
     <SwiperSlide
       class="flex h-full w-full snap-always items-center justify-center">
       {#if currentVideoIndex - 2 < i && currentVideoIndex + keepVideosLoadedCount > i}
-        <HomeFeedPlayer
-          {i}
-          id={video.id}
-          likes={Number(video.like_count)}
-          displayName={video.created_by_display_name[0]}
-          profileLink={video.created_by_unique_user_name[0] ??
-            video.created_by_user_principal_id}
-          liked={video.liked_by_me}
-          createdById={video.created_by_user_principal_id}
-          videoViews={Number(video.total_view_count)}
-          publisherCanisterId={video.publisher_canister_id}
-          userProfileSrc={video.created_by_profile_photo_url[0]}
-          enrolledInHotOrNot={video.hot_or_not_feed_ranking_score &&
-            video.hot_or_not_feed_ranking_score[0] !== undefined}
-          thumbnail={getThumbnailUrl(video.video_uid)}
-          on:like={() => handleLike(i)}
-          on:share={() => handleShare(i)}>
+        <PlayerLayout
+          bind:post
+          index={i}
+          watchHistoryDb="watch"
+          showLikeButton
+          showReferAndEarnLink
+          showShareButton
+          showHotOrNotButton
+          justWatched={i === lastWatchedVideoIndex}
+          let:recordView>
           <VideoPlayer
             on:loaded={() => hideSplashScreen(500)}
-            on:watchedPercentage={({ detail }) =>
-              recordStats(
-                detail,
-                video.publisher_canister_id,
-                video.id,
-                video.created_by_unique_user_name[0] ??
-                  video.created_by_user_principal_id,
-                video.home_feed_ranking_score,
-              )}
-            {i}
+            on:watchedPercentage={({ detail }) => recordView(detail)}
+            index={i}
             playFormat="hls"
             {Hls}
-            isiPhone={isIPhone}
-            inView={i == currentVideoIndex &&
-              !isDocumentHidden &&
-              pathname.includes('feed')}
-            uid={video.video_uid} />
-        </HomeFeedPlayer>
+            inView={i == currentVideoIndex && !$playerState.visible}
+            uid={post.video_uid} />
+        </PlayerLayout>
       {/if}
     </SwiperSlide>
   {/each}
