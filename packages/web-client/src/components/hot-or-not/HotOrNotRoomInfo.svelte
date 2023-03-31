7b80458f928f80404cd8a2de6199dda0fd5ce87c
harshbaz <harsh@gobazzinga.io>
2023-03-31 17:29:42 +0530

    Show updated info if betting is open

--- packages/web-client/src/components/hot-or-not/HotOrNotRoomInfo.svelte
+++ packages/web-client/src/components/hot-or-not/HotOrNotRoomInfo.svelte
@@ -1,26 +1,49 @@
+<script lang="ts" context="module">
+import type {
+  BettingStatus,
+  SystemTime,
+} from '$canisters/individual_user_template/individual_user_template.did'
+type UnionKeyOf<U> = U extends U ? keyof U : never
+type BettingCurrentStatus = UnionKeyOf<BettingStatus>
+
+const systemTime: SystemTime = {
+  nanos_since_epoch: 0,
+  secs_since_epoch: BigInt(1),
+}
+</script>
+
 <script lang="ts">
 import PieChartIcon from '$components/icons/PieChartIcon.svelte'
 import TimerIcon from '$components/icons/TimerIcon.svelte'
 import UsersIcon from '$components/icons/UsersIcon.svelte'
-$: timeLeft = '50m 11s'
-$: usersInThisSlot = 48
-$: roomNumber = 24
+import { getMsLeftForBetResult } from '$lib/utils/timeLeft'
+
+export let bettingStatus: BettingStatus
+
+$: bettingOpen = bettingStatus['BettingOpen'] !== undefined
+$: usersInThisSlot = bettingStatus['BettingOpen']?.number_of_participants || 0
+$: onGoingSlot = bettingStatus['BettingOpen']?.ongoing_slot || 1
+$: betStartedAt = bettingStatus['BettingOpen']?.started_at || systemTime
+
+$: timeLeft = getMsLeftForBetResult(onGoingSlot, betStartedAt)
 </script>
 
-<div class="flex items-center space-x-3">
-  <div
-    class="flex items-center space-x-2 rounded-full bg-black/40 py-2 px-3 text-white">
-    <TimerIcon class="h-4 w-4" />
-    <span class="text-sm">{timeLeft}</span>
+{#if bettingOpen}
+  <div class="flex items-center space-x-3">
+    <div
+      class="flex items-center space-x-2 rounded-full bg-black/40 px-3 py-2 text-white">
+      <TimerIcon class="h-4 w-4" />
+      <span class="text-sm">{$timeLeft}</span>
+    </div>
+    <div
+      class="flex items-center space-x-2 rounded-full bg-black/40 px-3 py-2 text-white">
+      <UsersIcon class="h-4 w-4" />
+      <span class="text-sm">{usersInThisSlot}/100</span>
+    </div>
+    <div
+      class="flex items-center space-x-2 rounded-full bg-black/40 px-3 py-2 text-white">
+      <PieChartIcon class="h-4 w-4" />
+      <span class="text-sm">{onGoingSlot}/48</span>
+    </div>
   </div>
-  <div
-    class="flex items-center space-x-2 rounded-full bg-black/40 py-2 px-3 text-white">
-    <UsersIcon class="h-4 w-4" />
-    <span class="text-sm">{usersInThisSlot}/100</span>
-  </div>
-  <div
-    class="flex items-center space-x-2 rounded-full bg-black/40 py-2 px-3 text-white">
-    <PieChartIcon class="h-4 w-4" />
-    <span class="text-sm">{roomNumber}/48</span>
-  </div>
-</div>
+{/if}
