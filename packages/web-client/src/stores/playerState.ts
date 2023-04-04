30abf0b182ee3fe14cc29b018d018d6228dbe14e
harshbaz <harsh@gobazzinga.io>
2023-04-04 11:19:02 +0530

    Add selectedCoins to playerState

--- packages/web-client/src/stores/playerState.ts
+++ packages/web-client/src/stores/playerState.ts
@@ -7,12 +7,14 @@ export const playerState = writable<{
   muted: boolean
   currentFeedUrl: string
   currentHotOrNotUrl: string
+  selectedCoins: 10 | 50 | 100
 }>({
   initialized: false,
   muted: true,
   visible: true,
   currentFeedUrl: '',
   currentHotOrNotUrl: '',
+  selectedCoins: 10,
 })
 
 export const homeFeedVideos = writable<PostPopulated[]>([])
