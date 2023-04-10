d768e6ef7e94aae5da14feb97eb2cb8eb972eaa6
harshbaz <harsh@gobazzinga.io>
2023-04-10 13:54:06 +0530

    Add icon

---
+++ packages/web-client/src/components/icons/ExternalLinkIcon.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+export { exportClass as class }
+let exportClass: any = ''
+</script>
+
+<svg
+  class={exportClass}
+  viewBox="0 0 24 24"
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none">
+  <path
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    d="M12 6H7a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1v-5m-6 0 7.5-7.5M15 3h6v6" />
+</svg>
