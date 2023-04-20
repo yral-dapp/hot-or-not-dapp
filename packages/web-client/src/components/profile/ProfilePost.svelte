04b332837418020aa29cb602e8f5993c9134c9db
harshbaz <106158714+harshbaz@users.noreply.github.com>
2023-04-20 11:25:26 +0530

    Show not available in profile thumbnail posts as well (#1851)
    * Add image loading and error handling to post
    
    * Handle unavailable speculation posts thumbnails
    
    * Cleanup

--- packages/web-client/src/components/profile/ProfilePost.svelte
+++ packages/web-client/src/components/profile/ProfilePost.svelte
@@ -3,21 +3,45 @@ import { page } from '$app/stores'
 import EyeIcon from '$components/icons/EyeIcon.svelte'
 import { getShortNumber } from '$lib/utils/shortNumber'
 import HeartIcon from '$components/icons/HeartIcon.svelte'
+import { onMount } from 'svelte'
+import CloudNotAvailableIcon from '$components/icons/CloudNotAvailableIcon.svelte'
 
 export let id: number
 export let imageBg: string
 export let views: number
 export let likes: number
+
+let _img = ''
+let notAvailable = false
+
+function loadImg() {
+  const image = new Image()
+  image.addEventListener('load', () => (_img = imageBg))
+  image.addEventListener('error', () => {
+    notAvailable = true
+  })
+  image.src = imageBg
+}
+
+onMount(loadImg)
 </script>
 
 <a
   aria-roledescription="user-post"
   href={`/profile/${$page.params.id}/post/${id}`}
   data-sveltekit-preload-data="tap"
-  class="relative aspect-[9/16] w-full cursor-pointer overflow-hidden rounded-md border-[1px] border-white/5">
-  <div
-    class="absolute inset-0 scale-110 bg-cover bg-center"
-    style="background-image: url('{imageBg}')" />
+  class="relative aspect-[9/16] w-full cursor-pointer overflow-hidden rounded-md border-[1px] border-white/20">
+  {#if _img}
+    <div
+      class="absolute inset-0 scale-110 bg-cover bg-center"
+      style="background-image: url('{_img}')" />
+  {:else if notAvailable}
+    <div
+      class="gap- absolute inset-0 flex flex-col items-center justify-center">
+      <CloudNotAvailableIcon class="h-8 w-8" />
+      <span class="text-xs">Not Available</span>
+    </div>
+  {/if}
   <div
     class="pointer-events-none absolute inset-x-0 bottom-0 flex h-1/3 items-end justify-between space-x-2 px-2 pb-2"
     style="background: linear-gradient(0deg, rgba(0,0,0,0.5) 0%, transparent 100%);">
