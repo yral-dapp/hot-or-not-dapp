79bd8bef9a9a9116f69cd9e1a7aebfd631865048
harshbaz <harsh@gobazzinga.io>
2023-05-01 18:55:47 +0700

    Bind prop

--- packages/web-client/src/routes/airdrop/+page.svelte
+++ packages/web-client/src/routes/airdrop/+page.svelte
@@ -5,6 +5,9 @@ import HomeLayout from '$components/layout/HomeLayout.svelte'
 import WaitlistForm from '$components/waitlist-form/WaitlistForm.svelte'
 import goBack from '$lib/utils/goBack'
 import { navigateBack } from '$stores/navigation'
+import { confetti } from '@neoconfetti/svelte'
+
+let participated = false
 </script>
 
 <svelte:head>
@@ -27,7 +30,7 @@ import { navigateBack } from '$stores/navigation'
   </svelte:fragment>
   <svelte:fragment slot="content">
     <div class="h-full w-full overflow-y-auto px-2 py-24">
-      <WaitlistForm />
+      <WaitlistForm bind:participated />
     </div>
   </svelte:fragment>
 </HomeLayout>
