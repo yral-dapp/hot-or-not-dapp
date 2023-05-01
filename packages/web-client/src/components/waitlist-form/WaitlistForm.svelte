52ca1e7a4a0a55ebd3796b76b167a0a4ca0a72da
harshbaz <harsh@gobazzinga.io>
2023-05-01 18:17:27 +0700

    Create component

---
+++ packages/web-client/src/components/waitlist-form/WaitlistForm.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+let loading = true
+let participated = false
+const backgroundUrl =
+  'https://miro.medium.com/v2/resize:fit:4800/format:webp/1*JCirrVIiqLPa14NEBRx_6A.jpeg'
+</script>
+
+<waitlist-form
+  class="mx-3 flex w-full max-w-5xl flex-col overflow-hidden rounded-md shadow-md md:mx-8">
+  <div style="background-image: url('{backgroundUrl}');" class="h-12 w-full" />
+</waitlist-form>
