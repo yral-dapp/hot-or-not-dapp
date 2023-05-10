f6d82896925bf9d51d428bbc5e022a06d086bd2d
harshbaz <harsh@gobazzinga.io>
2023-05-10 13:07:27 +0700

    Show airdrop popup only on feed layout

--- packages/web-client/src/routes/(feed)/(splash)/+layout.svelte
+++ packages/web-client/src/routes/(feed)/(splash)/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 import SplashScreen from '$components/layout/SplashScreen.svelte'
+import AirdropPopup from '$components/popup/AirdropPopup.svelte'
 </script>
 
 <SplashScreen />
+<AirdropPopup />
 
 <slot />
