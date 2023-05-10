e4d2d0f89ba05dc72c0b9fc8a8b422d4ba99fcb8
harshbaz <harsh@gobazzinga.io>
2023-05-10 11:29:34 +0700

    Rename components

--- packages/web-client/src/components/vote-result/VoteWon.svelte
+++ packages/web-client/src/components/vote-result/VoteWon.svelte
@@ -1,18 +1,22 @@
 <script lang="ts">
 import Avatar from '$components/avatar/Avatar.svelte'
 import Button from '$components/button/Button.svelte'
+import VoteWonBackground from '$components/icons/VoteWonBackground.svelte'
 import { fade } from 'svelte/transition'
 </script>
 
-<bet-lost
+<vote-win
   transition:fade|local
   class="absolute z-[100] flex h-full w-full flex-col items-center justify-center space-y-10 bg-black px-8 text-white">
+  <div class="absolute z-0 h-full w-full overflow-hidden">
+    <VoteWonBackground class="h-full opacity-50" />
+  </div>
   <Avatar
     class="h-24 w-24"
     src="https://beebom.com/wp-content/uploads/2022/02/Featured.jpg?w=730&h=487&crop=1&quality=75" />
-  <div class="text-3xl font-bold">Uh-Oh! You lost</div>
+  <div class="text-3xl font-bold">Congrats! You win</div>
   <div class="text-center text-white/80">
-    You lost 50 tokens on the vote you placed on video “Dance with me”
+    You won 50 tokens on the vote you placed on video “Dance with me”
   </div>
   <div class="flex flex-col items-center">
     <div class="text-white/80">Your rank</div>
@@ -22,4 +26,4 @@ import { fade } from 'svelte/transition'
     <Button class="w-full">Place a new vote</Button>
     <Button type="secondary" class="w-full">Check balance</Button>
   </div>
-</bet-lost>
+</vote-win>
