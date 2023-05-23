683fd103f977d0abe4167be57a17bdbb734c0da5
harshbaz <harsh@gobazzinga.io>
2023-05-23 13:44:12 +0700

    Remove countdown popup

--- packages/web-client/src/routes/(feed)/(splash)/+layout.svelte
+++ packages/web-client/src/routes/(feed)/(splash)/+layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 import SplashScreen from '$components/layout/SplashScreen.svelte'
-import AirdropPopupOnlyCountdown from '$components/popup/AirdropPopupOnlyCountdown.svelte'
+import AirdropPopup from '$components/popup/AirdropPopup.svelte'
 import { splashScreenPopup } from '$stores/popups'
 </script>
 
 <SplashScreen />
 {#if !$splashScreenPopup.show}
-  <AirdropPopupOnlyCountdown />
+  <AirdropPopup />
 {/if}
 
 <slot />
