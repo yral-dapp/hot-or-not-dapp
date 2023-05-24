a9bca2d4152e91540b81a64fb3aa079123927989
harshbaz <harsh@gobazzinga.io>
2023-05-24 19:38:48 +0700

    Revert changes

--- packages/web-client/cypress/e2e/feed.cy.ts
+++ packages/web-client/cypress/e2e/feed.cy.ts
@@ -22,15 +22,13 @@ describe('Home Feed Tests', () => {
   })
 
   it('First video on the feed starts auto-playing', () => {
-    cy.get('video').first().its('0.paused').should('equal', false)
+    cy.get('video').first().and('have.prop', 'paused', false)
   })
 
   it('First video on a feed has a positive duration', () => {
-    cy.get('video').first()
     cy.get('video')
       .first()
-      .its('0.paused')
-      .should('equal', false)
+      .and('have.prop', 'paused', false)
       .then(() => {
         cy.get('video')
           .first()
@@ -41,11 +39,13 @@ describe('Home Feed Tests', () => {
   })
 
   it('Click to unmute video', () => {
-    const video = cy.get('video').first()
-    video.its('0.paused').should('equal', false)
-    video.and('have.prop', 'muted', true)
+    const video = cy
+      .get('video')
+      .first()
+      .and('have.prop', 'paused', false)
+      .and('have.prop', 'muted', true)
     video.click().then(() => {
-      video.and('have.prop', 'muted', false)
+      video.should('have.prop', 'muted', false)
     })
   })
 
