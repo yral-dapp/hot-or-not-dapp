d2ec0047dc442a9584e1441eebdfc9b44ac21a77
harshbaz <harsh@gobazzinga.io>
2023-06-07 15:35:43 +0530

    Update guide

--- packages/web-client/src/routes/airdrop-guide/+page.svelte
+++ packages/web-client/src/routes/airdrop-guide/+page.svelte
@@ -111,6 +111,7 @@ import { showAirdropPopup } from '$stores/popups'
           <li>Plug Principal ID holding Hot or Not Funded NFT</li>
           <li>NNS Principal ID holding SNS-1 Token</li>
           <li>NNS Principal ID holding Chat Token</li>
+          <li>DSCVR Principal ID for DSCVR users</li>
         </p>
 
         <h4>Rewards</h4>
