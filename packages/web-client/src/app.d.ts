4741e3b31f1a35b122dad9d0745b79a0fd4b6a96
harshbaz <harsh@gobazzinga.io>
2023-07-05 16:37:42 +0700

    Cleanup add typings

--- packages/web-client/src/app.d.ts
+++ packages/web-client/src/app.d.ts
@@ -9,28 +9,3 @@ declare namespace App {
   // interface Session {}
   // interface Stuff {}
 }
-
-namespace svelte.JSX {
-  interface SvelteWindowProps {
-    onbeforeinstallprompt?:
-      | EventHandler<BeforeInstallPromptEvent, Window>
-      | undefined
-      | null
-    onappinstalled?: EventHandler<Event, Window> | undefined | null
-  }
-
-  interface HTMLProps<T> {
-    'disableremoteplayback'?: boolean
-    'disablepictureinpicture'?: boolean
-    'x-webkit-airplay'?: 'deny' | 'allow'
-  }
-}
-
-interface BeforeInstallPromptEvent extends Event {
-  readonly platforms: string[]
-  readonly userChoice: Promise<{
-    outcome: 'accepted' | 'dismissed'
-    platform: string
-  }>
-  prompt(): Promise<void>
-}
