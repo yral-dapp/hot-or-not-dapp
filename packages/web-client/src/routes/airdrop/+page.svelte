5ab85fa5b743d459056d895b68691f281ad4617e
harshbaz <harsh@gobazzinga.io>
2023-07-27 18:53:30 +0700

    Remove unused logic

--- packages/web-client/src/routes/airdrop/+page.svelte
+++ packages/web-client/src/routes/airdrop/+page.svelte
@@ -5,28 +5,6 @@ import CaretLeftIcon from '$components/icons/CaretLeftIcon.svelte'
 import HomeLayout from '$components/layout/HomeLayout.svelte'
 import goBack from '$lib/utils/goBack'
 import { navigateBack } from '$stores/navigation'
-import { authState } from '$stores/auth'
-import { loadingAuthStatus } from '$stores/loading'
-import { isNNSIdRegistered, airdropEntryDetails } from '$lib/helpers/airdrop'
-
-let participatedForNNS = false
-let participatedForAirdrop = false
-let loading = true
-
-$: authorized = $authState.isLoggedIn && !$loadingAuthStatus
-$: authorized && checkIfCompleted()
-
-async function checkIfCompleted() {
-  if ($authState.idString) {
-    const res = await airdropEntryDetails($authState.idString)
-    if (!res) {
-      participatedForAirdrop = false
-    } else {
-      participatedForNNS = await isNNSIdRegistered($authState.idString)
-    }
-  }
-  loading = false
-}
 </script>
 
 <svelte:head>
