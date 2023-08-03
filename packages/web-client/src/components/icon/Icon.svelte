f46e8ffc960645a750746e9e8c40844315e23f46
harshbaz <harsh@gobazzinga.io>
2023-08-03 22:35:38 +0700

    Create icon component

---
+++ packages/web-client/src/components/icon/Icon.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+import type { IconName } from './icon.type'
+
+export let name: IconName
+export { exportClass as class }
+let exportClass: any = ''
+</script>
+
+<svg class={exportClass}>
+  <use xlink:href="/icons.sprite.svg#{name}" />
+</svg>
