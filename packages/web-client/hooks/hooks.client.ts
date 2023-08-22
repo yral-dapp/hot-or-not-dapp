d31ddfa30e75be6686e71e109105771893acd8a9
harshbaz <harsh@gobazzinga.io>
2023-08-22 09:30:13 +0530

    Update hooks

--- packages/web-client/hooks/hooks.client.ts
+++ packages/web-client/hooks/hooks.client.ts
@@ -1,20 +1,24 @@
+import { page } from '$app/stores'
 import { handleErrorWithSentry, Replay } from '@sentry/sveltekit'
 import * as Sentry from '@sentry/sveltekit'
+import { get } from 'svelte/store'
+
+const pageStore = get(page)
 
 Sentry.init({
   dsn: 'https://7586a69b01314524b31c8f4f64b41988@o4504076385124352.ingest.sentry.io/4504076386238464',
   tracesSampleRate: 1.0,
-
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
-
-  // If the entire session is not sampled, use the below sample rate to sample
-  // sessions when an error occurs.
+  replaysSessionSampleRate: 0.3,
   replaysOnErrorSampleRate: 1.0,
-
-  // If you don't want to use Session Replay, just remove the line below:
+  environment: pageStore.url.host.includes('t:') ? 'localDev' : 'production',
   integrations: [new Replay()],
+  ignoreErrors: [
+    /Adding invalid event/i, // Replay Error
+    /Error in compression worker/i, // Replay Error
+    /e.getLastBreadcrumb/i, // Sentry error
+    /chrome-extension/i, // Chrome extensions error
+    /. is not defined/i, //Unknown error
+  ],
 })
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
--- packages/web-client/hooks/server.hooks.ts
+++ packages/web-client/hooks/server.hooks.ts
@@ -1,7 +1,6 @@
 import { sequence } from '@sveltejs/kit/hooks'
-import { handleErrorWithSentry, sentryHandle } from '@sentry/sveltekit'
-import * as Sentry from '@sentry/sveltekit'
-import type { Handle } from '@sveltejs/kit'
+import type { Handle, HandleServerError } from '@sveltejs/kit'
+import { Toucan } from 'toucan-js'
 
 const handleHeaders: Handle = async ({ event, resolve }) => {
   const response = await resolve(event)
@@ -15,13 +14,46 @@ const handleHeaders: Handle = async ({ event, resolve }) => {
   return response
 }
 
-Sentry.init({
-  dsn: 'https://7586a69b01314524b31c8f4f64b41988@o4504076385124352.ingest.sentry.io/4504076386238464',
-  tracesSampleRate: 1.0,
-})
+const handleErrorWithToucan: HandleServerError = ({ error, event }) => {
+  const sentry = new Toucan({
+    dsn: 'https://7586a69b01314524b31c8f4f64b41988@o4504076385124352.ingest.sentry.io/4504076386238464',
+    environment: event.url.host.includes('t:') ? 'localDev' : 'production',
+    // @ts-ignore
+    release: import.meta.env.VITE_SENTRY_RELEASE,
+    request: event.request,
+    ignoreErrors: [
+      /Adding invalid event/i, // Replay Error
+      /Error in compression worker/i, // Replay Error
+      /e.getLastBreadcrumb/i, // Sentry error
+      /chrome-extension/i, // Chrome extensions error
+      /. is not defined/i, //Unknown error
+    ],
+    requestDataOptions: {
+      allowedHeaders: [
+        'user-agent',
+        'cf-challenge',
+        'accept-encoding',
+        'accept-language',
+        'cf-ray',
+        'content-length',
+        'content-type',
+        'x-real-ip',
+        'host',
+      ],
+      allowedSearchParams: /(.*)/,
+    },
+  })
+
+  sentry.captureException(error)
+
+  return {
+    ...(error as any),
+    code: (error as any)?.code ?? 'UNKNOWN',
+  }
+}
 
 // If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
-export const handle = sequence(sentryHandle(), handleHeaders)
+export const handle = sequence(handleHeaders)
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
-export const handleError = handleErrorWithSentry()
+export const handleError = handleErrorWithToucan
