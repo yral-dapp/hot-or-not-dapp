668882fb18a703da56f3e41a90830d5193cc5b35
harshbaz <harsh@gobazzinga.io>
2023-08-31 13:22:19 +0530

    Update completed logic

--- packages/web-client/src/routes/airdrop/+page.svelte
+++ packages/web-client/src/routes/airdrop/+page.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
-import AirdropForm from '$components/airdrop-form/AirdropForm.svelte'
 import IconButton from '$components/button/IconButton.svelte'
 import HomeLayout from '$components/layout/HomeLayout.svelte'
 import goBack from '$lib/utils/goBack'
 import { navigateBack } from '$stores/navigation'
-import { page } from '$app/stores'
 import { authState } from '$stores/auth'
 import { loadingAuthStatus } from '$stores/loading'
 import { airdropEntryDetails, isNNSIdRegistered } from '$lib/helpers/airdrop'
 import Icon from '$components/icon/Icon.svelte'
-import AirdropCompleted from '$components/airdrop-form/AirdropCompleted.svelte'
 import AirdropCompleteGraphics from '$components/airdrop-form/AirdropCompleteGraphics.svelte'
 import Button from '$components/button/Button.svelte'
 import AirdropEndGraphics from '$components/airdrop-form/AirdropEndGraphics.svelte'
@@ -17,16 +14,12 @@ import AirdropEndGraphics from '$components/airdrop-form/AirdropEndGraphics.svel
 let loading = true
 let participatedForNNS = false
 let participatedForAirdrop = false
-let submitted = false
 let nnsValue = ''
 let wallet = {
   coyn: '0',
   hot: 0,
   loading: true,
 }
-
-$: edit = !!$page.url.searchParams.has('edit')
-$: editAndSubmitted = edit && !submitted
 
 async function checkIfCompleted() {
   if ($authState.idString) {
@@ -50,6 +43,7 @@ async function checkIfCompleted() {
 
 $: authorized = $authState.isLoggedIn && !$loadingAuthStatus
 $: authorized && checkIfCompleted()
+$: !authorized && (loading = false)
 </script>
 
 <svelte:head>
@@ -79,68 +73,72 @@ $: authorized && checkIfCompleted()
       <div
         class="flex h-full w-full flex-col items-center gap-10 overflow-y-auto py-8">
         {#if participatedForNNS}
-          <AirdropCompleted />
-        {:else if !participatedForAirdrop && authorized}
-          <div class="max-w-80 mt-4 px-16 sm:mt-10 sm:!max-h-80">
-            <AirdropCompleteGraphics class="h-full w-full" />
-          </div>
-          <div
-            class="flex w-full max-w-md flex-col items-center space-y-4 px-16">
-            <div
-              class="text-center text-3xl font-bold uppercase text-transparent text-white">
-              Airdrop Registration Has Ended
-            </div>
-            <div class="text-center text-sm">
-              Thank you for your interest! We are no longer accepting new
-              registrations.
-            </div>
-
-            <Button href="/" disabled={loading} class="w-full">
-              Play to Earn
-            </Button>
-            <div class="text-center text-[0.6rem] opacity-70">
-              <span class="font-bold">PID:</span>
-              <span>P: {$authState.idString}</span>
-              <span class="font-bold">CID:</span>
-              <span>{$authState.userCanisterId}</span>
-              <span class="font-bold">S:</span>
-              <span>
-                A:{participatedForAirdrop ? '1' : '0'},N:{participatedForNNS
-                  ? '1'
-                  : '0'}
-              </span>
-            </div>
-          </div>
-        {:else}
           <div class="max-w-80 mt-4 px-16 sm:mt-10 sm:!max-h-80">
             <AirdropEndGraphics class="h-full w-full" />
           </div>
           <div
             class="flex w-full max-w-md flex-col items-center space-y-4 px-16">
             <div
-              class="text-center text-3xl font-bold uppercase text-transparent text-white">
-              the wait is over!!
+              class="text-3xl font-bold uppercase text-transparent text-white">
+              Well done!
             </div>
             <div class="text-md text-center">
-              {#if authorized}
-                Your profile has been successfully registered for the airdrop.
-                Please submit your NNS HOT Address to start the claim process.
+              You have successfully submitted your NNS Principal ID to claim
+              your airdrop.
+            </div>
+            {#if !wallet.loading}
+              <div>
+                <div class="text-md text-center">Your Hot token allocation</div>
+                <div class="text-md text-center text-3xl font-bold">
+                  {wallet.hot}
+                </div>
+              </div>
+            {/if}
+            <div class="py-4 text-center text-sm">
+              The HOT tokens will be transferred to your NNS wallet by our team
+              over the course of a few months as we go through the process
+              manually with over 16,000 winners. Please be patient and check our
+              socials for updates.
+            </div>
+
+            <Button href="/hotornot" class="w-full">Play to earn</Button>
+
+            <a
+              href="/airdrop-guide"
+              on:click|stopPropagation
+              class="text-sm underline opacity-75">
+              Learn more about the rewards here
+            </a>
+          </div>
+        {:else}
+          <div class="max-w-80 mt-4 px-16 sm:mt-10 sm:!max-h-80">
+            <AirdropCompleteGraphics class="h-full w-full" />
+          </div>
+          <div
+            class="flex w-full max-w-md flex-col items-center space-y-4 px-16">
+            <div
+              class="text-center text-2xl font-bold uppercase text-transparent text-white">
+              {#if participatedForAirdrop}
+                Airdrop Claim Form is Closed
               {:else}
-                The HOT token airdrop claim process has started. If you
-                registered for the airdrop before 15th July 2023, please login
-                to see your status.
+                Airdrop Registration Has Ended
+              {/if}
+            </div>
+            <div class="text-md text-center">
+              {#if authorized && participatedForAirdrop}
+                Thank you for your interest! We are no longer accepting NNS IDs
+                of Airdrop participants.
+              {:else if authorized}
+                Thank you for your interest! We are no longer accepting new
+                registrations. Follow our socials for more information
+              {:else}
+                Thank you for your interest! We are no longer accepting new
+                registrations. If you have already claimed the airdrop, please
+                login to see your status.
               {/if}
             </div>
 
-            <Button disabled={loading} href="/airdrop" class="w-full">
-              {#if loading}
-                <Icon name="loading" class="h-4 w-4 animate-spin-slow" />
-              {:else if authorized}
-                Claim your airdrop
-              {:else}
-                Login
-              {/if}
-            </Button>
+            <Button href="/" class="w-full">Play to Earn</Button>
           </div>
         {/if}
         <div class="flex flex-col items-center justify-center space-y-4 px-8">
