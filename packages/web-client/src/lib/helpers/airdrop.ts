10c1febcbbc72d0e4ab8465bd526bd0b801e31ee
harshbaz <harsh@gobazzinga.io>
2023-08-31 23:34:33 +0530

    Remove unused methods

--- packages/web-client/src/lib/helpers/airdrop.ts
+++ packages/web-client/src/lib/helpers/airdrop.ts
@@ -1,39 +1,3 @@
-export async function registerNNSId(
-  data: {
-    principalId: string
-    canisterId: string
-    nnsId: string
-  },
-  update: boolean,
-): Promise<boolean> {
-  try {
-    const res = await fetch(
-      update
-        ? 'https://updatennsid-5nps3y6y6a-uc.a.run.app'
-        : 'https://registernnsid-5nps3y6y6a-uc.a.run.app',
-      {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          ...data,
-          uid: btoa(data.canisterId + 'a@1' + data.principalId),
-        }),
-      },
-    )
-
-    const body = await res.json()
-
-    if (body.success) return true
-    return false
-  } catch (e) {
-    console.error('Error adding document: ', e)
-    return false
-  }
-}
-
 export async function airdropEntryDetails(principalId: string) {
   try {
     const res = await fetch(
