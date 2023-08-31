b3ca09fd2dd4f31c8f9013d32d668dcc81a6cd04
harshbaz <harsh@gobazzinga.io>
2023-08-31 12:17:15 +0530

    Remove airdrop popup

--- packages/web-client/src/routes/(feed)/(splash)/+layout.svelte
+++ packages/web-client/src/routes/(feed)/(splash)/+layout.svelte
@@ -1,12 +1,6 @@
 <script lang="ts">
 import SplashScreen from '$components/layout/SplashScreen.svelte'
-import AirdropPopup from '$components/popup/AirdropPopup.svelte'
-import { showAirdropPopup, splashScreenPopup } from '$stores/popups'
 </script>
 
 <SplashScreen />
-{#if $showAirdropPopup}
-  <AirdropPopup />
-{/if}
-
 <slot />
