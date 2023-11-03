79d8d4c963bebb4e11b413c0cfe6b3b7d7dd22d3
harshbaz <harsh@gobazzinga.io>
2023-11-03 07:15:27 +0530

    Cleanup

--- packages/experiments/src/lib/utils/feedUrl.ts
+++ packages/experiments/src/lib/utils/feedUrl.ts
@@ -1,12 +1,10 @@
 import { registerPageView } from '$components/analytics/GA.svelte'
-import type { PostPopulated } from '$lib/helpers/feed'
-import { navigateBack } from '$stores/navigation'
+import type { UpDownPost } from '$lib/helpers/db.type'
 import { playerState } from '$stores/playerState'
 
-export function updateURL(post?: PostPopulated) {
+export function updateURL(post?: UpDownPost) {
   if (!post) return
-  const url = post.publisher_canister_id + '@' + post.post_id
-  navigateBack.set(url)
+  const url = post.ouid + '@' + post.oid
   playerState.update((o) => ({ ...o, currentFeedUrl: url }))
   window.history.replaceState('', '', url)
   registerPageView(new URL(window.location.href))
