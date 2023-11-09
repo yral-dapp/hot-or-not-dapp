539ca54f4d7f632518c36893a7a37017107af6a2
harshbaz <harsh@gobazzinga.io>
2023-11-09 09:33:12 +0530

    Update selector

--- packages/experiments/src/routes/(feed)/+layout.svelte
+++ packages/experiments/src/routes/(feed)/+layout.svelte
@@ -39,18 +39,23 @@ $: votesPage = pathname.includes('votes')
         class={c(
           'absolute inset-x-0 z-[1] h-9 rounded-full bg-primary transition-all duration-200',
           {
-            'opacity-0': walletPage || votesPage,
+            'opacity-0': walletPage,
             'w-20 translate-x-[10.25rem]': resultPage,
-            'w-40 translate-x-2': !walletPage && !resultPage,
+            'w-40 translate-x-2': !walletPage && !resultPage && !votesPage,
+            'w-[6.5rem] translate-x-2': votesPage,
           },
         )} />
 
-      <a
-        on:click={() => (resultPage = false)}
-        href="/up-down"
-        class="z-[2] shrink-0">
-        Up & Down Game
-      </a>
+      {#if votesPage}
+        <div class="z-[2] shrink-0">Your votes</div>
+      {:else}
+        <a
+          on:click={() => (resultPage = false)}
+          href="/up-down"
+          class="z-[2] shrink-0">
+          Up & Down Game
+        </a>
+      {/if}
 
       <a
         on:click={() => (resultPage = true)}
