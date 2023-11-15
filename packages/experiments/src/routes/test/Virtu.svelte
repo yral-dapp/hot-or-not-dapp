1091ce5a1fa24d1046a911fbbe9960de7cc9177e
harshbaz <harsh@gobazzinga.io>
2023-11-15 07:39:09 +0530

    Add slider test

---
+++ packages/experiments/src/routes/test/Virtu.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+import { tick } from 'svelte'
+import { debounce } from 'throttle-debounce'
+
+export let activeIndex: number
+export let index: number
+
+let show = index < 3
+
+const checkToShow = debounce(750, async (activeIndex: number) => {
+  await tick()
+  setTimeout(() => {
+    show = index > activeIndex - 4 && index < activeIndex + 4
+  }, 0)
+})
+
+$: checkToShow(activeIndex)
+$: console.log(index, show)
+</script>
+
+{#if show}
+  <slot {show} />
+{/if}
