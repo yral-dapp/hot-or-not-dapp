5d60d72e1edb069042d604f320258c3fd2a11078
harshbaz <harsh@gobazzinga.io>
2023-11-18 13:29:12 +0530

    Move actions into a separate file

---
+++ packages/experiments/src/components/layout/PlayerLayout.actions.ts
@@ -0,0 +1,124 @@
+import { registerEvent } from '$components/analytics/GA.utils'
+import {
+  shareVideo,
+  toggleDislike,
+  toggleLike,
+  viewVideo,
+} from '$lib/db/actions'
+import type { UpDownPost } from '$lib/db/db.types'
+import { updatePostInWatchHistory } from '$lib/idb/history'
+import { authState } from '$stores/auth'
+import { get } from 'svelte/store'
+
+export type WatchProgress = {
+  totalCount: number
+  partialWatchedPercentage: number
+}
+
+export async function sharePost(post: UpDownPost) {
+  try {
+    await navigator.share({
+      title: 'Hot or Not',
+      text: `Check out this video at UpDown! \n${post.description}`,
+      url: `https://experiments.hotornot.wtf/up-down/${post.ouid}@${post.oid}`,
+    })
+  } catch (e) {
+    console.warn("Can't share", e)
+  } finally {
+    await shareVideo({
+      videoId: post.id,
+      videoOid: post.oid,
+      videoUoid: post.ouid,
+      videoUid: post.video_uid,
+    })
+
+    const authStateData = get(authState)
+
+    registerEvent('share_video', {
+      userId: authStateData.userId,
+      video_id: post.id,
+      share_count: post.share_count,
+      anon: !!authStateData.isLoggedIn,
+    })
+  }
+}
+
+export async function likePost(post: UpDownPost) {
+  await toggleLike({
+    videoId: post.id,
+    videoOid: post.oid,
+    videoUoid: post.ouid,
+    videoUid: post.video_uid,
+  })
+
+  // updatePostInWatchHistory('up-down-watch-history', post, {
+  //   liked_by_me: !post.liked_by_me,
+  //   like_count: post.like_count + BigInt(post.liked_by_me ? -1 : 1),
+  // })
+
+  const authStateData = get(authState)
+
+  registerEvent('like_video', {
+    userId: authStateData.userId,
+    video_id: post.id,
+    likes: post.likes_count,
+    anon: !!authStateData.isLoggedIn,
+  })
+}
+
+export async function dislikePost(post: UpDownPost) {
+  await toggleDislike({
+    videoId: post.id,
+    videoOid: post.oid,
+    videoUoid: post.ouid,
+    videoUid: post.video_uid,
+  })
+
+  // updatePostInWatchHistory('up-down-watch-history', post, {
+  //   liked_by_me: !post.liked_by_me,
+  //   like_count: post.like_count + BigInt(post.liked_by_me ? -1 : 1),
+  // })
+
+  const authStateData = get(authState)
+
+  registerEvent('dislike_video', {
+    userId: authStateData.userId,
+    video_id: post.id,
+    likes: post.likes_count,
+    anon: !!authStateData.isLoggedIn,
+  })
+}
+
+export async function viewPost(
+  post: UpDownPost,
+  watchProgress?: WatchProgress,
+) {
+  if (!watchProgress) return
+  const { totalCount, partialWatchedPercentage } = watchProgress
+  if (totalCount === 0 && partialWatchedPercentage === 0) return
+
+  updatePostInWatchHistory('up-down-watch-history', post)
+
+  const watchCount = (totalCount || 0) + (partialWatchedPercentage || 0)
+
+  if (!watchCount) return
+
+  await viewVideo(
+    {
+      videoId: post.id,
+      videoOid: post.oid,
+      videoUoid: post.ouid,
+      videoUid: post.video_uid,
+    },
+    watchCount,
+  )
+
+  const authStateData = get(authState)
+
+  registerEvent('view_video', {
+    userId: authStateData.userId,
+    video_id: post.id,
+    view_count: post.views_count,
+    anon: !!authStateData.isLoggedIn,
+  })
+}
