379b961566c7ee611b94de00eb7f29c71db6fad7
harshbaz <harsh@gobazzinga.io>
2023-11-18 13:29:00 +0530

    Move GA utils into a separate file

--- packages/experiments/src/components/analytics/GA.svelte
+++ packages/experiments/src/components/analytics/GA.svelte
@@ -1,45 +1,7 @@
-<script lang="ts" context="module">
-// const debugMode = import.meta.env.NODE_ENV === 'development';
-const debugMode = true
-
-export const registerPageView = (url: URL = new URL(window.location.href)) => {
-  if (url?.href) {
-    window.gtag?.('event', 'page_view', {
-      page_location: url.href,
-    })
-  }
-}
-
-export const updateConfig = (params?: Gtag.CustomParams) => {
-  if (window.gtag) {
-    window.gtag('config', 'G-PPE5XD2VKV', {
-      ...params,
-      ...(debugMode && { debug_mode: true }),
-    })
-    return true
-  }
-}
-
-export const setUserProperties = (params?: Gtag.CustomParams) => {
-  window.gtag?.('set', 'user_properties', {
-    ...params,
-  })
-}
-
-export const registerEvent = (
-  eventName: Gtag.EventNames | string,
-  eventParams?: Gtag.ControlParams | Gtag.EventParams | Gtag.CustomParams,
-) => {
-  window.gtag?.('event', eventName, {
-    ...eventParams,
-    ...(debugMode && { debug_mode: true }),
-  })
-}
-</script>
-
 <script lang="ts">
 import { page } from '$app/stores'
 import { splashScreenPopup } from '$stores/popups'
+import { registerPageView, updateConfig } from './GA.utils'
 
 let configured = false
 $: href = $page?.url?.href
