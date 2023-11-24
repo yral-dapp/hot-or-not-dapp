8bc3ec0568f352b81a8e841c6dbdd5449c2ea850
harshbaz <harsh@gobazzinga.io>
2023-11-24 09:27:08 +0530

    Update page

--- packages/experiments/src/routes/invite/+page.svelte
+++ packages/experiments/src/routes/invite/+page.svelte
@@ -11,7 +11,7 @@ import { onMount } from 'svelte'
 function checkIfInviteIsValid() {
   const refById = $page.url.searchParams.get('userId')
   if (!refById) {
-    goto('/')
+    // goto('/')
   } else {
     $authState.refById = refById
   }
@@ -38,7 +38,7 @@ onMount(() => {
   </svelte:fragment>
   <svelte:fragment slot="content">
     <div
-      class="flex h-full w-full flex-col items-center space-y-4 overflow-hidden overflow-y-scroll px-8 pb-20 pt-16">
+      class="flex h-full w-full flex-col items-center space-y-4 overflow-hidden overflow-y-scroll px-8 pb-20 pt-24">
       <Icon name="hot-or-not-logo" class="h-56 w-56" />
       {#if $authState.isLoggedIn}
         <div class="shrink-0 text-center text-sm opacity-70">
@@ -46,7 +46,7 @@ onMount(() => {
         </div>
         <Button href="/refer">Go to refer & earn</Button>
       {:else}
-        <div class="text-center text-3xl">Welcome</div>
+        <div class="pt-8 text-center text-3xl">Welcome</div>
         <div class="text-center text-sm opacity-70">
           You have been invite to join Hot or Not experiments!
         </div>
@@ -55,7 +55,9 @@ onMount(() => {
             Join now
           </Button>
         </div>
-        <div>or you can visit our homepage to check out hottest videos!</div>
+        <div class="text-center text-sm opacity-70">
+          or you can visit our homepage to check out hottest videos!
+        </div>
         <Button href="/">Go to homepage</Button>
       {/if}
     </div>
