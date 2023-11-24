8e966c31c2d6e8cdc927a3e156668f828cf77d79
harshbaz <harsh@gobazzinga.io>
2023-11-24 09:20:00 +0530

    Persist referral id

---
+++ packages/experiments/src/routes/invite/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { goto } from '$app/navigation'
+import { page } from '$app/stores'
+import { authState } from '$stores/auth'
+import { onMount } from 'svelte'
+
+function checkIfInviteIsValid() {
+  const refById = $page.url.searchParams.get('userId')
+  if (!refById) {
+    goto('/')
+  } else {
+    $authState.refById = refById
+  }
+}
+
+onMount(() => {
+  checkIfInviteIsValid()
+})
+</script>
