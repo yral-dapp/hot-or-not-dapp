17cf67104e717d584ab79deebf36d82bd2094a89
harshbaz <harsh@gobazzinga.io>
2023-11-28 10:16:47 +0530

    Mount onboarding after some time

--- packages/experiments/src/routes/(feed)/up-down/+layout.svelte
+++ packages/experiments/src/routes/(feed)/up-down/+layout.svelte
@@ -1,14 +1,29 @@
 <script lang="ts">
 import FeedLayout from '$components/layout/FeedLayout.svelte'
-import Onboarding from '$components/popup/Onboarding.svelte'
+import { showOnboardingPopup } from '$stores/popups'
+import { onMount } from 'svelte'
+
+let onboarding: any
+function mountOnboarding() {
+  setTimeout(
+    () =>
+      import('$components/popup/Onboarding.svelte').then(
+        (d) => (onboarding = d.default),
+      ),
+    3000,
+  )
+}
+
+onMount(() => mountOnboarding())
 </script>
 
 <svelte:head>
   <title>Up Down | Hot or Not</title>
 </svelte:head>
 
-<Onboarding />
-
+{#if onboarding && $showOnboardingPopup}
+  <svelte:component this={onboarding} />
+{/if}
 <FeedLayout>
   <svelte:fragment slot="content">
     <slot />
