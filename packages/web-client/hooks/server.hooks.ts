0daa3f5e9a47cc0d7884f8e54bb96a4e000ede08
harshbaz <harsh@gobazzinga.io>
2024-03-27 09:38:06 +0530

    Bump deps

--- packages/web-client/hooks/server.hooks.ts
+++ packages/web-client/hooks/server.hooks.ts
@@ -1,10 +1,10 @@
 import type { Handle } from '@sveltejs/kit'
 
 const hlsScriptTag =
-  '<script async="false" defer="false" src="https://cdn.jsdelivr.net/npm/hls.js@1.5.5/dist/hls.light.min.js"></script>'
+  '<script async="false" defer="false" src="https://cdn.jsdelivr.net/npm/hls.js@1.5.7/dist/hls.light.min.js"></script>'
 
 export const handle: Handle = async ({ event, resolve }) => {
-  const useHls = true
+  const useHls = false
   const response = await resolve(event, {
     transformPageChunk: ({ html }) =>
       html.replace('%js.deps%', useHls ? hlsScriptTag : ''),
